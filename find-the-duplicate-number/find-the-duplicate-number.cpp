bf0d5972c384adbcae286b6c5ae15ff47f0ee8b0
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2021-12-26 14:38:04 +0530

    Time: 160 ms (25.66%), Space: 61.2 MB (90.31%) - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -3,18 +3,25 @@ public:
     int findDuplicate(vector<int>& nums) {
         
         int slow = nums[0];
-        int fast = nums[nums[0]];
-        
-        while(slow != fast){
+        int fast = nums[0];
+        do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
+        while(slow!=fast);
         
-        cout << fast << endl;
         
         
-        // fast = nums[0];
-        fast = 0;
+//         while(slow != fast){
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         }
+        
+//         cout << fast << endl;
+        
+        
+        fast = nums[0];
+        // fast = 0;
         
         
         while(slow!= fast){
