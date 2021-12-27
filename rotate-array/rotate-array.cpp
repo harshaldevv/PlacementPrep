b416deec0dc1985a6f203ff4f57be016d51038da
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2021-12-27 09:24:32 +0530

    Time: 32 ms (43.02%), Space: 25 MB (79.11%) - LeetHub

--- rotate-array/rotate-array.cpp
+++ rotate-array/rotate-array.cpp
@@ -3,10 +3,16 @@ public:
     void rotate(vector<int>& nums, int k) {
         
         int n = nums.size();
-        k = k%n;
-        reverse(nums, 0, n-1);
-        reverse(nums, 0, k-1);
-        reverse(nums, k, n-1);
+        
+        if(k==n) return;
+        if(k==0) return;
+        if(n<=1) return;
+        // if(k == k%n) return; // it means ki effectively k = 0 hai 
+        
+        k = k%n;  // to bring the k in its limit
+        reverse(nums, 0, n-1); // reverse the whole list
+        reverse(nums, 0, k-1); // reverse the first k elements
+        reverse(nums, k, n-1); // reverse the last n-k elements
         
         
     }
