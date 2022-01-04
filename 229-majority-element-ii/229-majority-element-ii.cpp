cfc89b242127468d9c75a9c1348fecd352b2bf01
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-01-04 18:32:51 +0530

    Time: 15 ms (39.07%), Space: 15.8 MB (80.50%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -5,6 +5,58 @@ public:
         // Hint - How many majority elements could it possibly have
         // Ans - maximum 2 majority elements --> Therefore try doing it in constant space complexity
         
+        int count1 = 0;
+        int count2 = 0;
+        
+        int numb1 = INT_MIN;
+        int numb2 = INT_MIN;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            if(numb1 == nums[i]){
+                count1++;
+            }
+            else if(numb2 == nums[i]){
+                count2++;
+            }
+            else if(count1 ==0){
+                numb1 = nums[i];
+                count1++;
+            }
+            else if (count2 == 0){
+                numb2 = nums[i];
+                count2++;
+            }
+            else{
+                count1--, count2--;
+            }
+        }
+        
+        vector<int> ans;
+        
+        int c1=0;
+        int c2 = 0;
+        
+        for(int  i = 0 ; i < n ; i++){
+            if(nums[i] == numb1){
+                c1++;
+            }
+            if(nums[i] == numb2){
+                c2++;
+            }
+        }
+        
+        
+        if(c1 > n/3){
+            ans.push_back(numb1);
+        }
+        if(c2 > n/3){
+            ans.push_back(numb2);
+        }
+        
+        return ans;
+        
+        
+        
         
         
         
@@ -28,30 +80,25 @@ public:
         
 
         
-        unordered_map<int, int> mp;
-        unordered_map<int, int> mp2;
+//         unordered_map<int, int> mp;
+//         unordered_map<int, int> mp2;
         
-        int n = nums.size();
+//         int n = nums.size();
         
-        vector<int> ans;
+//         vector<int> ans;
         
-        for(int i = 0 ; i < n; i++){
+//         for(int i = 0 ; i < n; i++){
         
-            mp[nums[i]]++;
-        }
+//             mp[nums[i]]++;
+//         }
         
-        for( auto it : mp){
-            if(it.second > n/3){
-                ans.push_back(it.first);
-                // mp2[it.first]++;
-            }
-        }
+//         for( auto it : mp){
+//             if(it.second > n/3){
+//                 ans.push_back(it.first);
+//             }
+//         }
         
-        
-        // for(auto it : mp2){
-        //     ans.push_back(it.first);
-        // }
-        return ans;
+//         return ans;
         
     }
 };
