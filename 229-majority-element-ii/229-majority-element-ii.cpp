a00225f23381babf77c3fa1fd573a4d450b6575c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-01-04 14:55:47 +0530

    Time: 37 ms (5.16%), Space: 16 MB (12.36%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
+        // Hint - How many majority elements could it possibly have
+        // Ans - maximum 2 majority elements --> Therefore try doing it in constant space complexity
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        
         unordered_map<int, int> mp;
         unordered_map<int, int> mp2;
         
@@ -12,23 +38,19 @@ public:
         for(int i = 0 ; i < n; i++){
         
             mp[nums[i]]++;
-            
-            // cout << mp[nums[i]] << endl;
-            // if(mp[nums[i]] > n/3){
-            //     ans.push_back(nums[i]);
-            // }
         }
         
         for( auto it : mp){
             if(it.second > n/3){
-                mp2[it.first]++;
+                ans.push_back(it.first);
+                // mp2[it.first]++;
             }
         }
         
         
-        for(auto it : mp2){
-            ans.push_back(it.first);
-        }
+        // for(auto it : mp2){
+        //     ans.push_back(it.first);
+        // }
         return ans;
         
     }
