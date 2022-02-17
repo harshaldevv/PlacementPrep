fee7dd33f86b681ed0732d50106bf0ca354e8a0b
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-02-17 12:46:53 +0530

    Time: 96 ms (62.78%), Space: 10.5 MB (83.35%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int n = coins.size();
+        
+        int t[n+1][amount +1];
+        
+        for(int i = 0 ; i < n+1 ; i++){
+            t[i][0] = 0;
+        }
+        
+        for(int j =0  ; j < amount +1 ; j++){
+            t[0][j] = INT_MAX -1;
+        }
+        
+        t[0][0] = 0;  // dekhte hai chalo
+        
+        for(int i = 1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < amount +1 ; j++){
+                if(coins[i-1] <= j){
+                    t[i][j] = min(1 +  t[i][j-coins[i-1]], t[i-1][j]);
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        
+        int ans = t[n][amount];
+        
+        if(ans > amount){
+            return -1;
+        }
+        else{
+            return ans;
+        }
+        
+        
+        
+    }
+};
