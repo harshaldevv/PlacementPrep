fcf6b87d5dd6ad8710b3e338924a4defecb1e36f
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-02 23:22:55 +0530

    Time: 402 ms (66.23%), Space: 65 MB (57.83%) - LeetHub

--- 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
+++ 2183-count-array-pairs-divisible-by-k/2183-count-array-pairs-divisible-by-k.cpp
@@ -1,29 +1,63 @@
 class Solution {
-public:
-    long long countPairs(vector<int>& nums, int k) {
-        
-        unordered_map<int, int> mp;
-        
-        int n = nums.size();
+    
+    long long helper(vector<int> &nums, int k){
+        // striver approach --> https://www.youtube.com/watch?v=khFetYmKf-4
         long long ans = 0;
         
-        for(int i = 0 ; i < n ; i++){
-            
+        int  n = nums.size();
+        unordered_map<int, int> mp;
+        
+        
+        for(int i = 0; i < n; i++){
             long long gcd1 = __gcd(nums[i], k);
+            long long gcd2 = k/gcd1;
             
-            for(auto it : mp){
-                int gcd2 = it.first;
-                
-                if( (long long)gcd1 *gcd2 %k ==0 ) {
-                    ans += it.second;
-                }
+            if(gcd2 == 1){
+                ans += i;
+            }
+            else{
+                for(auto it: mp){
+                    if(it.first%gcd2 == 0){
+                        ans+= it.second;
+                    }
+                }  
             }
             
             mp[gcd1]++;
-            
         }
         
         return ans;
+    }
+    
+public:
+    long long countPairs(vector<int>& nums, int k) {
+        
+        return helper(nums, k); // striver approach
+        
+        
+//         // coding decoded approach ---> https://www.youtube.com/watch?v=3pF0cdWfqSE
+//         unordered_map<int, int> mp;
+        
+//         int n = nums.size();
+//         long long ans = 0;
+        
+//         for(int i = 0 ; i < n ; i++){
+            
+//             long long gcd1 = __gcd(nums[i], k);
+            
+//             for(auto it : mp){
+//                 int gcd2 = it.first;
+                
+//                 if( (long long)gcd1 *gcd2 %k ==0 ) {
+//                     ans += it.second;
+//                 }
+//             }
+            
+//             mp[gcd1]++;
+            
+//         }
+        
+//         return ans;
         
     }
 };
