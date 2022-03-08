253b75c60de7a795924149abeab483574c1b76cf
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-09 02:12:21 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -12,40 +12,32 @@ class Solution
     //Function to calculate the span of stockâ€™s price for all n days.
     vector <int> calculateSpan(int price[], int n)
     {
+        // NGL basically 
+        
        // Your code here
-    stack<pair<int, int>> st;
-    vector<int> v;
-    vector<int> ans;
-    int size = n;
-    
-    
-    for(int i = 0 ; i < size ; i++){
-        if(st.size() == 0){
-            v.push_back(-1);
-        }
-        else if (st.size() > 0 && st.top().first > price[i]){
-            v.push_back(st.top().second);
-        }
-        else if(st.size() > 0 && st.top().first <= price[i]){
-            while(st.size() > 0 && st.top().first <= price[i]){
-                st.pop();
-            }
-            
-            if(st.size() == 0){
-                v.push_back(-1);
-            }
-            else{
-                v.push_back(st.top().second);
-            }
-        }
-        st.push({price[i], i});
-    }
-    
-    for(int i = 0 ; i < size ; i++){
-        v[i] = ( i- v[i]);
-    }
-    
-    return v;
+       vector<int> v;
+       stack<pair<int, int>> st;  // first -> price ; second - > index
+       vector<int> ans(n);
+       
+       for(int i = 0 ; i < n ; i++){
+           
+           while(!st.empty() && st.top().first <= price[i]){
+               st.pop();
+           }
+           
+           if(st.empty()){
+               v.push_back(-1);
+           }
+           else{
+               v.push_back(st.top().second );
+           }
+           pair<int, int> p(price[i], i);
+           st.push(p);
+           
+           ans[i] = i - v[i];
+       }
+       
+       return ans;
     }
 };
 
