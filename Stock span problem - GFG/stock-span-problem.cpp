928c3919f62aabf090fbf56cdfed599b335b4e3c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-09 02:25:30 +0530

    Added solution - LeetHub

--- Stock span problem - GFG/stock-span-problem.cpp
+++ Stock span problem - GFG/stock-span-problem.cpp
@@ -12,32 +12,65 @@ class Solution
     //Function to calculate the span of stockâ€™s price for all n days.
     vector <int> calculateSpan(int price[], int n)
     {
-        // NGL basically 
+        // NGL basically
+        vector<int> ans;
+        stack<pair<int, int>> st;
         
-       // Your code here
-       vector<int> v;
-       stack<pair<int, int>> st;  // first -> price ; second - > index
-       vector<int> ans(n);
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && st.top().first <= price[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                ans.push_back(i-(-1));
+            }
+            else{
+                ans.push_back(i - st.top().second);
+            }
+            
+            pair<int, int> p(price[i], i);
+            st.push(p);
+        }
+        
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    //   // Your code here
+    //   vector<int> v;
+    //   stack<pair<int, int>> st;  // first -> price ; second - > index
+    //   vector<int> ans(n);
        
-       for(int i = 0 ; i < n ; i++){
+    //   for(int i = 0 ; i < n ; i++){
            
-           while(!st.empty() && st.top().first <= price[i]){
-               st.pop();
-           }
+    //       while(!st.empty() && st.top().first <= price[i]){
+    //           st.pop();
+    //       }
            
-           if(st.empty()){
-               v.push_back(-1);
-           }
-           else{
-               v.push_back(st.top().second );
-           }
-           pair<int, int> p(price[i], i);
-           st.push(p);
+    //       if(st.empty()){
+    //           v.push_back(-1);
+    //       }
+    //       else{
+    //           v.push_back(st.top().second );
+    //       }
+    //       pair<int, int> p(price[i], i);
+    //       st.push(p);
            
-           ans[i] = i - v[i];
-       }
+    //       ans[i] = i - v[i];
+    //   }
        
-       return ans;
+    //   return ans;
     }
 };
 
