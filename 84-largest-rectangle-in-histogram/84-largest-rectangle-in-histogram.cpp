764bd72a56e72df2e8358240b9c7d97caa956f5e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-09 03:23:38 +0530

    Time: 251 ms (30.27%), Space: 92.1 MB (5.90%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,107 +1,72 @@
 class Solution {
 public:
-    vector<int> NSR(vector<int> &arr){
-        stack<pair<int, int>> st;
-        vector<int> v;
-        int pseudoIndex = arr.size() ;
-        
-        for(int i = arr.size() -1 ; i >=0  ; i--){
-            if(st.size() == 0){
-                v.push_back(pseudoIndex);
-            }
-            
-            if(st.size() > 0 && st.top().first < arr[i]){
-                v.push_back(st.top().second);
-            }
-            if(st.size() > 0 && st.top().first >= arr[i]){
-                while(st.size() >0 && st.top().first >= arr[i]){
-                    st.pop();
-                }
-                
-                if(st.size() == 0){
-                    v.push_back(pseudoIndex);
-                }
-                else{
-                    v.push_back(st.top().second);
-                }
-            }
-            
-            st.push({arr[i], i});
-        }
-        
-        // for(int i = 0 ; i < arr.size() ; i++){
-        //     v[i] = i- v[i];
-        // }
-        
-        reverse(v.begin(), v.end());
-        return v;
-    }
     
-    vector<int> NSL(vector<int> &arr){
-        stack<pair<int, int>> st;
+    vector<int> NSL(vector<int> &nums, int n){
         vector<int> v;
+        stack<pair<int, int>> st;  // pair -> first == height, second == index
+        
         int pseudoIndex = -1;
         
-        for(int i = 0 ; i < arr.size() ; i++){
-            if(st.size() == 0){
-                v.push_back(pseudoIndex);
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && st.top().first >= nums[i]){
+                st.pop();
             }
             
-            if(st.size() > 0 && st.top().first < arr[i]){
+            if(st.empty()){
+                v.push_back(pseudoIndex);
+            }
+            else{
                 v.push_back(st.top().second);
             }
-            if(st.size() > 0 && st.top().first >= arr[i]){
-                while(st.size() >0 && st.top().first >= arr[i]){
-                    st.pop();
-                }
-                
-                if(st.size() == 0){
-                    v.push_back(pseudoIndex);
-                }
-                else{
-                    v.push_back(st.top().second);
-                }
-            }
-            st.push({arr[i], i});
+            
+            st.push({nums[i], i});
         }
-        
-        // for(int i = 0 ; i < arr.size() ; i++){
-        //     v[i] = i- v[i];
-        // }
         
         return v;
     }
     
+    vector<int> NSR(vector<int> &nums, int n){
+        vector<int> ans;
+        stack<pair<int, int>> st;
+        int pseudoIndex = n;
+        
+        for(int i = n-1; i >= 0 ; i--){
+            while(!st.empty() && st.top().first >= nums[i]){
+                st.pop();
+            }
+            
+            if(st.empty()){
+                ans.push_back(pseudoIndex);
+            }
+            else{
+                ans.push_back(st.top().second);
+            }
+            
+            st.push({nums[i], i});
+        }
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
     
     int largestRectangleArea(vector<int>& heights) {
-        // find NSR index
-        vector<int> right = NSR(heights);
+    
+        int n = heights.size();
         
-        for(int i = 0 ; i < right.size() ; i++){
-            cout << right[i] << " " ;
+        vector<int> right = NSR(heights, n);
+        vector<int> left = NSL(heights, n);
+        
+        int area;
+        int maxArea = INT_MIN;
+        for(int i = 0 ; i < n ; i++){
+            //cout << right[i] << " , " <<left[i] << " " << " , h =  " << heights[i] << endl;
+            int width = (right[i] - left[i] -1);
+            area = heights[i] * width ;
+            maxArea = max(area, maxArea);
         }
-        // find NSL index
-        vector<int> left = NSL(heights);
-        cout << endl;
-        for(int i = 0 ; i < left.size() ; i++){
-            cout << left[i] << " " ;
-        }
-        
-        int maxArea = -1;
-        
-        // find width[] --> right - left -1
-        vector<int> width(heights.size(), -1);
-        vector<int> area(heights.size(), -1);
-        
-        for(int i = 0 ; i < heights.size() ; i++){
-            width[i] = right[i] - left[i] -1;
-            area[i] = width[i] * heights[i];
-            maxArea = max(area[i], maxArea);
-        }
-        // find area array
-        
-        // return maximum in area array
         
         return maxArea;
+        
     }
 };
