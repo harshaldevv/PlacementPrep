085d7d389e490f5a4e866b6c09314e17a0cb2042
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-15 03:02:16 +0530

    Time: 213 ms (61.42%), Space: 89.9 MB (21.62%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -19,19 +19,8 @@ class Solution {
         
         
         
-        // cout << "Before Reversal " << endl;
-        // for(int i = 0 ; i < n ; i++){
-        //     cout << ans[i] << " " ;
-        // }
-        
         // reverse
         reverse(ans.begin(), ans.end());
-        
-        // cout << endl;
-        // cout << "After Reversal " << endl;
-        // for(int i = 0 ; i < n ; i++){
-        //     cout << ans[i] << " " ;
-        // }
         
         
         return ans;
@@ -39,12 +28,62 @@ class Solution {
     }
     
     
+    vector<int> onePassSoln(vector<int>& nums, int n){
+        // basically find NGR_index and then do ngr[i] = ngr[i] - i
+        // end me reverse kr dena dhyan se lul
+        
+        vector<int> ans;
+        stack<pair<int,int>> st;
+        
+        for(int i = n-1; i >= 0 ; i--){
+            while(!st.empty() && st.top().first <= nums[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                ans.push_back(0);
+            }
+            else{
+                ans.push_back(st.top().second - i);
+            }
+            st.push({nums[i], i});
+        }
+        
+        //reverse
+        
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
+    }
+    
+    
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         
+        // one pass soln
+        
+        return onePassSoln(temperatures, temperatures.size());
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // my below soln is 2 pass , however it can be made into one pass
+        
         // get NSR index
         
-        vector<int> right = getNGRIndex(temperatures, temperatures.size());
+        vector<int> right = getNGRIndex(temperatures, temperatures.size()); 
+        
         
         
         // jahan bhi -1 hoga wahan toh 0 kar dena ngr array
