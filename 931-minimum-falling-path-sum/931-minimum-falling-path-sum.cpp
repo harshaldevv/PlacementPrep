e6c7d5c98570f788530ae09ffd97b29681f90bd5
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-14 23:03:26 +0530

    Time: 17 ms (62.60%), Space: 9.8 MB (93.71%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -6,9 +6,8 @@ public:
         int cols = matrix[0].size();
         
         int t[rows][cols+2];
-        //memset(t, -1, sizeof(-1));
-        
-        //initalization of first col and last col with +infinty
+    
+        //initalization of first col and last col with positive infinty
         
         for(int i = 0 ; i < rows; i++){
             t[i][0] = INT_MAX;
@@ -22,20 +21,17 @@ public:
         
         
         // normal looping
-        
         for(int i = 1 ; i < rows ; i++){
             for(int j = 1 ; j < cols + 1 ; j++){
-                //cout << t[i][j] << endl;
+                
                 t[i][j] = matrix[i][j-1] +  min( t[i-1][j-1] , min(t[i-1][j], t[i-1][j+1]));
-                // cout << i << j << endl;
+                
             }
         }
         
         
         
-        // looping in the last row;
-        
-
+        // looping in the last row for ans
         int mini = INT_MAX;
         for(int i = 1 ; i < cols+1 ; i++){
             mini = min(mini, t[rows-1][i]);
