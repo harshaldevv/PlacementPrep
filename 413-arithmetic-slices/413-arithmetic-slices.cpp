f906bcfcd134e0b082ad6ba2e15d36a68cfeb267
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-14 14:28:36 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (10.72%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+    int t[5000+1];
     
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
@@ -8,9 +8,51 @@ public:
             return 0;
         }
         
-        return dp(nums, n, 0);
+        // vector<int> t(n, -1);
+        memset(t, -1,  sizeof(t));
+        return dp2(nums, n, 0);
+        // return dp(nums, n, 0);
         
     }
+    int dp2(vector<int> &nums, int n, int res){
+        
+        if(n<3){
+            t[n] = 0;
+            return t[n];
+        }
+        
+        if(t[n] != -1){
+            return t[n];
+        }
+        
+        if(nums[n-1] - nums[n-2] == nums[n-2] - nums[n-3]){
+            res++;
+            t[n] = res+dp2(nums, n-1, res);
+        }
+        else{
+            t[n] = dp2(nums, n-1, 0);
+        }
+        
+        return t[n];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     int dp(vector<int> &nums, int n, int res){
         
