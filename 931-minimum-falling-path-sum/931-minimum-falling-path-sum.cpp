3de5f5de91aac5408c9bff4699ce335df36fe028
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-14 23:02:46 +0530

    Time: 12 ms (93.06%), Space: 9.9 MB (74.55%) - LeetHub

---
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        int t[rows][cols+2];
+        //memset(t, -1, sizeof(-1));
+        
+        //initalization of first col and last col with +infinty
+        
+        for(int i = 0 ; i < rows; i++){
+            t[i][0] = INT_MAX;
+            t[i][cols + 2 -1] = INT_MAX;
+        }
+        
+        // filling first row
+        for(int i = 0 ; i < cols ; i++){
+            t[0][i+1] = matrix[0][i];
+        }
+        
+        
+        // normal looping
+        
+        for(int i = 1 ; i < rows ; i++){
+            for(int j = 1 ; j < cols + 1 ; j++){
+                //cout << t[i][j] << endl;
+                t[i][j] = matrix[i][j-1] +  min( t[i-1][j-1] , min(t[i-1][j], t[i-1][j+1]));
+                // cout << i << j << endl;
+            }
+        }
+        
+        
+        
+        // looping in the last row;
+        
+
+        int mini = INT_MAX;
+        for(int i = 1 ; i < cols+1 ; i++){
+            mini = min(mini, t[rows-1][i]);
+        }
+
+        return mini;
+
+        
+        
+    }
+};
