3d67e72e64f7cf9ebba9978f646ea126ecea2a69
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-24 16:02:42 +0530

    Time: 4 ms (23.68%), Space: 5.9 MB (72.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,8 +2,33 @@ class Solution {
 public:
     int dp[100+1][100+1];
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return helper(m,n);
+        
+        int t[m+1][n+1];
+        memset(t, -1, sizeof(t));
+        
+        // filling base case
+        for(int i = 1 ; i < n+1 ; i++){
+            t[1][i] = 1;
+        }
+        
+        for(int i = 1 ; i < m+1 ; i++){
+            t[i][1] = 1;
+        }
+        
+        
+        for(int i = 2 ; i < m+1 ; i++){
+            for(int j = 2 ; j < n+1 ; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        
+        return t[m][n];
+        
+        
+        
+        
+        // memset(dp, -1, sizeof(dp));
+        // return helper(m,n);
     }
     
     int helper(int m, int n){
