fcd33e69c516a1f734244706743b8e5dfb528ce4
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-24 16:03:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[100+1][100+1];
+    //int dp[100+1][100+1];
     int uniquePaths(int m, int n) {
         
         int t[m+1][n+1];
@@ -31,18 +31,18 @@ public:
         // return helper(m,n);
     }
     
-    int helper(int m, int n){
-        if(m==1 || n ==1){
-            dp[m][n] = 1;
-            return 1;
-        }
+//     int helper(int m, int n){
+//         if(m==1 || n ==1){
+//             dp[m][n] = 1;
+//             return 1;
+//         }
         
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
+//         if(dp[m][n] != -1){
+//             return dp[m][n];
+//         }
         
-        dp[m][n] =  helper(m,n-1) + helper(m-1, n);
-        return dp[m][n];
+//         dp[m][n] =  helper(m,n-1) + helper(m-1, n);
+//         return dp[m][n];
         
-    }
+//     }
 };
