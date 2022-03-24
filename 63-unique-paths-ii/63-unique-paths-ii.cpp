668560e2d9b69dd6e4e1be646e2afca299882fa8
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-24 23:21:42 +0530

    Time: 3 ms (80.57%), Space: 7.6 MB (70.98%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,13 +2,60 @@ class Solution {
 public:
     int dp[100+1][100+1];
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
-        // normal recursion -- gives TLE --> 27/41 passed
         int rows = obstacleGrid.size();
         int cols = obstacleGrid[0].size();
-        memset(dp, -1, sizeof(dp));
+        //doing tabulation
+        int t[rows][cols];
+        memset(t, 0 , sizeof(t));
         
-        return helper(0, 0, obstacleGrid);
+        // filling first row
+        for(int i = 0 ; i < cols ; i++){
+            if(obstacleGrid[0][i] == 1){
+                t[0][i] = 0;
+                break;
+            }
+            else{
+                t[0][i] = 1;
+            }
+        }
+        
+        //filling first column
+        for(int i = 0 ; i < rows ; i++){
+            if(obstacleGrid[i][0] == 1){
+                t[i][0] = 0;
+                break;
+            }
+            else{
+                t[i][0] = 1;
+            }
+        }
+        
+        
+        for(int i = 1 ;  i< rows ; i++){
+            for(int j = 1 ; j< cols ; j++){
+                if(obstacleGrid[i][j] == 1){
+                    t[i][j]=0;
+                }
+                else{
+                    t[i][j] = t[i-1][j] + t[i][j-1];
+                }
+            }
+        }
+        
+        return t[rows-1][cols-1];
+        
+        
+        
+        
+        
+        
+//         //memoziation passes all test cases --> TC - O(m+n), SC - O(m*n)
+//         // normal recursion -- gives TLE --> 27/41 passed
+//         int rows = obstacleGrid.size();
+//         int cols = obstacleGrid[0].size();
+//         memset(dp, -1, sizeof(dp));
+        
+//         return helper(0, 0, obstacleGrid);
         
     }
     
