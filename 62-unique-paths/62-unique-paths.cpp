272695c179057d46d83592ce395245f6c2a089f7
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-03-24 16:32:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (51.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -3,30 +3,49 @@ public:
     //int dp[100+1][100+1];
     int uniquePaths(int m, int n) {
         
-        int t[m+1][n+1];
-        memset(t, -1, sizeof(t));
+        // tabulation space optimized -> TC - O(m+n), SC - O(n) aka number of columns
         
-        // filling base case
-        for(int i = 1 ; i < n+1 ; i++){
-            t[1][i] = 1;
-        }
-        
-        for(int i = 1 ; i < m+1 ; i++){
-            t[i][1] = 1;
-        }
-        
-        
-        for(int i = 2 ; i < m+1 ; i++){
-            for(int j = 2 ; j < n+1 ; j++){
-                t[i][j] = t[i-1][j] + t[i][j-1];
+        vector<long> t(n+1, 1);
+        for(int i = 1 ; i < m; i++){
+            for(int j = 1 ; j < n+1 ; j++){
+                t[j] += t[j-1];
             }
         }
         
-        return t[m][n];
+        
+        return t[n-1];
         
         
         
+        // tabulation -- TC - O(m+n), SC - O(m+n)
         
+        
+//         int t[m+1][n+1];
+//         memset(t, -1, sizeof(t));
+        
+//         // filling base case
+//         for(int i = 1 ; i < n+1 ; i++){
+//             t[1][i] = 1;
+//         }
+        
+//         for(int i = 1 ; i < m+1 ; i++){
+//             t[i][1] = 1;
+//         }
+        
+        
+//         for(int i = 2 ; i < m+1 ; i++){
+//             for(int j = 2 ; j < n+1 ; j++){
+//                 t[i][j] = t[i-1][j] + t[i][j-1];
+//             }
+//         }
+        
+//         return t[m][n];
+        
+        
+        
+        // recursion --> TC - O(2^[m+n]), SC - O(m+n) ->inbuilt recursive stack
+        
+        // then memoization --> TC - O(m+n), SC - O(m+n)
         // memset(dp, -1, sizeof(dp));
         // return helper(m,n);
     }
