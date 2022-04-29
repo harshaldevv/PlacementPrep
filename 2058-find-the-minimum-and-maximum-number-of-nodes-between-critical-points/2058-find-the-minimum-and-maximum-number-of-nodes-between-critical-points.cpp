fc397fe597a1cd5fac08bc0c0fae90864cee7877
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-04-30 01:35:13 +0530

    Time: 290 ms (56.27%), Space: 117.4 MB (31.50%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,30 +11,24 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> v(2, -1);
+        //Attempt 1;
         
-        vector<int> v(2,-1);
-
         if(head == nullptr || head->next == nullptr){
             return v;
         }
         
+        int criticalPointsCount = 0;
+        vector<int> criticalPointsData;
+        
+        
+        
         ListNode *prev = head;
         ListNode *curr = head->next;
-        
-        int index = 0;
-        int criticalPointsCount = 0;
-        vector<int> criticalPointIndex;
-        
-        int firstCriticalPoint = -1;
-        int prevCriticalPoint = -1;
-        int currCriticalPoint = -1;
-        
-        int minDiff = INT_MAX;
-        int maxDiff = INT_MIN;
+        int index = 1;
         
         while(curr != nullptr){
-            index++;
-            
+            index++; //index incrementer
             ListNode *nxt = curr->next;
             
             if(nxt == nullptr){
@@ -44,38 +38,25 @@ public:
             if( (curr->val < prev->val && curr-> val < nxt->val) || (curr->val > prev->val && curr->val > nxt->val)   ){
                 //local min  or local max
                 criticalPointsCount++;
-                
-                if(firstCriticalPoint == -1){
-                    firstCriticalPoint = index;
-                    prevCriticalPoint = index;
-                }
-                else{
-                    currCriticalPoint = index;
-                    minDiff = min(minDiff, currCriticalPoint - prevCriticalPoint);
-                    
-                    //updating prevCriticalPoint
-                    prevCriticalPoint = currCriticalPoint;
-                }
+                criticalPointsData.push_back(index);
             }
-
+            
             prev = curr;
             curr = curr->next;
         }
-        
         
         if(criticalPointsCount < 2){
             return v; // v = [-1, -1]
         }
         
-        // v[0] --> min_dist;
-        // v[1] --> max_dist
+        v[0] = INT_MAX; // min-Dist;
+        for(int i = 0 ; i < criticalPointsData.size() -1 ; i++){
+            v[0] = min(v[0], criticalPointsData[i+1] - criticalPointsData[i] );
+        }
         
-        v[0] = minDiff;
         
-        v[1] = currCriticalPoint - firstCriticalPoint;
-        
+        v[1] =  criticalPointsData.back() - criticalPointsData.front() ;//max-Dist
         
         return v;
-        
     }
 };
