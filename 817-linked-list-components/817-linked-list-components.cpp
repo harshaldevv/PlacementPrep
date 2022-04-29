aae446e036298c035394a68226a6b7fac596255d
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-04-29 18:50:12 +0530

    Time: 62 ms (38.25%), Space: 21.5 MB (60.08%) - LeetHub

--- 817-linked-list-components/817-linked-list-components.cpp
+++ 817-linked-list-components/817-linked-list-components.cpp
@@ -11,11 +11,22 @@
 class Solution {
 public:
     int numComponents(ListNode* head, vector<int>& nums) {
+        
+        
+        
+        // attempt 1 --> https://leetcode.com/problems/linked-list-components/discuss/123842/C%2B%2BJavaPython-Easy-and-Concise-Solution-with-Explanation
+        
+        // count "tail" of connected components  <-- VERY CLEVER IDEA
+        
+        // from comments (of TulakHord) , see below , why the "if" condn is working
+        // To do this, we just need to count tails of red parts."
+        // tail of red parts = the current node is in setG and the next is not in setG
+        
         unordered_set<int> s (nums.begin(), nums.end());
         
         ListNode *curr = head;
         long ans = 0;
-        // count "tail" of connected components
+        
         
         while(curr != nullptr){
             if(s.count(curr->val) &&(curr->next == nullptr || !s.count(curr->next->val) )){
