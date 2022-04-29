b512d025f643bc2c18d4d349b71733a0734dfb8d
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-04-30 00:47:22 +0530

    Time: 272 ms (69.11%), Space: 117.4 MB (31.80%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -22,12 +22,6 @@ public:
         ListNode *curr = head->next;
         
         int index = 0;
-        
-        int minDist = INT_MAX;
-        int maxDist = INT_MIN;
-        int minIndex;
-        int maxIndex;
-        
         int criticalPointsCount = 0;
         vector<int> criticalPointIndex;
         
@@ -37,43 +31,39 @@ public:
             ListNode *nxt = curr->next;
             
             if(nxt == nullptr){
-                cout << " Here 2" << endl;
                 break;
             }
             
             if(curr->val < prev->val && curr-> val < nxt->val){
+                //local min
                 criticalPointsCount++;
-                criticalPointIndex.push_back(index);
-                
+                criticalPointIndex.push_back(index);  
             }
             else if(curr->val > prev->val && curr->val > nxt->val){
+                //local max
                 criticalPointsCount++;
                 criticalPointIndex.push_back(index);
             }
+            
             prev = curr;
             curr = curr->next;
-
         }
-        
-        cout << " Here " << endl;
         
         
         if(criticalPointsCount < 2){
-            // v[0] = -1;
-            // v[1] = -1;
-            return v;
+            return v; // v = [-1, -1]
         }
         
         // v[0] --> min_dist;
         // v[1] --> max_dist
         
-        v[1] = criticalPointIndex.back() - criticalPointIndex.front();  
-        //max dist will always be b/w the first and the last critical point
-        
         v[0] = INT_MAX;
         for(int i = 0 ; i < criticalPointIndex.size() -1 ; i++){
             v[0] = min(v[0], criticalPointIndex[i+1] - criticalPointIndex[i]);
         }
+        
+        v[1] = criticalPointIndex.back() - criticalPointIndex.front();  
+        //max dist will always be b/w the first and the last critical point
         
         return v;
         
