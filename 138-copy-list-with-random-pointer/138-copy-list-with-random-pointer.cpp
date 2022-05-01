4d5a53ca86fd75c9285781071db598eba5c8c3c9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-02 01:30:27 +0530

    Time: 14 ms (41.12%), Space: 11.3 MB (63.75%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,31 +17,61 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> mp;
+        
+        if(head == nullptr){
+            return nullptr;
+        }
         
         Node *curr = head;
         
-        //putting all nodes in hashmap
         while(curr != nullptr){
-            mp[curr] = new Node(curr->val);
-            curr = curr->next;
+            Node *t = new Node(curr->val);
+            t->next = curr->next;
+            curr->next = t;
+            curr = curr->next->next;
         }
         
-        //
+        // return head->next;
+        
+        curr = head;
+        while(curr != nullptr){
+            Node *t = curr->next;
+            Node *rand = curr->random;
+            
+            if(rand != nullptr){
+                t->random = rand->next;
+            }
+            else{
+                t->random = nullptr;
+            }
+            
+            
+            curr = curr->next->next;
+        }
+        
+        // reweaving the list
+        
+        Node *answer = head->next;
+        
+        Node *clonedList = head->next;
         curr = head;
         
         while(curr != nullptr){
-            mp[curr]->next = mp[curr->next];
-            mp[curr]->random = mp[curr->random];
+            curr->next = curr->next->next;
+            
+            if(clonedList->next != nullptr){
+                clonedList->next = clonedList->next->next;
+                clonedList =  clonedList->next;
+            }
+            else{
+                clonedList->next =  nullptr;
+            }
+            
             curr = curr->next;
         }
         
-        return mp[head];
         
-        
-        
-        
-        
+        return answer;
         
     }
 };
