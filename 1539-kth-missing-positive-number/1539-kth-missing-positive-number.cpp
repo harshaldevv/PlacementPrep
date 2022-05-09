eb3e5fe61f26ae2fd68fabef50bc650cb7ec79a6
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-10 00:27:26 +0530

    Time: 3 ms (90.05%), Space: 9.5 MB (67.51%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -6,13 +6,22 @@ public:
             return k;
         }
         
-        for(int i = 0 ; i < arr.size() ; i++){
-            if(arr[i] <= k){
-                k++;
+        int start = 0;
+        int end = arr.size() -1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(arr[mid] - mid -1 >= k){
+                // go left
+                end = mid-1;
+            }
+            else{
+                start = mid +1;
             }
         }
         
-        return k;
+        return start + k;
         
     }
 };
