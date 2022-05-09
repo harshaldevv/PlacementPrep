bb1de3d41725b2ef4dd36bb27493222dc00f18ae
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-09 18:02:26 +0530

    Time: 33 ms (11.01%), Space: 16 MB (27.11%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
+        // find ceil of a number/alphabet
         
+        int n = letters.size();
         int start = 0;
-        int end = letters.size()-1;
+        int end = n-1;
+        
         char res = '#';
         
         while(start <= end){
-            int mid = start + (end - start)/2;
+            int mid = start + (end-start)/2;
             
-            if(letters[mid] <= target){  
-                // letter[mid] < target and then doing start = mid +1 was giving wrong ans
-                // res = letters[mid];
-                // end = mid -1;
-                start = mid +1;
-                
+            if(letters[mid] == target){
+                //go right 
+                start = mid+1;
+            }
+            else if(letters[mid] > target){
+                //go left
+                res = letters[mid];
+                end = mid-1;
             }
             else{
-                res = letters[mid];
-                end = mid -1;
+                start = mid+1;
             }
         }
+        
         
         if(res == '#'){
             res = letters[0];
         }
         
         return res;
-        
     }
 };
