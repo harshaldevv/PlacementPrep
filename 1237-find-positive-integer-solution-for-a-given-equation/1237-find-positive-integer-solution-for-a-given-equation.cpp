2e99e8830ad6cc15781c963744c3a43d53ae8f99
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-09 20:36:51 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.51%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
@@ -13,30 +13,61 @@
 class Solution {
 public:
     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        // soln went from O(x*y) to O(xlogy) ---> to O(x+y)
         vector<vector<int>> v;
-        for(int i = 1 ; i <= 1000 ; i++){
-            // do binary search here 
-            int start = 1;
-            int end = 1000 +1;
-            
-            while(start <= end){
-                int mid = start + (end-start)/2;
-                
-                int myZ = customfunction.f(i,mid);
-                if(myZ == z){
-                    v.push_back({i, mid});
-                    break;
-                }
-                else if(myZ > z){
-                    // go left
-                    end = mid -1;
-                }
-                else{
-                    start = mid +1;
-                }
+        
+        int x = 1;
+        int y = 1000;
+        
+        while ( x <= 1000 && y >0){
+            int myZ = customfunction.f(x,y);
+            if(myZ == z){
+                v.push_back({x,y});
+                x++;
+                y--;
+            }
+            else if(myZ > z){
+                y--;
+            }
+            else{
+                x++;
             }
         }
-        
         return v;
+        
+        
+        
+//         vector<vector<int>> v;
+//         for(int i = 1 ; i <= 1000 ; i++){
+            
+//             // do binary search here 
+//             int start = 1;
+            
+//             int end = 1000+1;
+            
+//             if(!v.empty()){  // a teensy bit optimizaiton --> see note  
+//                 // cout << " here " << endl;
+//                 end = v.back()[1] +1;
+//             }
+            
+//             while(start <= end){
+//                 int mid = start + (end-start)/2;
+                
+//                 int myZ = customfunction.f(i,mid);
+//                 if(myZ == z){
+//                     v.push_back({i, mid});
+//                     break;
+//                 }
+//                 else if(myZ > z){
+//                     // go left
+//                     end = mid -1;
+//                 }
+//                 else{
+//                     start = mid +1;
+//                 }
+//             }
+//         }
+        
+//         return v;
     }
 };
