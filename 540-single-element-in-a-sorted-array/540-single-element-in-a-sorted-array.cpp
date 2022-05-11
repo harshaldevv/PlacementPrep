08dcbb6c40cdb6b0a85d919bce4bb19777687b84
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-11 13:53:45 +0530

    Time: 32 ms (61.51%), Space: 22.2 MB (91.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        
-        for(auto x : nums){
-            ans^=x;
+        // linear search
+        for(int i= 0 ; i < nums.size()-1;i+=2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
         }
-        return ans;
+        return nums[nums.size()-1]; // warna phir ab last element hu hoga hamara ans
+        
+        
+//         // XOR SOLUTION
+//         // We can use the property of xor operation in this problem. We know that a ⊕ a = 0, 
+//         // i.e, two same numbers when xor-ed, cancels each other out. In our case, every element 
+//         // occurs twice except the one. Thus, if we xor all the elements from nums, every 
+//         // element will cancel each other out except the required answer.
+//         int ans = 0;
+        
+//         for(auto x : nums){
+//             ans^=x;
+//         }
+//         return ans;
     }
 };
