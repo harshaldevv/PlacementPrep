04c170d768c1ebf4040c3ffad3b42d23ad45f8c4
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-13 16:26:56 +0530

    Time: 428 ms (6.93%), Space: 58 MB (9.67%) - LeetHub

---
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(vector<int> &pos, int magnets, int myDist){
+        // magnets --> total magnets i have
+        
+        int m = 1;
+        int start = pos[0];
+        for(int i = 1 ; i < pos.size() ; i++){
+            if(pos[i] - start >= myDist){
+                m++;
+                start = pos[i];
+            }
+            
+            if(m >= magnets){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    int maxDistance(vector<int>& position, int m) {
+        int start = 1;
+        int end = 1000000000;
+        
+        sort(position.begin(), position.end());
+        
+        long res = -1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(isValid(position, m, mid) == true){
+                res = mid;
+                //find a larger dist
+                start = mid +1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        
+        return res;
+        
+    }
+};
