2f1f4c523b64d71ae1cc8effbe1eb146e11078ff
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-21 20:31:29 +0530

    Time: 31 ms (58.71%), Space: 13.4 MB (79.87%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     bool isBipartiteHelper(int currVertex, vector<vector<int>> &adjList, vector<int> &color){
         
-        cout << "color of curr = " << color[currVertex] << endl;
+        
         
         if(color[currVertex] == -1){ 
             color[currVertex] = 1;
         }
         
-        cout << "color of curr  (after if) = " << color[currVertex] << endl;
+        
         
         for(auto nextVertex : adjList[currVertex]){
             if(color[currVertex] == color[nextVertex]){
-                cout << "curr vertex --> " << currVertex << endl;
-                cout << "nextVertex nextVertex --> " << nextVertex << endl;
-                cout << "color curr vertex -> " << color[currVertex]  << endl;
-                cout << "color nextVertex -> " << color[nextVertex]  << endl;
-                
                 return false;
             }
             else if(color[nextVertex] == -1){
@@ -34,6 +29,9 @@ public:
         int vertices = graph.size();        
         
         vector<int> color(vertices, -1);
+        // -1 --> unvisited uncolored
+        // 0 --> red
+        // 1 --> blue
         
         for(int i = 0 ; i < vertices ; i++){
             if(color[i] == -1){
