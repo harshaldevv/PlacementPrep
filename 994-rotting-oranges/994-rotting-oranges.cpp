73e876d722840f18a27fa28bc28fa72c3399ccb0
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-23 01:23:16 +0530

    Time: 15 ms (28.77%), Space: 13.1 MB (80.03%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -18,10 +18,20 @@ public:
                 }
             }
         }
-        int dx[4] = {0,0, -1,+1};   // l, r, t, d
-        int dy[4] = {-1, +1, 0, 0};
+        
+        int dx[4] = {0,0, -1,+1};   // left, right, top, down
+        int dy[4] = {-1, +1, 0, 0}; // left, right, top, down 
         
         int t = -1; // time
+        
+        /***
+        we increment time whenever we get rotten oranges. 
+        The ones which are rotten when they are encountered then also we 
+        will increment, but it should be zero at that point,
+        so initializing with -1 ensures that at that point in time it 
+        will be zero
+        ***/
+        
         while(!q.empty()){
             int sz = q.size();
             while(sz--){
@@ -45,11 +55,14 @@ public:
                     }
                 }                
             }
+            // cout << "at t = " << t << " , fresh oranges left = " << fresh << endl;
             t++;
+            
         }
         
         if(fresh == 0){
-            return t == -1 ? 0 : t ;
+            return t == -1 ? 0 : t ; 
+            //we initialised with -1, so if there were no oranges it'd take 0 mins.
         }
         else{
             return -1;
