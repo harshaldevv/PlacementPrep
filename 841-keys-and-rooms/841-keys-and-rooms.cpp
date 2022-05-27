5649af69224ee43c9b37127689b5ea13a7cb09b0
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-28 01:27:21 +0530

    Time: 13 ms (49.88%), Space: 10.4 MB (79.59%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(int i, vector<vector<int>> &rooms, vector<bool> &vis){
+        if(vis[i]){ // if already visited
+            return;
+        }
+        
+        vis[i] = true;
+        
+        for(auto x : rooms[i]){
+            dfs(x, rooms, vis);
+        }
+        
+        return ;
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        
+        vector<bool> vis(n,false);
+    
+        dfs(0, rooms, vis);
+        
+        for(int i = 0 ; i < n ; i++){
+            // cout << vis[i] << " , " ;
+            if(!vis[i]){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
