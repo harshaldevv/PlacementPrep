c7c9b280bbbcc4f5b7626cc9094e2eb5663ded51
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-28 15:16:13 +0530

    Time: 14 ms (73.58%), Space: 9.5 MB (67.15%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,85 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid, int i , int j){
-        if( i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
-            // invalid coordinates
-            return ;
+    
+    bool dfs(vector<vector<int>> &grid, int i, int j){
+        if(i <0  || j < 0 || i >= grid.size() || j>= grid[0].size() ){
+            return false ;
         }
-        
         if(grid[i][j] != 0){
-            return; // either land or already visited,
+            return true; // either land or already visited
         }
         
-        grid[i][j]  = 2; // marking visited
+        grid[i][j] = 2; // marking land as viisted
         
+        bool top = dfs(grid, i-1, j); 
+        bool down = dfs(grid, i+1, j);
+        bool left = dfs(grid, i, j-1);
+        bool right = dfs(grid, i, j+1);
         
-        int dx[] = {1,0,-1,0,1}; // hmmm smart
-        
-        for(int k = 0 ; k < 4 ; k++){
-            dfs(grid, i + dx[k], j + dx[k+1]);
-        }
-     
-        return ;
+        return top && down && left && right;
         
     }
-    
     int closedIsland(vector<vector<int>>& grid) {
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                cout << grid[i][j] << " " ;
-            }
-            cout << endl;
-        }
-        
-        // sare boundaries pe jao and sare 0 connected (boundary) walo ko "water or visited" bana do
-        for(int i = 0 ; i < n ; i++){
-            if(grid[i][0] == 0){
-                dfs(grid, i, 0);
-                // dfs(grid, )
-            }
-            if(grid[i][m -1] == 0){
-                dfs(grid, i , m-1);
-            }
-        }
-        
-        for(int j = 0 ; j < m ; j++ ){
-            if(grid[0][j] == 0){
-                dfs(grid, 0, j);
-            }
-            if(grid[n-1][j] == 0){
-                dfs(grid, n-1, j);
-            }
-        }
-        // printing matrix
-        cout << endl;
-        cout << endl;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
-                cout << grid[i][j] << " " ;
-            }
-            cout << endl;
-        }
-        
-        // uske baad do normal dfs 
-        // count no of islands basically
         
         int count = 0;
         
-        for(int i = 0 ; i< grid.size();i++){
+        for(int i = 0 ; i < grid.size()  ; i++){
             for(int j = 0 ; j < grid[0].size() ; j++){
                 if(grid[i][j] == 0){
-                    dfs(grid, i, j);
-                    count++;
+                    if(dfs(grid, i, j)){
+                        //cout << " i , j = " << i << " , " << j << endl;
+                        count++;
+                    }
+                    
                 }
             }
         }
         
         return count;
-        
         
     }
 };
