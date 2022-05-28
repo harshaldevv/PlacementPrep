a346e9e4712a12d13594bec63b68d11fe792927e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-28 14:01:01 +0530

    Time: 15 ms (67.87%), Space: 9.5 MB (36.38%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    bool dfs(vector<vector<int>> &grid, int i, int j){
+        if(i <0  || j < 0 || i >= grid.size() || j>= grid[0].size() ){
+            return false ;
+        }
+        if(grid[i][j] != 0){
+            return true;
+        }
+        
+        grid[i][j] = 2; // marking water as viisted
+        
+        bool top = dfs(grid, i-1, j); 
+        bool down = dfs(grid, i+1, j);
+        bool left = dfs(grid, i, j-1);
+        bool right = dfs(grid, i, j+1);
+        
+        return top && down && left && right;
+        
+    }
+    int closedIsland(vector<vector<int>>& grid) {
+        
+        int count = 0;
+        
+        for(int i = 0 ; i < grid.size()  ; i++){
+            for(int j = 0 ; j < grid[0].size() ; j++){
+                if(grid[i][j] == 0){
+                    if(dfs(grid, i, j)){
+                        //cout << " i , j = " << i << " , " << j << endl;
+                        count++;
+                    }
+                    
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+};
