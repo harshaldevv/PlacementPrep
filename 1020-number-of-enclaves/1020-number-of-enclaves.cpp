bc1d2224944a3c36eda903fb4106125b064dff03
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-29 12:42:34 +0530

    Time: 143 ms (9.49%), Space: 21.8 MB (58.38%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int ans = 0;
+
     void dfs(vector<vector<int>> &grid, int i, int j){
         if( i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
             return;
@@ -10,59 +10,24 @@ public:
         }
         
         grid[i][j] = 2;
-        ans++;
         
-        int dx[] = {1,0,-1,0,1};
+        int dir[] = {1,0,-1,0,1}; // dir array
         
         for(int k = 0 ; k < 4 ; k++){
-            dfs(grid, i + dx[k], j + dx[k+1]);
+            dfs(grid, i + dir[k], j + dir[k+1]);
         }
         
         return ;
     }
     
-    void dfs2(vector<vector<int>> &grid, int i, int j){
-        if(i<0 || j < 0 || i>= grid.size() || j >= grid[0].size()){
-            return ;
-        }
-        
-        if(grid[i][j] != 1){
-            return ;
-        }
-        
-        grid[i][j] = 2;
-        ans++;
-        return ;
-        
-        
-    }
     int numEnclaves(vector<vector<int>>& grid) {
         
 
         int m = grid.size();
         int n = grid[0].size();
-        cout << " m =  " << m <<  endl;
         
-//         // first col, last col
-//         for(int i = 0 ; i < m ; i++){
-//             if(grid[i][0] == 1){
-//                 dfs(grid, i, 0);
-//             }
-//             if(grid[i][n-1] == 1){
-//                 dfs(grid, i, m-1);
-//             }
-//         }
-        
-//         // top row, last row
-//         for(int j = 0 ; j < n ; j++){
-//             if(grid[0][j] == 1){
-//                 dfs(grid, 0, j);
-//             }
-//             if(grid[m-1][j] == 1){
-//                 dfs(grid, n-1, j);
-//             }
-//         }
-        
+        // we fill/visit all those cells with 1 which are at boundary or
+        // are somehow connected to a boundary cell which has value = 1
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if( i== 0 || j == 0 || i == m-1 || j == n-1){
@@ -71,8 +36,14 @@ public:
             }
         }
         
+        // now that we've visited and marked those cells,
+        //we'll just count the remaining number of land with cell value = 1
+        // koi dfs nahi lagana yahan
+        
+        // had it been ki count the number of components, toh dfs lagata 
+        // like in --> https://leetcode.com/submissions/detail/708895770/
+            
         int count = 0;
-        //ans = 0;
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(grid[i][j] == 1){
