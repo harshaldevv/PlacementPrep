065cb408c7f4290a3b8e55851e5575cedaae3e84
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-29 12:57:54 +0530

    Time: 88 ms (42.90%), Space: 21.7 MB (58.38%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,58 +1,42 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<int>> &grid, int i, int j){
-        if( i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
-            return;
+    int dfs2(vector<vector<int>> &grid, int i, int j){
+        if( i <0 || j < 0 || i>= grid.size() || j >= grid[0].size() ){
+            return -1;
         }
+        
         if(grid[i][j] != 1){
-            return ; // either visited or sea
+            return 0;
         }
         
         grid[i][j] = 2;
         
-        int dir[] = {1,0,-1,0,1}; // dir array
+        int a = dfs2(grid, i+1, j);
+        int b = dfs2(grid, i, j-1);
+        int c = dfs2(grid, i-1, j);
+        int d = dfs2(grid, i, j+1);
         
-        for(int k = 0 ; k < 4 ; k++){
-            dfs(grid, i + dir[k], j + dir[k+1]);
+        if(a==-1 || b == -1 || c == -1 || d == -1){
+            return -1;
         }
         
-        return ;
+        return 1 + a+b+c+d;
     }
-    
     int numEnclaves(vector<vector<int>>& grid) {
-        
-
         int m = grid.size();
         int n = grid[0].size();
-        
-        // we fill/visit all those cells with 1 which are at boundary or
-        // are somehow connected to a boundary cell which has value = 1
+        int res = 0;
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if( i== 0 || j == 0 || i == m-1 || j == n-1){
-                    dfs(grid, i, j);
-                }
-            }
-        }
-        
-        // now that we've visited and marked those cells,
-        //we'll just count the remaining number of land with cell value = 1
-        // koi dfs nahi lagana yahan
-        
-        // had it been ki count the number of components, toh dfs lagata 
-        // like in --> https://leetcode.com/submissions/detail/708895770/
-            
-        int count = 0;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+            for(int j = 0 ; j< n ; j++){
                 if(grid[i][j] == 1){
-                    count++;
-                    
+                    int temp = dfs2(grid, i, j);
+                    if(temp > 0 ){
+                        res+=temp;
+                    }
                 }
             }
         }
-        return count;
+        return res;
         
     }
 };
