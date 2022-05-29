ff94e5704809c4c330c50bca692387f3864411bf
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-29 12:59:20 +0530

    Time: 81 ms (53.68%), Space: 21.7 MB (58.38%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int dfs2(vector<vector<int>> &grid, int i, int j){
+        // though this approach is rather non intutive ish
+        // the flood fill one is more intutive 
+        
+        // this uses the leetcode #1254 and 
+        // python code in this post's comment -->
+        // https://leetcode.com/problems/number-of-enclaves/discuss/266168/Easy-Java-DFS-6ms-solution
         if( i <0 || j < 0 || i>= grid.size() || j >= grid[0].size() ){
             return -1;
         }
