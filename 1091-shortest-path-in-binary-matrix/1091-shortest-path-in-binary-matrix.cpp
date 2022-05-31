3900160a4710144148dd6b0ccc0741eb3f2bc22e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-05-31 12:03:01 +0530

    Time: 109 ms (47.62%), Space: 19.5 MB (65.59%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,50 +1,64 @@
 class Solution {
 public:
-    
-    bool isValid(vector<vector<int>>& grid, int i, int j, int n, vector<vector<bool>>& visited){
-        
-        return (i>=0 and i<n and j>=0 and j<n and grid[i][j]==0 and !visited[i][j]);
-        
+    bool isValid(int i, int j, int n){
+        if(i< 0 || j < 0 || i >= n || j >= n){
+            // invalid coordinates
+            return false;
+        }
+        return true;
     }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
         int n = grid.size();
-        vector<vector<bool>> visited(n, vector<bool> (n, false));
-        queue<pair<int, int>> q;
-        int ans = 0;
-        int nodesPushed;
+        vector<vector<bool>> vis(n, vector<bool>(n, false));
         
-        if(grid[0][0] == 0){
-            q.push({0, 0});
-            visited[0][0] = true;
+        int x = 0;
+        int y = 0;
+        if(grid[x][y] == 1){
+            return -1;
+        } 
+        
+        int tx = n-1;
+        int ty = n-1;
+        
+        if(x== tx && y == ty){
+            return 1;
         }
-                
+        
+        int dir[8][2] = { {-1,0}, {1,0}, {0, -1}, {0, 1}, {-1, +1}, {+1, +1}, {+1, -1}, {-1,-1}   };
+        
+        queue<pair<int, int>> q;
+    
+        q.push({x,y});
+        vis[x][y] = true;
+        
+        int steps = 0;
+        
         while(!q.empty()){
-            
-            nodesPushed = q.size();
-            ans++;
-            
-            for(int cnt = 0; cnt < nodesPushed; cnt++){
+            int sz = q.size();
+            // cout << "size = " << sz << endl;
+            steps++;
+            while(sz--){
                 
-                pair<int, int> frontNode = q.front();
+                auto front = q.front();
                 q.pop();
-            
-                int i = frontNode.first, j = frontNode.second;
-
-                if(i==n-1 and j==n-1) return ans;
-
-                for(int k = i - 1; k <= i + 1 ; k++){
-                    for(int l = j - 1; l <= j + 1; l++){
-                        if(isValid(grid, k, l, n, visited)){
-                            q.push({k, l});
-                            visited[k][l] = true;
-                        }
-                    }
-                }                
+                int currx = front.first;
+                int curry = front.second;
                 
-            }            
-            
+                if(currx == tx && curry == ty){
+                    return steps;
+                }
+                
+                for(int k = 0 ; k < 8 ; k++){
+                    int newx = currx + dir[k][0];
+                    int newy = curry + dir[k][1];
+                    
+                    if(isValid(newx, newy, n) && !vis[newx][newy] && grid[newx][newy] == 0){
+                        q.push({newx, newy});
+                        vis[newx][newy] = true;
+                    }
+                }
+            }
         }
         
         return -1;
