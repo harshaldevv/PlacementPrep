38b3facd15dee2b7a9d0d136a64cc85c91799e2d
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-05 10:44:14 +0530

    Time: 3 ms (96.12%), Space: 7.1 MB (86.55%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,78 +1,119 @@
 class Solution {
 public:
     
-    bool possible(int r, int c, int n , vector<string> &board){
+//     bool possible(int r, int c, int n , vector<string> &board){
         
-        // check left side (column right to left)
-        for(int i = c ; i >= 0 ; i--){
-            if(board[r][i] == 'Q'){
-                return false;
-            }    
-        }
+//         // check left side (column right to left)
+//         for(int i = c ; i >= 0 ; i--){
+//             if(board[r][i] == 'Q'){
+//                 return false;
+//             }    
+//         }
         
-        //check left upper diagonal
-        int tempR = r;
-        int tempC = c;
-        while(tempR>=0 && tempC>=0){
-            if(board[tempR][tempC] == 'Q'){
-                return false;
-            }
-            tempR--;
-            tempC--;
-        }
+//         //check left upper diagonal
+//         int tempR = r;
+//         int tempC = c;
+//         while(tempR>=0 && tempC>=0){
+//             if(board[tempR][tempC] == 'Q'){
+//                 return false;
+//             }
+//             tempR--;
+//             tempC--;
+//         }
         
         
-        // check left lower diagonal
-        tempR = r;
-        tempC = c;
-        while(tempR<n && tempC>=0){
-            if(board[tempR][tempC] == 'Q'){
-                return false;
-            }
-            tempR++;
-            tempC--;
-        }
+//         // check left lower diagonal
+//         tempR = r;
+//         tempC = c;
+//         while(tempR<n && tempC>=0){
+//             if(board[tempR][tempC] == 'Q'){
+//                 return false;
+//             }
+//             tempR++;
+//             tempC--;
+//         }
         
-        return true;
+//         return true;
 
         
         
-    }
+//     }
     
-    void recursion(int n, int currCol, vector<vector<string>> &ans, vector<string> &board){
-        if(currCol > n){
+//     void recursion(int n, int currCol, vector<vector<string>> &ans, vector<string> &board){
+//         if(currCol > n){
+//             return ;
+//         }
+        
+//         if(currCol == n){
+//             ans.push_back(board) ;
+//             return ;
+//         }
+        
+//         for(int currRow = 0 ; currRow < n ; currRow++){
+//             // iterating rows
+//             // cout << "currRow = " << currRow << endl;
+//             if(possible(currRow, currCol, n, board)){
+//                 board[currRow][currCol] = 'Q';
+//                 recursion(n, currCol +1, ans, board);
+//                 board[currRow][currCol] = '.';
+//             }
+//         }
+//         return ;
+//     }
+    
+    
+    void recursion(int n, int currcol, vector<vector<string>> &ans, vector<string> &board, vector<int> &left, vector<int> &up, vector<int> &low){
+        
+        if(currcol > n){
+            return;
+        }
+        if(currcol == n){
+            ans.push_back(board);
             return ;
         }
         
-        if(currCol == n){
-            ans.push_back(board) ;
-            return ;
-        }
-        
-        for(int currRow = 0 ; currRow < n ; currRow++){
-            // iterating rows
-            // cout << "currRow = " << currRow << endl;
-            if(possible(currRow, currCol, n, board)){
-                board[currRow][currCol] = 'Q';
-                recursion(n, currCol +1, ans, board);
-                board[currRow][currCol] = '.';
+        for(int currrow = 0 ; currrow < n ; currrow++){
+            if(left[currrow] == 0 && up[currrow + currcol] == 0 && low[n-1 + currcol - currrow] == 0){ 
+                // this if condn replaces the possible() function
+                
+                board[currrow][currcol] = 'Q';
+                left[currrow] = 1;
+                up[currrow + currcol] = 1;
+                low[n-1 + currcol - currrow] = 1;
+                
+                recursion(n, currcol+1, ans, board, left, up, low);
+                
+                board[currrow][currcol] = '.';
+                left[currrow] = 0;
+                up[currrow + currcol] = 0;
+                low[n-1 + currcol - currrow] = 0;
+                
             }
         }
         return ;
+        
     }
+    
     vector<vector<string>> solveNQueens(int n) {
         
         
         vector<vector<string>> ans;
         vector<string> board(n,  string (n, '.') );
         
-        // string s(n, '.');
-        // for(int i = 0 ; i < n ; i++){
-        //     board[i] = s;
-        // }
         
-        // int col = 0;
-        recursion(n, 0, ans, board);
+        // recursion(n, 0, ans, board);
+        
+        //optimization 
+        // --> to avoid calling possible() again and again
+        // as possible is O(3n) and for it runs n times in recursion() func
+        // therefore leading to O(n^2) complexity
+        
+        // to lower this complexity to O(n) , we are doing this opitization
+        vector<int> left(n,0);
+        vector<int> upper(2*n-1,0); // upper diagonal
+        vector<int> lower(2*n-1,0); // lower diagonal
+        
+        recursion(n,0, ans, board, left, upper, lower);
         
         return ans;
         
