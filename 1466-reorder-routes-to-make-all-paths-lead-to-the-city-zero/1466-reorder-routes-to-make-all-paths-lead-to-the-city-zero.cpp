fd03cc14b8d7c67b5bf6ff075adf6f37140ef309
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-05 14:07:34 +0530

    Time: 615 ms (44.65%), Space: 116.2 MB (48.11%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    
+//     void dfs_old(int curr, vector<vector<pair<int,bool>>> &adjList, int n, vector<bool> &vis, int &ans){
+//         if(vis[curr]){
+//             return;
+//         }
+        
+//         vis[curr] = true;
+        
+//         for(auto x : adjList[curr]){
+//             if(!vis[x.first]){
+//                 dfs(x.first, adjList, n, vis, ans);
+//                 if(x.second == true){
+//                     cout << "here" << endl;
+//                     ans++;
+//                 }
+//             }
+//         }
+        
+//         return ;
+//     }
+    
+    void dfs(int curr, vector<vector<int>> &adjlist, vector<bool> &vis, int &ans){
+        if(vis[curr]){
+            return;
+        }
+        
+        vis[curr] = true;
+        for(auto x :  adjlist[curr]){
+            if(!vis[abs(x)]){
+                dfs(abs(x), adjlist, vis, ans);
+                if(x > 0){
+                    ans++;
+                }
+            }
+        }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+        // make graph (adjList)
+        vector<vector<int>> adjList(n);
+        for(auto x :connections){
+            int u = x[0];
+            int v = x[1];
+            adjList[u].push_back(v);
+            adjList[v].push_back(-u);
+        }
+        vector<bool> vis(n, false);
+        int ans = 0;
+        dfs(0, adjList, vis, ans);
+        return ans;
+        
+        
+//         //adjList
+//         vector<vector<pair<int, bool>>> adjList(n);
+        
+//         for(auto x : connections){
+//             int u = x[0];
+//             int v = x[1];
+
+//             adjList[u].push_back({v, false}); 
+//             adjList[v].push_back({u, true}); // true represents that this edges needs to be reversed
+//         }
+        
+//         vector<bool> vis(n, false);
+//         int ans = 0;
+//         dfs_old(0, adjList, n, vis, ans);
+        
+//         return ans;
+        
+        
+        
+    }
+};
