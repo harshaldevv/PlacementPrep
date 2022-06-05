ef19bf0acde1bdbfbbad714b7ac35dab72b0ed1a
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-05 14:40:57 +0530

    Time: 585 ms (49.63%), Space: 117.5 MB (41.92%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     
-//     void dfs_old(int curr, vector<vector<pair<int,bool>>> &adjList, int n, vector<bool> &vis, int &ans){
-//         if(vis[curr]){
-//             return;
-//         }
+    void dfs_old(int curr, vector<vector<pair<int,bool>>> &adjList, int n, vector<bool> &vis, int &ans){
+        if(vis[curr]){
+            return;
+        }
         
-//         vis[curr] = true;
+        vis[curr] = true;
         
-//         for(auto x : adjList[curr]){
-//             if(!vis[x.first]){
-//                 dfs(x.first, adjList, n, vis, ans);
-//                 if(x.second == true){
-//                     cout << "here" << endl;
-//                     ans++;
-//                 }
-//             }
-//         }
+        for(auto x : adjList[curr]){
+            if(!vis[x.first]){
+                // cout << "x.first = " << x.first <<endl;
+                dfs_old(x.first, adjList, n, vis, ans);
+                if(x.second == true){
+                    // cout << "here" << endl;
+                    ans++;
+                }
+            }
+        }
         
-//         return ;
-//     }
+        return ;
+    }
     
     void dfs(int curr, vector<vector<int>> &adjlist, vector<bool> &vis, int &ans){
         if(vis[curr]){
@@ -36,38 +37,39 @@ public:
             }
         }
     }
+    
     int minReorder(int n, vector<vector<int>>& connections) {
         
-        // make graph (adjList)
-        vector<vector<int>> adjList(n);
-        for(auto x :connections){
-            int u = x[0];
-            int v = x[1];
-            adjList[u].push_back(v);
-            adjList[v].push_back(-u);
-        }
-        vector<bool> vis(n, false);
-        int ans = 0;
-        dfs(0, adjList, vis, ans);
-        return ans;
-        
-        
-//         //adjList
-//         vector<vector<pair<int, bool>>> adjList(n);
-        
-//         for(auto x : connections){
+//         // make graph (adjList)
+//         vector<vector<int>> adjList(n);
+//         for(auto x :connections){
 //             int u = x[0];
 //             int v = x[1];
-
-//             adjList[u].push_back({v, false}); 
-//             adjList[v].push_back({u, true}); // true represents that this edges needs to be reversed
+//             adjList[u].push_back(v);
+//             adjList[v].push_back(-u);
 //         }
-        
 //         vector<bool> vis(n, false);
 //         int ans = 0;
-//         dfs_old(0, adjList, n, vis, ans);
-        
+//         dfs(0, adjList, vis, ans);
 //         return ans;
+        
+        
+        //adjList
+        vector<vector<pair<int, bool>>> adjList(n);
+        
+        for(auto x : connections){
+            int u = x[0];
+            int v = x[1];
+
+            adjList[u].push_back({v, true}); // true represents that this edges needs to be reversed
+            adjList[v].push_back({u, false}); 
+        }
+        
+        vector<bool> vis(n, false);
+        int ans = 0;
+        dfs_old(0, adjList, n, vis, ans);
+        
+        return ans;
         
         
         
