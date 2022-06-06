62782f49aaef2c4a5e6ae9fdb79c9f952916fd59
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-06 20:30:26 +0530

    Time: 130 ms (6.51%), Space: 32.1 MB (62.71%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -7,46 +7,93 @@ public:
         }
         return true;
     }
+    
     bool canReach(vector<int>& arr, int start) {
-        
         int n = arr.size();
-        
         queue<pair<int, int>> q;
-        vector<bool> vis(n, false);
         
         q.push({start, arr[start]});
-        vis[start] = true;
-        
         
         while(!q.empty()){
             auto front = q.front();
             q.pop();
+            
             int index = front.first;
             int val = front.second;
-            
             if(val == 0){
                 return true;
             }
+            if(val<0){
+                continue;
+            }
             
-            
-            int temp1 = index + val;
+            int temp1 = index +  val;
             int temp2 = index - val;
             
             if(isvalid(temp1, n)){
-                if(!vis[temp1]){
-                    q.push({temp1, arr[temp1]});
-                    vis[temp1] = true;
-                }
-                
+                q.push({temp1, arr[temp1]});
             }
             if(isvalid(temp2, n)){
-                if(!vis[temp2]){
-                    q.push({temp2, arr[temp2]});
-                    vis[temp2] = true;
-                }
+                q.push({temp2, arr[temp2]});
             }
+            
+            arr[index] = -val; // negating the value which we have --> 
+            // since array has non negative numbers, therefore if we negate a number
+            // and we aage kabhi encounter a negative number, then we know it is already visited;
+            // therefore we "continue" the code;
+            
         }
+        
         return false;
+        
+        
+        
+        
+//         SC - O(N) --> it can be optimised , see above
+//         TC - O(N)
+        
+//         int n = arr.size();
+        
+//         queue<pair<int, int>> q;
+        
+//         vector<bool> vis(n, false); 
+//         // need this , else as you can see in test case example 3,
+//         // {index, val} --> {2,2} && {4,2} oscillate karte rahenge
+        
+//         q.push({start, arr[start]});
+//         vis[start] = true;
+        
+        
+//         while(!q.empty()){
+//             auto front = q.front();
+//             q.pop();
+//             int index = front.first;
+//             int val = front.second;
+            
+//             if(val == 0){
+//                 return true;
+//             }
+            
+            
+//             int temp1 = index + val;
+//             int temp2 = index - val;
+            
+//             if(isvalid(temp1, n)){
+//                 if(!vis[temp1]){
+//                     q.push({temp1, arr[temp1]});
+//                     vis[temp1] = true;
+//                 }
+                
+//             }
+//             if(isvalid(temp2, n)){
+//                 if(!vis[temp2]){
+//                     q.push({temp2, arr[temp2]});
+//                     vis[temp2] = true;
+//                 }
+//             }
+//         }
+        
+//         return false;
         
     }
 };
