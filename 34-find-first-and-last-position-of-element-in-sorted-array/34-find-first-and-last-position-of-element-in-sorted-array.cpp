733efe7ed3102dbe30a96b1e2e88bb943d08c91e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-10 13:27:45 +0530

    Time: 8 ms (80.42%), Space: 13.8 MB (18.27%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,54 +1,70 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int target) {
+    int first(vector<int> &arr, int target){
         int n = arr.size();
-        // first index
-        int start = 0 ; 
+        int start = 0;
         int end = n-1;
-        
-        int first = -1;
-        
-        while(start <= end){
-            int mid = start + ( end-start)/2;
+        int res = -1;
+        while(start<= end){
+            int mid = start + (end-start)/2;
             if(arr[mid] == target){
-                first = mid;
-                end = mid -1;
+                // go left
+                res = mid;
+                end = mid-1;
             }
-            else if(arr[mid] > target){
+            else if(arr[mid]>target){
+                //goleft
                 end = mid-1;
             }
             else{
-                start = mid +1;
-            }
-        }
-        
-        if (first == -1){
-            return {-1, -1};
-        }
-        
-        int second = -1;
-        
-        start = 0;
-        end = n-1;
-        
-        while(start <= end){
-            int mid = start + (end-start)/2;
-            if(arr[mid] == target){
-                second = mid;
-                start = mid +1;
-            }
-            else if(arr[mid]  < target){
                 start = mid+1;
             }
-            else{
-                end = mid -1;
-            }
         }
         
-        return {first, second};
+        return res;
+    }
+    
+    int last(vector<int> &arr, int target){
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+        int res = -1;
+        while(start<= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] == target){
+                // go right
+                res = mid;
+                start = mid+1;
+            }
+            else if(arr[mid] > target){
+                //go left
+                end = mid -1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return res;
+       
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
         
         
+        vector<int> ans(2, -1);
+        if(nums.size() == 1 ){
+            if(nums[0] == target){
+                ans[0] = 0;
+                ans[1] = 0;
+                return ans;
+            }
+            else{
+                return ans;
+            }
+        }
+        ans[0] = first(nums, target);
+        ans[1] = last(nums, target);
         
+        return ans;
         
     }
 };
