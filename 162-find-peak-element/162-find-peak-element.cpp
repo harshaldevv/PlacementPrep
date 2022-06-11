87a7b93b9edeb57fc6b05482a7e4b077cf9c3c2a
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-11 14:24:17 +0530

    Time: 8 ms (33.08%), Space: 8.8 MB (89.28%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,40 +1,45 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int start = 0;
-        int n = nums.size();
-        int end = n-1;
+    int findPeakElement(vector<int>& arr) {
+        int n= arr.size();
         
-        if(nums.size() == 1){
+        if(n==1){
             return 0;
         }
         
+        int start = 0;
+        int end = n-1;
+        
+        
+        
         while(start <= end){
+            int mid = start + (end-start)/2;
             
-            int mid = start + (end- start)/2;
-            cout << "mid = " << mid << endl;
-            
-            if(mid > 0 && mid < n-1 ){
-                if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] ){
+            if(mid > 0 && mid < n-1){
+                // checking b/w index 1 to n-2 (both inclusive)
+                
+                if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
                     return mid;
                 }
-                else if(nums[mid +1] > nums[mid-1]){
-                    start = mid +1;
+                else if(arr[mid+1] > arr[mid] ){
+                    // right side is more promising / has a possible ans
+                    start = mid+1;
                 }
                 else{
                     end = mid -1;
                 }
             }
             else if(mid == 0){
-                if(nums[0] >= nums[1]){
-                  return 0;
+                if(arr[0] > arr[1]){
+                    return 0;
                 }
                 else{
                     return 1;
-                }  
+                }
             }
-            else if(mid == n-1){
-                if(nums[n-1] > nums[n-2]){
+            else{
+                // mid == n-1
+                if(arr[n-1] > arr[n-2]){
                     return n-1;
                 }
                 else{
