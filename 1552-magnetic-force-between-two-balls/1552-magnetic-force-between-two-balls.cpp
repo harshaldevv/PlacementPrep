2ed8ace023783f096216e580b3f24690955c5ce9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-14 21:11:57 +0530

    Time: 452 ms (6.56%), Space: 58 MB (41.27%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    bool isValid(vector<int> &pos, int magnets, int myDist){
-        // magnets --> total magnets i have
+    int isvalid(int F, vector<int> &pos, int m){
+        int balls = 1;
+        int latest = 0;
         
-        int m = 1;
-        int start = pos[0];
-        for(int i = 1 ; i < pos.size() ; i++){
-            if(pos[i] - start >= myDist){
-                m++;
-                start = pos[i];
-            }
-            
-            if(m == magnets){
-                return true;
+        for(int i = 1 ; i < pos.size(); i++){
+            if(abs(pos[i] - pos[latest]) >= F){
+                
+                latest = i;
+                
+                balls++;
+                
+                if(balls == m){
+                
+                    return true;
+                }
             }
         }
+        
         
         return false;
     }
@@ -23,23 +26,22 @@ public:
         int start = 1;
         int end = 1000000000;
         
+        int res = -1;
         sort(position.begin(), position.end());
-        
-        long res = -1;
-        
         while(start <= end){
             int mid = start + (end-start)/2;
-            if(isValid(position, m, mid) == true){
+            cout << endl;
+            
+            if(isvalid(mid, position, m)){
                 res = mid;
-                //find a larger dist
-                start = mid +1;
+                start = mid+1;
+                
             }
             else{
-                end = mid-1;
+               end = mid -1;
             }
         }
         
         return res;
-        
     }
 };
