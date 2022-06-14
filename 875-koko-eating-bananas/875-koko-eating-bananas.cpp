cb774a3b17be7c95e8459fa25b890dbe57484f9a
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-14 19:00:28 +0530

    Time: 46 ms (89.16%), Space: 18.9 MB (66.57%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
-    bool isValid(vector<int> &piles, int h, int probableRate){
-        long long int hours = 0;
-        for(int i = 0 ; i< piles.size() ; i++){
-            hours += (piles[i]/probableRate);
-            if(piles[i]%probableRate != 0){
-                hours++;
+    long isvalid(int rate, vector<int> &piles, int h){
+        long timeTaken = 0;
+        
+        for(auto x : piles){
+            //cout << "x = " << x << endl; 
+            long t = x/rate;
+            //cout << "t = " << t << endl;
+            
+            if(x%rate != 0){ 
+                //if the pile[i] has than 'k' bananas
+                // k = rate 
+                t++;
             }
-            if(hours > h){
-            return false;
+            //cout << "t = " << t << endl;
+            
+            timeTaken += t;
+            // cout << "timeTaken = " << timeTaken << endl;
+            // cout << endl;
+            
+            if(timeTaken > h){
+                return false;
             }
         }
-
         return true;
     }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
+        // Binary search on answer
+        // try on 14 Jun 2022
         
-        long start = 1; //start rate - 1 banana per hour
-        long long int end = 1000000000; // end = all bananas per hour
-        long k = -1;
-        //cout << start << end << endl;
+        int start =  1; //*max_element(piles.begin(), piles.end());
+        int end = 0;
+        end =  1000000000; //accumulate(piles.begin(), piles.end(), end);
+        //cout << "end = " <<end << endl;
+        
+        long res = -1;
         while(start <= end){
-            
-            long mid = start + (end-start)/2;
-            // mid == our probable k
-        
-            if(isValid(piles, h, mid)== true){
-                k = mid;
+            int mid = start+ (end-start)/2;
+            //cout << "mid = " << mid << endl;
+            if(isvalid(mid, piles, h)){
+                res = mid;
                 end = mid-1;
             }
             else{
-                start = mid+1;
+                start = mid +1;
             }
         }
-        return k;
+        
+        return res;
         
     }
 };
