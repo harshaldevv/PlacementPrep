6ec00e553d1bbb14f591803884eddc1923995d91
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-17 21:16:26 +0530

    Time: 4 ms (39.52%), Space: 8.6 MB (15.33%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -13,13 +13,47 @@ class Solution {
 public:
     vector<int> ans;
     vector<int> preorderTraversal(TreeNode* root) {
-        if(root == NULL){
+        
+        //iterative soln -- > TC - O(N), SC - O(N)
+        
+        if(root == nullptr){
             return ans;
         }
-        ans.push_back(root->val); // print(root->data);
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
+        
+        stack<TreeNode*> st; // push  right , then left, cuz stack works in LIFO
+        
+        st.push(root);
+        
+        while(!st.empty()){
+            auto front = st.top();
+            st.pop();
+            cout << front->val << " ";
+            ans.push_back(front->val);
+            
+            // push  right , then left, cuz stack works in LIFO
+            if(front->right){
+                st.push(front->right);
+            }
+            
+            if(front->left){
+                st.push(front->left);
+            }
+        }
+        
         return ans;
+        
+        
+        
+        
+        // Recursive SOln - > TC - O(N), SC - O(N)
+        
+        // if(root == NULL){
+        //     return ans;
+        // }
+        // ans.push_back(root->val); // print(root->data);
+        // preorderTraversal(root->left);
+        // preorderTraversal(root->right);
+        // return ans;
         
     }
 };
