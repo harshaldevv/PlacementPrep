d363148ee1229c0df2d6e2a692c4296af0018124
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-17 21:54:58 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (41.15%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,16 +11,49 @@
  */
 class Solution {
 public:
-    vector<int> ans;
+    
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root==NULL){
+        
+        vector<int> ans;
+        // iterative soln
+        
+        if(root == nullptr){
             return ans;
         }
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
+        
+        stack<TreeNode*> st;
+        TreeNode *node = root;
+        
+        
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }
+            else{
+                if(st.empty()){
+                    break;
+                }
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
         
         return ans;
+        
+        
+        // recursive soln
+        
+//         if(root==NULL){
+//             return ans;
+//         }
+//         inorderTraversal(root->left);
+//         ans.push_back(root->val);
+//         inorderTraversal(root->right);
+        
+//         return ans;
         
         
     }
