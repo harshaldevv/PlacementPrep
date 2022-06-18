6e27b03903419936712365a5532b0799df596103
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-18 13:09:36 +0530

    Time: 32 ms (20.85%), Space: 13.5 MB (97.53%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,37 @@
+#define pii pair<int, int>
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        // Trivial solution --  Sorting (TC - O(nlogn));
+        
+        // Heap --> Min Heap
+        priority_queue<pii, vector<pii>, greater<pii>> minheap;
+        
+        unordered_map<int, int> mp;
+        for(auto &x : nums){
+            mp[x]++;
+        }
+        
+        //make heap from map;
+        for(auto x : mp){
+            int elem = x.first;
+            int freq = x.second;
+            minheap.push({freq, elem});
+            if(minheap.size() > k){
+                minheap.pop();
+            }
+        }
+        
+        vector<int> ans;
+        while(!minheap.empty()){
+            auto TOP = minheap.top();
+            minheap.pop();
+            ans.push_back(TOP.second);
+        }
+        
+        return ans;
+        
+        
+    }
+};
