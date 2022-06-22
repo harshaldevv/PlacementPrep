0f96bdc1573e4bab7c145af80683a8aaa2f79a37
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-23 00:25:40 +0530

    Time: 41 ms (38.11%), Space: 31.2 MB (54.37%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,24 +3,40 @@ public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> left(n);
-        vector<int> right(n);
-        
-        int l = 0;
-        int r = 0;
-        for(int i = 0 ; i < n ; i++){
-            l+=nums[i];
-            r+=nums[n-1-i];
-            left[i] = l;
-            right[n-1-i] = r;
-        
+        // TC - O(n), SC - O(1)
+        int right = 0;
+        for(auto x : nums){
+            right +=x;
         }
-        
+        int left = 0;
         for(int i = 0 ; i < n ; i++){
-            if(left[i] == right[i]){
+            right-=nums[i];
+            if(left == right){
                 return i;
             }
+            left+=nums[i];
+            
         }
+        
+        // TC - O(n), SC- O(n)
+//         vector<int> left(n);
+//         vector<int> right(n);
+        
+//         int l = 0;
+//         int r = 0;
+//         for(int i = 0 ; i < n ; i++){
+//             l+=nums[i];
+//             r+=nums[n-1-i];
+//             left[i] = l;
+//             right[n-1-i] = r;
+        
+//         }
+        
+//         for(int i = 0 ; i < n ; i++){
+//             if(left[i] == right[i]){
+//                 return i;
+//             }
+//         }
         
         return -1;
     }
