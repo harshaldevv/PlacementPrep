b5a938274aae73ea0e148ac90447e484c6419d04
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-23 14:17:07 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (69.73%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -23,13 +23,41 @@ public:
     
     vector<vector<int>> subsets(vector<int>& nums) {
         
+//         vector<vector<int>> ans;
+        
+//         vector<int> holder;
+        
+//         solve(nums,0, holder, ans);
+        
+//         return ans;
+        
+        // RECURSIVE (BACKTRACK) METHOD ^^
+        
+        
+        // ITERATIVE METHOD --> OP
+        
+        // EXPLAINATION BELOW
+        /*
+        [] ----------> []
+        [1] ---------> [] ,  [1]
+        [1,2] -------> [] ,  [1] , [2] , [1,2]
+        [1,2,3] -----> [] ,  [1] , [2] , [1,2], [3], [1,3], [2,3], [1,2,3]
+        */
+        // basically upar walo ko sabko lelo without nums[i] (exclude case), then usko include karlo (nums[i])
+        
         vector<vector<int>> ans;
+        ans.push_back({});
         
-        vector<int> holder;
-        
-        solve(nums,0, holder, ans);
+        for(int i = 0 ; i < nums.size(); i++){
+            int sz = ans.size();
+            
+            for(int j = 0 ; j < sz ; j++){
+                vector<int> copy = ans[j];
+                copy.push_back(nums[i]);
+                ans.push_back(copy);
+            }
+        }
         
         return ans;
-        
     }
 };
