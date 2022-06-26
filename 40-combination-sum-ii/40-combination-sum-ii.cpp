6f9bdf5eee6c17fd077bdc778acd525eb7c12e31
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-26 16:36:45 +0530

    Time: 16 ms (27.91%), Space: 14.9 MB (15.04%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solve(int i, int sum, vector<int> &nums, vector<int> &holder, vector<vector<int>> &ans, int target, bool pre){
+        
+        if(sum == target){
+            ans.push_back(holder);
+            return ;
+        }
+        
+        if(sum> target || i >= nums.size()){
+            return ;
+        }
+        
+        int e = nums[i];
+        
+        //exclude
+        solve(i+1, sum, nums, holder, ans, target, false);
+        if(i>0 && nums[i] == nums[i-1] && !pre){
+            return ;
+        }
+        
+        // include
+        holder.push_back(e);
+        solve(i+1, sum+e, nums, holder, ans, target, true);
+        
+        holder.pop_back();
+        
+        // //exclude
+        // holder.pop_back();
+        // solve(i+1, sum, nums, holder, ans, target);
+        
+        return ;
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> ans;
+        vector<int> holder;
+        int sum = 0;
+        
+        solve(0, sum, nums, holder, ans, target,false);
+        
+        return ans;
+        
+    }
+};
