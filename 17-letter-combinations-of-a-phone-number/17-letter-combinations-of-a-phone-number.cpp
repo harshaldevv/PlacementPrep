3a9d9e0b54b84811437a66b833f2286a78512e26
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-27 00:49:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (60.27%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,17 +1,55 @@
 class Solution {
 public:
+    void solve(int i, vector<string> &phone, string &digits,  string &path,vector<string> &ans){
+        if(i==digits.size()){
+            ans.push_back(path);
+                return ;
+        }
+        
+        for(auto &c : phone[digits[i] - '0']){
+            path.push_back(c);
+            solve(i+1, phone, digits, path, ans);
+            path.pop_back(); // back track
+        }
+        return ;
+    }
     vector<string> letterCombinations(string digits) {
         
-        if(digits == "") return {};
-	vector<string> mappings{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
-	for(auto digit : digits){
-		vector<string> extendCombination;
-		for(auto& currentCombination : ans)
-			for(auto newChar : mappings[digit - '2'])
-				extendCombination.push_back(currentCombination + newChar);                            
-		ans = move(extendCombination); // same as ans = extendCombination, just avoids copying each value. You Can also use swap(ans,extendCombination)
-	}
-	return ans;
+        vector<string> phone{"", "", "abc", "def", "ghi", "jkl", "mno","pqrs","tuv","wxyz"};
+        
+        
+
+        vector<string> ans;
+        if(digits.size() == 0){
+            return ans;
+        }
+        string path = "";
+        solve(0, phone, digits, path, ans);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // backtracking khtm
+        
+        // YAHI 3 bach rahe hai, jaldi jaldi khtm kardo
+        
+        // trees (interviewbit poora)
+        // graphs (dijsktra, all path algorithms, 0-1 BFS)
+        // DP revise karna hai
+        
+        // 2 pointers / sliding window (aditya verma)
+        
+        
+        
+        
+        
+        return ans;
+        
         
     }
 };
