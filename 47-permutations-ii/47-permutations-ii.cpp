4ca9856c9dbbc7809537f5c236b083771c48e227
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-26 23:22:04 +0530

    Time: 6 ms (85.13%), Space: 8.4 MB (95.33%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -6,6 +6,7 @@ public:
             ans.push_back(nums);
             return ;
         }
+       
         
         for(int j = i ; j < nums.size() ; j++){
             
@@ -15,6 +16,7 @@ public:
             
             swap(nums[i], nums[j]);
             solve(i+1, nums, ans);
+       
             
             //exclude
             //swap(nums[i], nums[j]);
@@ -22,12 +24,61 @@ public:
         
         return ;
     }
+    
+    bool nextPermutations(vector<int> &nums){
+        
+        int n = nums.size();
+        
+        int i= n-2;
+        
+        // 1 2 4 3 2 1
+        // left small, right big
+        while(i>=0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        
+        if(i==-1){
+            return false;
+            // if nums = [3,2,1]
+            // toh iska next permutations nahi exist krta
+            //isliye false return krdo
+        }
+        
+        //find next bigger than nums[i]
+        int next = n-1;
+        for(  ; next > i ; next--){
+            if(nums[next] > nums[i]){
+                break;
+            }
+        }
+        
+        swap(nums[i], nums[next]);
+        
+        reverse(nums.begin() +i +1, nums.end());
+        
+        return true;
+    }
+    
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
+//         sort(nums.begin(), nums.end());
         
+//         vector<vector<int>> ans;
+//         solve(0, nums, ans);
+        
+//         return ans;
+        
+    // Above --> Backtracking
+        
+    // Below --> Next Permutation
+        
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        solve(0, nums, ans);
+        ans.push_back(nums);
+        
+        while(nextPermutations(nums)){
+            ans.push_back(nums);
+        }
         
         return ans;
     }
