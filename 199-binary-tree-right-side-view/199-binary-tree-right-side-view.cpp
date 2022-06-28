3739c9c5eefe895fa9e4db0bbfe6079a6c6ebbbb
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-29 01:45:33 +0530

    Time: 0 ms (100.00%), Space: 11.8 MB (88.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,39 +11,84 @@
  */
 class Solution {
 public:
+    void solve(TreeNode *root, int level, vector<int> &ans){
+        if(root== nullptr){
+            return ;
+        }
+        
+        if(level == ans.size()){
+            ans.push_back(root->val);
+        }
+        
+        solve(root->right  , level+1 , ans);
+        solve(root->left , level+1 , ans);
+        
+        // for right view, we do --> root, right, left  (modified pre order traversal)
+        
+        // for left view, we do -> root, left, right (normal pre order traversal)
+        
+        return ;
+        
+        // TC - O(N);
+        // SC - O(N)
+        
+        
+        
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         if(root== nullptr){
             return ans;
         }
         
-        queue<pair<TreeNode*, int>> q; // node, row
-        map<int, int> mp ; // row, node->val
+        // TC - O(NLOGN)  -->Better soln below --> TC - O(N)
+        // SC - O(N)
         
-        q.push({root,0});
+//         queue<pair<TreeNode*, int>> q; // node, row
+//         map<int, int> mp ; // row, node->val
         
-        while(!q.empty()){
-            auto front = q.front();
-            q.pop();
+//         q.push({root,0});
+        
+//         while(!q.empty()){
+//             auto front = q.front();
+//             q.pop();
             
-            TreeNode* node = front.first;
-            int row = front.second;
+//             TreeNode* node = front.first;
+//             int row = front.second;
             
-            mp[row] = node->val;
+//             mp[row] = node->val;  // in right/;eft view, we gotta do it with row
+//             // instead of col
             
-            if(node->left){
-                q.push({node->left, row+1});
-            }
-            if(node->right){
-                q.push({node->right, row+1});
-            }
-        }
+//             if(node->left){
+//                 q.push({node->left, row+1});
+//             }
+//             if(node->right){
+//                 q.push({node->right, row+1});
+//             }
+//         }
         
-        for(auto x : mp){
-            ans.push_back(x.second);
-        }
+//         for(auto x : mp){
+//             ans.push_back(x.second);
+//         }
         
+//         return ans;
+        
+        
+        
+        
+        // modified pre order traversal (root, right, left)
+        solve(root,0,  ans);
         return ans;
-            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
