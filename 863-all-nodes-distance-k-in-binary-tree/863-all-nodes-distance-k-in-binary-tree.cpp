3fd4dd8b936d624906c3a47fbe8c9c822730f6da
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-06-29 23:51:39 +0530

    Time: 11 ms (49.12%), Space: 15.1 MB (16.54%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -44,11 +44,19 @@ public:
     }
     
     void doTraversal(TreeNode *root, int dist, int k, vector<int> &ans){
+        // instead of using recursion here, we could also use queue here 
+        // as shown in striver's video, but i find this recursion intutive 
+        // and it came naturally to me
+        
+        
         if(root == nullptr){
             return ;
         }
         if(visited.find(root) != visited.end()){
-            // node already visited
+            // this is done to prevent visiting the same node jahan se hum aye the
+            // remove this line to see what happens for example 1, you'll understand
+            
+            // node already visited --> therefore no use of this node, we can return
             return ;
         }
         
@@ -59,13 +67,14 @@ public:
             return;
         }
         
-        //go LEFT 
+        // go LEFT  (down)
         doTraversal(root->left, dist+1, k , ans);
         
-        // go RIGHT
+        // go RIGHT (down)
         doTraversal(root->right, dist+1, k , ans);
         
         // go UP
+        // access parent of root from the parent data structure aka MAP which we created
         doTraversal(mp[root], dist+1, k, ans);
         
         return ;
@@ -74,11 +83,19 @@ public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
        
         
+        // In this question, we had to even move upwards from a our target node
+        // downwards me left or right toh easily jaa sakte the, lekin
+        // upar jane ka koi tarika tha nahi 
+        
+        // therefore to tackle this problem, we store every node's parent in a 
+        // data structure (here map), to quickly gets its parent 
+        
+        // We do BFS to traverse each level and create our "parents" wala Data struc (see code)
         
         
         getParent(root);
         
-        // start traversal from root;
+        // start traversal from target;
         vector<int> ans;
         doTraversal(target, 0, k, ans);
         
