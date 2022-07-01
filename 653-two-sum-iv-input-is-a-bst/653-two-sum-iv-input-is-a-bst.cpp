96c040e95baac2a15276ea77a35cf2ef1bcd75e3
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-01 13:37:37 +0530

    Time: 42 ms (90.40%), Space: 36.7 MB (80.30%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nums;
+    void inorder(TreeNode *root){
+        if(root==nullptr){
+            return ;
+        }
+        
+        inorder(root->left);
+        nums.push_back(root->val);
+        inorder(root->right);
+        
+        return ;
+    }
+    bool twoSum(TreeNode *root, int k){
+        int i = 0;
+        int j = nums.size() -1;
+        
+        while ( i < j){
+            int a = nums[i];
+            int b = nums[j];
+            int sum = a+b;
+            if(sum ==k){
+                return true;
+            }
+            else if(sum > k){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        return false;
+    }
+    bool findTarget(TreeNode* root, int k) {
+        inorder(root);
+        bool ans = twoSum(root, k);
+        
+        return ans;
+        
+        
+    }
+};
