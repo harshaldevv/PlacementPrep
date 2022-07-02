bb50ba1a83766e7a6178cade307cf70a7e1ce75c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-02 23:56:40 +0530

    Time: 30 ms (57.83%), Space: 12 MB (60.85%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        int t[n+1][amount+1];
         
+        int  n = coins.size();
+        int  W = amount;
         
-        //initialization
-        for(int i = 0 ; i < n+1 ; i++){
-            t[i][0] = 1; 
-        }
+        int t[n+1][W+1];
+        //mapping it to Knapsack (unbounded) but with different intialization
+       
+        memset(t, -1, sizeof(t));
         
-        for(int j = 0 ; j < amount+1 ; j++){
+        // fill first row with 0;
+        for(int  j = 0 ; j < W +1 ; j++){
             t[0][j] = 0;
         }
         
-        t[0][0] = 1;
+        // fill first col with 1
+        for(int i = 0 ; i < n+1 ; i++){
+            t[i][0] = 1;
+        }
         
-        
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < amount +1 ; j++){
-                if(coins[i-1] <=j){
-                    t[i][j] = t[i-1][j]  +  t[i][j-coins[i-1]];
+        for(int i =1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < W+1; j++){
+                if(coins[i-1] <= j){
+                    t[i][j] = t[i-1][j] + t[i][j-coins[i-1]];
                 }
                 else{
                     t[i][j] = t[i-1][j];
@@ -28,7 +31,6 @@ public:
             }
         }
         
-        return t[n][amount];
-        
+        return t[n][W];
     }
 };
