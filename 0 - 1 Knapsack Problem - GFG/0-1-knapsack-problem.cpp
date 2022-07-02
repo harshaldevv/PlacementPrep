6e72712ccc8ff3005cfaf224d10f673d090ae7e8
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-02 10:16:31 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,33 +8,32 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knap(int wt[], int val[], int n, int W, vector<vector<int>> &t){
-       if(n== 0 || W == 0){
-           return 0;
-       }
-       
-       if(t[n][W] != -1){
-           return t[n][W];
-       }
-       
-       if(wt[n-1] <= W){
-           t[n][W] = max(val[n-1] + knap(wt, val, n-1, W-wt[n-1], t), knap(wt, val, n-1, W, t));
-           
-       }
-       else{
-           t[n][W] = knap(wt, val, n-1, W, t);
-           return t[n][W];
-       }
-    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
-       
-       vector<vector<int>> t(n+1, vector<int>(W+1, -1));
-       
+       int t[n+1][W+1];
        
        
-       return knap(wt, val, n, W, t);
+       for(int i = 0 ; i < n+1; i++){
+           for(int j = 0 ;  j < W+1 ; j++){
+               if(i == 0 || j == 0){
+                   t[i][j] = 0;
+               }
+           }
+       }
+       
+       for(int i = 1 ; i < n+1 ; i++){
+           for(int j = 1; j < W+1 ; j++){
+               if(wt[i-1] <= j){
+                   t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+               }
+               else{
+                   t[i][j] = t[i-1][j];
+               }
+           }
+       }
+       
+       return t[n][W];
     }
 };
 
