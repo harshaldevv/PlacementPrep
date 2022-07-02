5534820170ce0be0232587f0766081074dc36e75
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-03 00:54:28 +0530

    Time: 73 ms (90.28%), Space: 10.6 MB (84.62%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -6,20 +6,23 @@ public:
         
         int t[n+1][amount +1];
         
+        
+        //first col
         for(int i = 0 ; i < n+1 ; i++){
             t[i][0] = 0;
         }
         
-        for(int j =0  ; j < amount +1 ; j++){
+        for(int j = 0 ; j < amount +1 ; j++){
             t[0][j] = INT_MAX -1;
         }
         
-        t[0][0] = 0;  // dekhte hai chalo
+        t[0][0] = 0;
         
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < amount +1 ; j++){
+        
+        for(int i =1 ; i < n+1 ; i++){
+            for(int j =1 ; j < amount +1 ; j++){
                 if(coins[i-1] <= j){
-                    t[i][j] = min(1 +  t[i][j-coins[i-1]], t[i-1][j]);
+                    t[i][j] = min(t[i-1][j],  1+ t[i][j-coins[i-1]]);
                 }
                 else{
                     t[i][j] = t[i-1][j];
@@ -27,19 +30,15 @@ public:
             }
         }
         
+        
         int ans = t[n][amount];
         
-        // cout << "ans = " << ans << endl;
-        // return ans;
-        
-        if(ans > amount){  //doubt here
+        if(ans > amount){
             return -1;
         }
         else{
             return ans;
         }
-        
-        
         
     }
 };
