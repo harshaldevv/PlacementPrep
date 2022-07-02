113a232a34955c5fb30306344e73329050e16004
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-02 11:55:23 +0530

    Time: 208 ms (65.96%), Space: 17.9 MB (43.57%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,43 +1,42 @@
 class Solution {
 public:
-    bool subsetSum(vector<int> arr, int sum){
+    
+    bool subsetSum(vector<int> &nums, int sum){
+        int n = nums.size();
+        int W = sum;
         
-        int n = arr.size();
-        if(n == 0 || sum == 0){
-            return false;
+        
+        int t[n+1][W+1];
+        
+        //filling first row
+        for(int j = 0 ; j < W +1 ; j++){
+            t[0][j] = false;
         }
-        
-        bool t[n+1][sum+1];
         
         for(int i = 0 ; i < n+1 ; i++){
             t[i][0] = true;
         }
         
-        for(int i = 0 ; i < sum+1 ; i++){
-            t[0][i] = false;
-        }
-        
-        t[0][0] = true;
-        
         for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j< sum+1; j++){
-                if(arr[i-1] <= j){
-                    t[i][j] = t[i-1][j - arr[i-1]] || t[i-1][j] ; 
+            for(int j = 1 ; j < W+1 ; j++){
+                if(nums[i-1] <= j){
+                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
                 }
                 else{
-                    t[i][j] = t[i-1][j];
+                    t[i][j] =  t[i-1][j];
                 }
             }
         }
         
-        
-        return t[n][sum];
-        
+        return t[n][W];
         
     }
     bool canPartition(vector<int>& nums) {
+        // knapsack variation
+        
         int sum = 0;
-        for(int x : nums){
+        
+        for(auto x : nums){
             sum+=x;
         }
         
