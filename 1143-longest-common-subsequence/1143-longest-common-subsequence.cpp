56c7cc40d2a36fe3c8e1fbfcf85ead2e6dfeefa8
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-03 02:14:54 +0530

    Time: 67 ms (9.75%), Space: 13.2 MB (20.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string X, string Y) {
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
         
-        int n = X.size();
-        int m = Y.size();
+        vector<vector<int>> t(n+1, vector<int>(m+1, -1));
         
-        int t[n+1][m+1];
-        
-        for(int i = 0 ; i < n+1 ; i++){
-            t[i][0] = 0;
-        }
-        
-        for(int j = 0 ; j < m+1 ; j++){
-            t[0][j] = 0;
-        }
-        
-        for(int i = 1 ; i < n+1 ; i++){
-            for(int j = 1 ; j < m+1 ; j++){
-                if(X[i-1] == Y[j-1]){
-                    t[i][j] = 1 + t[i-1][j-1];
-                }
-                else{
-                    t[i][j] = max (t[i][j-1], t[i-1][j]);
-                }
-            }
-        }
-        
-        return t[n][m];
-
-        
-        
-        // memoized gives TLE at test case 42
-//         int n = text1.size();
-//         int m = text2.size();
-        
-//         if( n<= 0 || m <= 0){
-//             return 0;
-//         }
-        
-//         vector<vector<int>> t( n+1 , vector<int>(m+1, -1));
-        
-//         return lcsHelper(text1, text2, n, m, t);
+        return LCS(text1, text2, n,m, t);
         
     }
     
-    int lcsHelper(string x, string y, int n, int m , vector<vector<int>> &t){
-        if(n <=0 || m <= 0){
+    int LCS(string &X, string &Y, int n, int m, vector<vector<int>> &t){
+        
+        if(n== 0 || m== 0){
             return 0;
         }
         
@@ -53,15 +20,13 @@ public:
             return t[n][m];
         }
         
-        if(x[n-1] == y[m-1]){
-            t[n][m] = 1 + lcsHelper(x,y,n-1,m-1, t);
-            return t[n][m];
+        if(X[n-1] == Y[m-1]){
+            t[n][m] = 1 + LCS(X,Y, n-1, m-1, t);
         }
         else{
-            t[n][m] = max(lcsHelper(x,y, n, m-1, t) , lcsHelper(x,y, n-1, m , t));
-            return t[n][m];
+            t[n][m] = max(LCS(X,Y, n, m-1, t), LCS(X,Y, n-1, m,t));
         }
         
-        
+        return t[n][m];
     }
 };
