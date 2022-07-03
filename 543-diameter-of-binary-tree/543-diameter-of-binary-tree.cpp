2beabd17194d3531926ca594f9bea22d0886ac83
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-04 01:53:12 +0530

    Time: 7 ms (95.82%), Space: 20.2 MB (64.74%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -11,59 +11,23 @@
  */
 class Solution {
 public:
-    int height(TreeNode *root){
+    int helper(TreeNode* root, int &dia){
         if(root == nullptr){
             return 0;
         }
         
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
+        int l = helper(root->left, dia);
+        int r = helper(root->right, dia);
         
-        return max(leftHeight,rightHeight ) +1;
-    }
-    
-    int height2(TreeNode *root, int &dia){
-        if(root== nullptr){
-            return 0;
-        }
+        dia = max(dia, l +r);
         
-        int lh = height2(root->left, dia);
-        int rh = height2(root->right, dia);
-        
-        dia = max(dia, lh+rh);
-        
-        return 1 + max(lh,rh);
+        return 1 + max(l,r);
     }
     int diameterOfBinaryTree(TreeNode* root) {
         
-        if(root == nullptr){
-            return 0;
-        }
+        int dia = 0;
+        helper(root, dia);
         
-//         int left = height(root->left);
-//         int right = height(root->right);
-        
-//         return left+right;
-        
-        
-        //Above approach fails at 100/104 th test case--> gives WA
-        // cuz diameter "path" may or "may not" "pass" through the "root".
-        
-        
-        
-        // DOing second approach
-        int diameter = 0;
-        height2(root, diameter);
-        return diameter;
-        
-        /***
-        We can solve this problem with two different cases:
-        If the longest path will include the root node, then the longest path must be the 
-        depth(root->right) + depth (root->left)
-        If the longest path does not include the root node, this problem is divided into 2 sub-
-        problem: set left child and right child as the new root separately, and repeat step1.
-        We could get the solution by returning the max path of 1 and 2.
-        ***/
-        
+        return dia;
     }
 };
