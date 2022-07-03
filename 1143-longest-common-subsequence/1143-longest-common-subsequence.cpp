7546d218b57693e2306fcf4cf563bd973b47bb45
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-03 11:13:57 +0530

    Time: 28 ms (63.59%), Space: 17.4 MB (10.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
+    int LCS_matrix(string &X, string &Y, int n, int m){
+        int t[n+1][m+1];
+        
+        // initalize first row and first col with zero
+        memset(t, 0, sizeof(t));
+        
+        // already initalised
+        
+        for(int i =1 ; i < n+1 ; i++){
+            for(int j = 1 ; j < m+1 ; j++){
+                if(X[i-1] == Y[j-1]){
+                    t[i][j] = 1 + t[i-1][j-1];
+                }
+                else{
+                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+                }
+            }
+        }
+        
+        return t[n][m];
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         
         vector<vector<int>> t(n+1, vector<int>(m+1, -1));
         
-        return LCS(text1, text2, n,m, t);
+        // return LCS(text1, text2, n,m, t);
         // passing string by reference, doesnt lead to TLE -->
         // first attempt me i didnt pass by reference and hence lead to TLE
+        
+        
+        return LCS_matrix(text1, text2, n,m);
         
     }
     
