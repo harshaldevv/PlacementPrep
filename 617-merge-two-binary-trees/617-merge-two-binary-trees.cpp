7b7d499f796eeb599aef929efd9d8c3f4c03d335
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-05 18:39:29 +0530

    Time: 64 ms (41.76%), Space: 32.2 MB (98.26%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -20,11 +20,11 @@ public:
             return root1;
         }
         
-        if(root2 != nullptr){
-            root1->val += root2->val;
-            root1->left = mergeTrees(root1->left, root2->left);
-            root1->right = mergeTrees(root1->right, root2->right);
-        }
+ 
+        root1->val += root2->val;
+        root1->left = mergeTrees(root1->left, root2->left);
+        root1->right = mergeTrees(root1->right, root2->right);
+
         
         
         return root1;
