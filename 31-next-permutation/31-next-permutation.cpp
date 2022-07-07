f15604c5509051b11dbc513760c3741e63419e77
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-08 02:20:53 +0530

    Time: 6 ms (61.69%), Space: 12.2 MB (36.62%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //hitesh sir taught me
-        // we want left small, right big
-        int i = nums.size() -2;// starting from second last element
-        while(i>= 0 && nums[i] >= nums[i+1]){
-            i--;
-        }
         
-        if(i==-1){
-            // array is reverse sorted,
-            // therefore
-            reverse(nums.begin(), nums.end());
-            return;
-        }
+        // left small , right big
         
-        // we got the case
+        int n = nums.size();
         
-        // 1,2,4,3,2,1
-        //   ^
-        
-        // find just bigger number than nums[i]
-        // go from behind -> cuz we know array is sorted 
-        int next = nums.size() -1;
-        for( ; next > i ; next--){
-            if(nums[next] > nums[i]){
+        int i = n-2;
+        for( ; i >= 0 ; i--){
+            if(nums[i] < nums[i+1] ){
                 break;
             }
         }
         
+        if(i ==-1){
+            // array is reverse sorted
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        
+        // next bigger number than nums[left];
+        
+        int next = n-1;
+        
+        while(next > i){
+            if(nums[next] > nums[i]){
+                break;
+            }
+            next--;
+        }
+        
         swap(nums[i], nums[next]);
         
-        // 1,3,4,2,2,1
-        //     ^
+        reverse(nums.begin() + i+1 , nums.end());
         
-        //now reverse from i+1, to end
-        reverse(nums.begin() + i+1, nums.end());
+        return ;
         
-        return;
         
         
     }
