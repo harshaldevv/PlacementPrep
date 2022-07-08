c5f5baf00dcc1af26d33f437d603c56ca769bf70
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-08 12:06:27 +0530

    Time: 51 ms (65.78%), Space: 19.1 MB (66.69%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        
+        sort(intervals.begin(), intervals.end());
+        
+        vector<vector<int>> ans;
+        
+        ans.push_back(intervals[0]);
+        
+        for(int i = 1 ; i < n ; i++){
+            if(ans.back()[1] >=intervals[i][0]){
+                
+                // merge but with caution
+                // ans.back()[1] = intervals[i][1]; // this wont work cuz if intervals[i][1] < ans.back()[1] --> leads to WA
+                
+                if( intervals[i][1] < ans.back()[1] ){
+                    // do nothing
+                }
+                else{
+                    ans.back()[1] = intervals[i][1];
+                }
+            }
+            else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
