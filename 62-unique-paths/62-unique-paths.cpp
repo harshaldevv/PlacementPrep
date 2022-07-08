7176d69bbee7b0c64e6ac52add8b560d7914724e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-09 00:33:16 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,94 +1,42 @@
 class Solution {
 public:
-    //int dp[100+1][100+1];
-    int uniquePaths(int m, int n) {
-        
-        
-        // combinatorics way --> TC - O(min(m,n)), SC - O(1)
-        
-        long N = m+n-2;
-        long R = min(m,n)-1;
-        
-        //do N_C_R
-        
-        long ans = 1;
-        int j = 1;
-        for(int i = N; i >= max(m,n); i--){
-            ans *= (i);
-            ans/=j;
-            j++;
-            // https://leetcode.com/problems/unique-paths/discuss/1581998/C%2B%2BPython-5-Simple-Solutions-w-Explanation-or-Optimization-from-Brute-Force-to-DP-to-Math
-            
-            // https://leetcode.com/problems/unique-paths/discuss/1582051/C%2B%2B-EASY-Intuitive-Solution-oror-Combinatorics-oror-TC-O(min(MN))-SC-O(1)-oror-Beats-100
+    int t[100+1][100+1];
+    
+    int helper(int i, int j){
+        if(i < 0 || j < 0){
+            return 0;
         }
         
-        return ans;
+        if(i == 0 && j == 0){
+            return 1;
+        }
         
-        
-        
-        
-        
-        // tabulation space optimized -> TC - O(m+n), SC - O(n) aka number of columns
-        
-        // feeling lelo is code ki kaise likhna hai, mene thora sloppy way me likha hai isko
-        
-//         vector<long> t(n+1, 1);
-//         for(int i = 1 ; i < m; i++){
-//             for(int j = 1 ; j < n+1 ; j++){
-//                 t[j] += t[j-1];
-//             }
-//         }
-        
-        
-//         return t[n-1];
-        
-        
-        
-        // tabulation -- TC - O(m+n), SC - O(m+n)
-        
-        
-//         int t[m+1][n+1];
-//         memset(t, -1, sizeof(t));
-        
-//         // filling base case
-//         for(int i = 1 ; i < n+1 ; i++){
-//             t[1][i] = 1;
-//         }
-        
-//         for(int i = 1 ; i < m+1 ; i++){
-//             t[i][1] = 1;
-//         }
-        
-        
-//         for(int i = 2 ; i < m+1 ; i++){
-//             for(int j = 2 ; j < n+1 ; j++){
-//                 t[i][j] = t[i-1][j] + t[i][j-1];
-//             }
-//         }
-        
-//         return t[m][n];
-        
-        
-        
-        // recursion --> TC - O(2^[m+n]), SC - O(m+n) ->inbuilt recursive stack
-        
-        // then memoization --> TC - O(m+n), SC - O(m+n)
-        // memset(dp, -1, sizeof(dp));
-        // return helper(m,n);
+        return helper(i-1, j) +  helper(i, j-1);
     }
     
-//     int helper(int m, int n){
-//         if(m==1 || n ==1){
-//             dp[m][n] = 1;
-//             return 1;
-//         }
+    int helperWithMemo(int i, int j){
+        if( i < 0 || j <  0){
+            return 0;
+        }
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
         
-//         if(dp[m][n] != -1){
-//             return dp[m][n];
-//         }
+        t[i][j] = helperWithMemo(i-1,j) + helperWithMemo(i, j-1);
+        return t[i][j];
         
-//         dp[m][n] =  helper(m,n-1) + helper(m-1, n);
-//         return dp[m][n];
         
-//     }
+        
+    }
+    int uniquePaths(int m, int n) {
+        
+        // return helper(m-1, n-1);
+        
+        memset(t, -1, sizeof(t));
+        return helperWithMemo(m-1, n-1);
+        
+    }
 };
