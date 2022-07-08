57bbef40920a28d525f6ad5178776f00b0ccef8c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-09 01:40:42 +0530

    Time: 4 ms (64.29%), Space: 7.6 MB (90.52%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,86 +1,55 @@
 class Solution {
 public:
-    int dp[100+1][100+1];
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int rows = obstacleGrid.size();
-        int cols = obstacleGrid[0].size();
-        //doing tabulation  --> https://leetcode.com/problems/unique-paths-ii/discuss/23395/Java-Simple-and-clean-DP-solution-easy-to-understand.   
-        
-        // ^^ this link helped me the most to write the tabulation part. 
-        // baaki sab toh ajeeb thi
-        int t[rows][cols];
-        memset(t, 0 , sizeof(t));
-        
-        // filling first row
-        for(int i = 0 ; i < cols ; i++){
-            if(obstacleGrid[0][i] == 1){
-                t[0][i] = 0;
-                break;
-            }
-            else{
-                t[0][i] = 1;
-            }
+    int t[100 +1][100+1];
+    int helper(int i, int j , vector<vector<int>> &grid){
+        if(i < 0 || j < 0){
+            return 0;
         }
         
-        //filling first column
-        for(int i = 0 ; i < rows ; i++){
-            if(obstacleGrid[i][0] == 1){
-                t[i][0] = 0;
-                break;
-            }
-            else{
-                t[i][0] = 1;
-            }
+        if(grid[i][j] == 1){
+            // obstacle
+            return 0;
         }
         
-        
-        for(int i = 1 ;  i< rows ; i++){
-            for(int j = 1 ; j< cols ; j++){
-                if(obstacleGrid[i][j] == 1){
-                    t[i][j]=0;
-                }
-                else{
-                    t[i][j] = t[i-1][j] + t[i][j-1];
-                }
-            }
+        if(i == 0 && j == 0){
+            return 1;
         }
         
-        return t[rows-1][cols-1];
+        return helper(i-1, j, grid) + helper(i, j-1, grid);
+            
+    }
+    
+    int helperWithMemo(int i, int j, vector<vector<int>> &grid){
+        if( i < 0 || j < 0){
+            return 0;
+        }
+        if(grid[i][j] == 1){
+            return t[i][j] = 0;
+        }
         
+        if(i == 0 && j == 0){
+            return t[i][j] = 1;
+        }
         
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
         
-        
-        
-        
-//         //memoziation passes all test cases --> TC - O(m+n), SC - O(m*n)
-//         // normal recursion -- gives TLE --> 27/41 passed
-//         int rows = obstacleGrid.size();
-//         int cols = obstacleGrid[0].size();
-//         memset(dp, -1, sizeof(dp));
-        
-//         return helper(0, 0, obstacleGrid);
+        return t[i][j] = helperWithMemo(i-1, j, grid) + helperWithMemo(i, j-1, grid);
         
     }
     
-    int helper(int i, int j , vector<vector<int>> &matrix){
-        if(i>= matrix.size() || j >= matrix[0].size()){
-            return 0;
-        }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         
-        if(matrix[i][j] == 1){
-            dp[i][j] = 0;
-            return 0;
-        }
         
-        if( i==matrix.size() -1 && j == matrix[0].size() -1  && (matrix[i][j] == 0) ){
-            dp[i][j] = 1;
-            return 1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+        // return helper(m-1, n-1, grid);  --> recursive approach
         
-        dp[i][j]  = helper(i+1, j, matrix) + helper(i, j+1, matrix);
-        return dp[i][j];
+        
+        memset(t, -1, sizeof(t));
+        return helperWithMemo(m-1, n-1, grid);
     }
+    
+    
 };
