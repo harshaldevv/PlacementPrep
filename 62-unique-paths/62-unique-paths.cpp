f94810db11a5ef02928490ccfa8315418fc0d429
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-09 01:29:43 +0530

    Time: 3 ms (43.92%), Space: 6 MB (73.35%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -18,8 +18,16 @@ public:
         if( i < 0 || j <  0){
             return 0;
         }
-        if(i == 0 && j == 0){
-            return 1;
+        
+        // if(i == 0 && j == 0){ --> this works too but below one is better cuz it help in filling our dp matrix 
+        //     return 1;
+        // }
+        
+        if(i == 0 || j == 0){
+            return 1; 
+            //Starting from cell [m - 1][n - 1] has 2 options: up and left. 
+            //If you arrive at m == 0 that means that the only way you can go is left, 
+            // that means that you have only 1 option to get there. Similar for n == 0.
         }
         if(t[i][j] != -1){
             return t[i][j];
@@ -28,8 +36,6 @@ public:
         t[i][j] = helperWithMemo(i-1,j) + helperWithMemo(i, j-1);
         return t[i][j];
         
-        
-        
     }
     int uniquePaths(int m, int n) {
         
@@ -37,6 +43,25 @@ public:
         
         memset(t, -1, sizeof(t));
         return helperWithMemo(m-1, n-1);
+        
+        
+        int t[m+1][n+1];
+        memset(t, 0, sizeof(t));
+        
+        //fill first row and first col = 0, except the first cell of the matrix
+        // fill first cell of the matrix with 1
+        t[0][0] = 1;
+        
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+                cout << t[i][j] << " ";
+            }
+        }
+        
+        return t[n-1][m-1];
+        
+        
         
     }
 };
