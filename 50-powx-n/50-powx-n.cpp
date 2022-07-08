a79d3a14d87efb95bfe5c0832f43b90a16d99981
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-08 21:03:00 +0530

    Time: 3 ms (46.78%), Space: 5.9 MB (80.12%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    
     double helper(double x, long long n){
         if(n == 0 || x == 1){
             return 1;
@@ -12,16 +13,55 @@ public:
         
         if(n%2 == 1){
             // n == odd
-            return x * myPow(x , n-1);
+            // return x * myPow(x , n-1);
+            return x * myPow(x*x , n/2);
         }
         else{
             return myPow(x*x, n/2);
         }
     }
     
+    double iterativeMyPow(double x, long long n){
+    
+        if(x == 1 || n ==0){
+            return 1;
+        }
+        
+        bool isNeg = false; 
+        // isNeg stores if the power was negative. If it was negative
+        // then we need to just 1/res our final answer
+        
+        if(n < 0){
+            isNeg = true;
+            n = -n;
+        }
+        
+        double res = 1;
+        
+        while(n){
+            if(n%2 == 1){
+                res*= x;
+                n--;
+            }
+            else{
+                x = (x*x);
+                n/=2;
+            }
+        }
+        
+        if(isNeg){
+            res = 1/res;
+        }
+        
+        return res;
+    }
+    
     double myPow(double x, int n) {
-        long long nn = ( (long long) (n) );
-        return helper(x, nn);
+        // long long nn = ( (long long) (n) );
+        // return helper(x, nn);
+        
+        long long nn = (long long)(n);
+        return iterativeMyPow(x, nn);
         
         
         
