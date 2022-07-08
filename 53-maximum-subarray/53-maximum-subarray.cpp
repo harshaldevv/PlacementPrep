1ef54b6344154f10ba3fa1566fcf16bdb93ae94c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-08 10:41:23 +0530

    Time: 170 ms (45.37%), Space: 67.8 MB (10.92%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -55,23 +55,68 @@ public:
         
         
 //         // kadane algo 
-        int sum = 0;
-        int maxi = INT_MIN;
+//         int sum = 0;
+//         int maxi = INT_MIN;
         
-        for(int i = 0. ; i < nums.size() ; i++){
+//         for(int i = 0. ; i < nums.size() ; i++){
             
-            int x = nums[i];
-            sum += x;
+//             int x = nums[i];
+//             sum += x;
+            
+//             maxi = max(maxi, sum);
+            
+//             sum = max(sum,0);
+//             // if(sum <0){
+//             //     sum = 0;
+//             // }
+            
+//         }
+        
+//         return maxi;
+        
+        // 8 july 2022
+        
+        int sum = 0;
+        int maxi = nums[0];
+        
+        for(int i = 0 ; i < nums.size() ; i++){
+            sum += nums[i];
             
             maxi = max(maxi, sum);
             
-            sum = max(sum,0);
-            // if(sum <0){
-            //     sum = 0;
-            // }
-            
+            if(sum < 0 ){
+                sum = 0;
+            }
         }
         
         return maxi;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
