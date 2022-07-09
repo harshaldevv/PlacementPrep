b498480fe21606e74fb14257e50d2de724bc0044
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-10 01:34:35 +0530

    Time: 134 ms (55.52%), Space: 41.4 MB (70.46%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
         unordered_map<int, int> mp;
-        int sum = 0;
+        int psum = 0;
         int ans = 0;
-        for(int i = 0 ; i< nums.size() ; i++){
-            sum += nums[i];
+        
+        for(int i = 0 ; i < n ; i++){
+            psum += nums[i];
             
-            if(sum == k){
-                ans++;
+           if(psum == k){
+               ans++;
+           }
+            
+            if(mp.find(psum - k) != mp.end()){
+                ans +=  mp[psum - k];
             }
-            
-            if(mp.find(sum - k) != mp.end()){                
-                ans+=mp[sum-k];
-            }
-            
-            mp[sum]++;
+            // else{  // else leke nahi chalega
+            //     // mp[psum] = i;
+            //     mp[psum]++;
+            // }
+            mp[psum]++;
         }
+        
         return ans;
+        
     }
 };
