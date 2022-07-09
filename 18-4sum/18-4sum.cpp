aa6c7408eed68774df52916e7c3f74d8ac771310
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-09 21:38:48 +0530

    Time: 89 ms (63.62%), Space: 13.2 MB (50.91%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i +1 ; j < n ; j++){
+                
+                int l = j + 1;
+                int r = n - 1;
+                
+                while(l < r){
+                    long long a = (long long)nums[i];
+                    long long b = (long long)nums[j];
+                    long long c = (long long)nums[l];
+                    long long d = (long long)nums[r];
+                    
+                    long long sum = a+b+c+d;
+                    
+                    if(sum == (long long)target){
+                        // store
+                        // ans.push_back({a,b,c,d});
+                        ans.push_back({ nums[i], nums[j], nums[l], nums[r] });
+                        
+                        
+                        while( l < r && nums[l] == nums[l+1]){
+                            l++;
+                        }
+                        while(l < r && nums[r] == nums[r-1]){
+                            r--;
+                        }
+                        
+                        l++;
+                        r--;
+                    }
+                    else if (sum < (long long)target){
+                        l++;
+                    }
+                    else{
+                        r--;
+                    }
+                }
+                
+                //roll duplicates of number 2
+                while( j + 1 < n && nums[j] == nums[j+1]){
+                    j++;
+                }
+            }
+            
+            // roll duplicates of number 1
+            while(i +1 < n && nums[i] == nums[i+1]){
+                i++;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
