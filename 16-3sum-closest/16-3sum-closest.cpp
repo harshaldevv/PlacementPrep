406617f4bd5afe72ce51871d0f7a735f6cad1534
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-09 16:53:56 +0530

    Time: 816 ms (15.62%), Space: 23.1 MB (46.52%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        // In this ques , we are asked "closest 3sum" to target
+        // therefore we want abs(target-sum) to be as minimum as possible 
+        // range of abs(target-sum) starts from 0 to +INF
+        // abs(target-sum) =0 if sum = target
+        // else we'll keep searching for our answer
+        // we'll keep checking the difference with our minimum diff (aka mini here)
+        // if our current difference is less than our current "mini", we'll
+        // update the "mini", which stores our minimum difference so far
+        // and also store this current "sum" we got (from the 3 elements) in our
+        // answer variable
         
-        int mini = INT_MAX;
-        int ans;
+        int mini = INT_MAX; // this holds our difference |target-sum|
+        int ans; // this holds our ans
         
         int n = nums.size();
         sort(nums.begin(), nums.end());
@@ -20,14 +30,17 @@ public:
                 
                 int sum = a+b+c;
                 
-                // mini = min(mini, abs(target-sum));
+                
                 if( abs(target-sum)  < mini ){
                     mini = abs(target-sum);
                     ans = sum;
                 }
                 
                 if(sum == target){
-                    // 
+                    // agar hume target hi mil gaya toh hum closest ho gaye na
+                    // cuz we wanted to minise our abs(target-sum).
+                    // here abs(target-sum) = 0, ho gaya
+                    // therefore return sum
                     return sum;
                 }
                 else if(sum < target){
@@ -38,6 +51,7 @@ public:
                 }
             }
             
+            //rolling the duplicates of number 1
             while(i+1 < n && nums[i] == nums[i+1]){
                 i++;
             }
