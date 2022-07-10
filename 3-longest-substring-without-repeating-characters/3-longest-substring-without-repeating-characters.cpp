3766ddefab9beb2fa5cadcd094639469948df60d
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-10 11:21:26 +0530

    Time: 56 ms (19.55%), Space: 10.9 MB (22.88%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,60 +1,62 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-//      // TC - O(2n), SC - O(n)
-//         int n = s.size();
-//         unordered_set<char> SET;
-        
-//         int l = 0;
-//         int r = 0;
-//         int maxlen = 0;
-        
-//         while ( r <  n ){
-//             if(SET.find(s[r]) == SET.end()){
-//                 // not found in our set
-                
-//                 //push in set
-//                 SET.insert(s[r]);
-//                 maxlen = max(maxlen, r-l +1);
-//                 r++;
-//             }
-//             else{
-                
-//                 SET.erase(s[l]);
-//                 l++;
-//             }
-//         }
-//         return maxlen;
-        
-        
-        
-        
-        
-        
-        
-        
+     // TC - O(2n), SC - O(n)
         int n = s.size();
-        unordered_map<char, int> mp;
-        int maxlen = 0;
+        unordered_set<char> SET;
         
         int l = 0;
         int r = 0;
+        int maxlen = 0;
         
-        while( r < n ){
-            auto c = s[r];
-            
-            if(mp.find(c) != mp.end()){
+        while ( r <  n ){
+            if(SET.find(s[r]) == SET.end()){
+                // not found in our set
                 
-                l = max(l, mp[c] +1);
+                //push in set
+                SET.insert(s[r]);
+                maxlen = max(maxlen, r-l +1);
+                r++;
             }
-            
-            mp[c] = r;
-            maxlen = max(maxlen, r-l +1);
-            
-            r++;
+            else{
+                
+                SET.erase(s[l]);
+                l++;
+            }
         }
-        
         return maxlen;
+        
+        
+        
+        
+        
+        
+        
+//         // TC - O(1*N) -> one pass
+//         // SC - O(N) 
+//         // upar wali zyada sahi lagi mujhe
+//         int n = s.size();
+//         unordered_map<char, int> mp;
+//         int maxlen = 0;
+        
+//         int l = 0;
+//         int r = 0;
+        
+//         while( r < n ){
+//             auto c = s[r];
+            
+//             if(mp.find(c) != mp.end()){
+                
+//                 l = max(l, mp[c] +1);
+//             }
+            
+//             mp[c] = r;
+//             maxlen = max(maxlen, r-l +1);
+            
+//             r++;
+//         }
+        
+//         return maxlen;
         
     }
 };
