2020b9a99a7a2bdb5db5d3d29ae48ee765eb681e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-10 11:20:35 +0530

    Time: 22 ms (60.59%), Space: 8.2 MB (81.73%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,29 +1,60 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-     // TC - O(2n), SC - O(n)
+//      // TC - O(2n), SC - O(n)
+//         int n = s.size();
+//         unordered_set<char> SET;
+        
+//         int l = 0;
+//         int r = 0;
+//         int maxlen = 0;
+        
+//         while ( r <  n ){
+//             if(SET.find(s[r]) == SET.end()){
+//                 // not found in our set
+                
+//                 //push in set
+//                 SET.insert(s[r]);
+//                 maxlen = max(maxlen, r-l +1);
+//                 r++;
+//             }
+//             else{
+                
+//                 SET.erase(s[l]);
+//                 l++;
+//             }
+//         }
+//         return maxlen;
+        
+        
+        
+        
+        
+        
+        
+        
         int n = s.size();
-        unordered_set<char> SET;
+        unordered_map<char, int> mp;
+        int maxlen = 0;
         
         int l = 0;
         int r = 0;
-        int maxlen = 0;
         
-        while ( r <  n ){
-            if(SET.find(s[r]) == SET.end()){
-                // not found in our set
+        while( r < n ){
+            auto c = s[r];
+            
+            if(mp.find(c) != mp.end()){
                 
-                //push in set
-                SET.insert(s[r]);
-                maxlen = max(maxlen, r-l +1);
-                r++;
+                l = max(l, mp[c] +1);
             }
-            else{
-                
-                SET.erase(s[l]);
-                l++;
-            }
+            
+            mp[c] = r;
+            maxlen = max(maxlen, r-l +1);
+            
+            r++;
         }
+        
         return maxlen;
+        
     }
 };
