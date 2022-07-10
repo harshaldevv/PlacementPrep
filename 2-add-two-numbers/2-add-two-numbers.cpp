76072e16ffbbb802aa29d4f4899f40d19965a9d7
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-10 21:12:59 +0530

    Time: 64 ms (33.71%), Space: 71.4 MB (50.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,42 +12,40 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
+        if(l1 == nullptr){
+            return l2;
+        }
+        if(l2 == nullptr){
+            return l1;
+        }
+        
         ListNode *p1 = l1;
         ListNode *p2 = l2;
         
-        
-        if(p1 == nullptr && p2 == nullptr){
-            return nullptr;
-        }
-        if(p1 == nullptr){
-            return p2;
-        }
-        if(p2 == nullptr){
-            return p1;
-        }
-        
         ListNode *dummy = new ListNode(-1);
-        ListNode *cursor = dummy;
+        ListNode* cursor = dummy;
         
+        int sum = 0;
         int carry = 0;
-        while(p1 || p2 || carry > 0){
-            int sum = 0;
-            // int sum = p1->val + p2->val;
+        
+        while ( p1 || p2 || carry){
+            
+            sum = 0;
+            
+            sum+= carry;
+            carry = 0;
             if(p1 != nullptr){
                 sum += p1->val;
             }
-            if(p2 != nullptr){
+            
+            if( p2 != nullptr){
                 sum += p2->val;
             }
             
-            if(carry != 0){
-                sum+=carry;
-                carry = 0; 
-                
-                
+            if(sum >= 10){
+                carry = sum/10;
+                sum%= 10;
             }
-            carry = sum/10;
-            sum%=10;
             
             ListNode *temp = new ListNode(sum);
             cursor->next = temp;
@@ -57,22 +55,12 @@ public:
                 p1 = p1->next;
             }
             
-            
-            if(p2 != nullptr){
+            if(p2 != nullptr ){
                 p2 = p2->next;
             }
             
-            
-            
         }
         
-        
-        
-        
-        
         return dummy->next;
-        
-        
-        
     }
 };
