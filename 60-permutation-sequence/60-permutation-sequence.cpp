eca3f0e25dd38b4c0c6ce6026ddee02be6b0db76
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-11 23:03:45 +0530

    Time: 489 ms (6.42%), Space: 6 MB (50.32%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,50 +1,92 @@
 class Solution {
 public:
+    
+    void nextPermutations (vector<int> &nums){
+        int n = nums.size();
+        // we want left small , right big
+        
+        int i = n-2;
+        
+        while( i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        
+        if( i == -1){
+            // nums is reverse sorted
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
+        
+        //find just bigger number than nums[i];
+        
+        int next = n-1;
+        for( ; next > i ; next--){
+            if(nums[next] > nums[i]){
+                break;
+            }
+        }
+        
+        // swap nums[i] and nums[next]
+        swap(nums[i], nums[next]);
+        
+        // reverse from i +1 to end
+        reverse(nums.begin() + i +1 , nums.end());
+        
+        return ;
+    }
     string getPermutation(int n, int k) {
-        vector<int> numbers;
+        
+        //doing on 11th july using next permutations cuz thats the first approach that struck me
+        vector<int> nums;
         for(int i = 1 ; i <= n ; i++){
-            numbers.push_back(i);
+            nums.push_back(i);
         }
         
-        int f = 1;
-        vector<int> fact;
-        fact.push_back(1);
-        for(int i = 1 ; i <= n ; i++){
-            f*=i;
-            // cout<< "f = " << f << endl;
-            fact.push_back(f);
+        for(int i = 0; i < k-1 ; i++){
+            nextPermutations(nums);
         }
-        // cout <<"numb --" << endl;
-        // for(auto x : numbers){
-        //     cout << x << " ";
-        // }
-        
-//         cout << endl;
-//         cout <<"fact --" << endl;
-//         for(auto x : fact){
-//             cout << x << " ";
-//         }
-        
-        
-        k--;
         
         string ans = "";
-        
-        while(n>0){
-            
-            int factorial = fact[n-1];
-            
-            int index = k/factorial;
-            // cout << "index = " << index << endl;
-            ans+= to_string(numbers[index]);
-            
-            numbers.erase(numbers.begin() + index);
-            n--;
-            k=k%factorial;
-
+        for(auto &x : nums){
+            ans += to_string(x) ;
         }
         
         return ans;
+        
+        
+//         vector<int> numbers;
+//         for(int i = 1 ; i <= n ; i++){
+//             numbers.push_back(i);
+//         }
+        
+//         int f = 1;
+//         vector<int> fact;
+//         fact.push_back(1);
+//         for(int i = 1 ; i <= n ; i++){
+//             f*=i;
+//             fact.push_back(f);
+//         }
+         
+        
+//         k--;
+        
+//         string ans = "";
+        
+//         while(n>0){
+            
+//             int factorial = fact[n-1];
+            
+//             int index = k/factorial;
+//             // cout << "index = " << index << endl;
+//             ans+= to_string(numbers[index]);
+            
+//             numbers.erase(numbers.begin() + index);
+//             n--;
+//             k=k%factorial;
+
+//         }
+        
+//         return ans;
 
     }
 };
