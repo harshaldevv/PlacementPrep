fcfee62df24735d6eb918432655dc140cc797783
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-12 09:51:50 +0530

    Time: 4 ms (65.62%), Space: 7.7 MB (78.23%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -18,11 +18,78 @@ public:
         
         return ;
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
+    int fact(int n){
+        if(n <=1){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    
+    void nextPermutation(vector<int> &nums){
+        int n = nums.size();
         
-        solve(nums, 0, ans);
+        // left small, right big
+        int i = n-2;
+        
+        while(i>= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        
+        if( i == -1){
+            // cuz we got reverse sorted array now, and hume ab stop karna hai
+            // cuz ye last permutation hai 
+            return ;
+        }
+        
+        // we got our small --> nums[i]
+        
+        //find the next bigger number than nums[i];
+        
+        int next = n-1;
+        
+        for ( ; next > i ; next--){
+            if(nums[next] > nums[i]){
+                break;
+            }
+        }
+        
+        // swap nums[i], nums[next]
+        swap (nums[i], nums[next]);
+        
+        // reverse from i+1;
+        reverse(nums.begin() + i +1 , nums.end());
+        
+        return ;
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        // doing on 12 July using next permutations
+        
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        ans.push_back(nums);
+        
+        for(int i = 1 ; i < fact(n) ; i++){
+            nextPermutation(nums);
+            ans.push_back(nums);
+        }
         
         return ans;
+        
+        
+//         vector<vector<int>> ans;
+        
+//         solve(nums, 0, ans);
+        
+//         return ans;
+        
+//         // https://leetcode.com/problems/permutations/discuss/1961596/C%2B%2B-oror-Easy-to-Understand-with-Diagram-oror-0-ms-oror-Backtracking
+        
+//         // diagram helped me formulating the soln
+        
     }
 };
