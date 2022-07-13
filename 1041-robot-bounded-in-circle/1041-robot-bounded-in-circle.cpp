007ed66e2ab4cd92b7160f23aa04831e9933060d
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-14 02:50:47 +0530

    Time: 5 ms (18.95%), Space: 6 MB (94.06%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        // therefore if their change in the direction vector, after 
+        int currx = 0;
+        int curry = 0;
+        int faceInWhichDirection = 0 ;  
+        
+        // 1 -> north (currx, curry +1)
+        // 2 -> east (currx-1, curry)
+        // 3 -> south, currx, curry -1
+        // 4 - west (currx +1, curry)
+        
+        
+        for(auto &instruct : instructions){
+            if(instruct == 'G'){
+                if(faceInWhichDirection == 0){
+                    currx +=0;
+                    curry +=1;
+                }
+                else if(faceInWhichDirection == 1){
+                    currx -=1;
+                    curry +=0;
+                }
+                else if(faceInWhichDirection == 2){
+                    currx+= 0;
+                    curry -=1;
+                }
+                else if(faceInWhichDirection == 3){
+                    currx +=1;
+                    curry += 0;
+                }
+            }
+            else if(instruct == 'L'){
+                faceInWhichDirection = faceInWhichDirection +1;
+                faceInWhichDirection = faceInWhichDirection%4;
+            }
+            else if(instruct == 'R'){
+                faceInWhichDirection = faceInWhichDirection - 1 + ( +4);
+                faceInWhichDirection = faceInWhichDirection%4;
+            }
+        }
+        
+        return faceInWhichDirection != 0 || (currx == 0 && curry == 0);
+        
+    }
+};
