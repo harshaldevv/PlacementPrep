2bc30c26d460484e4ad4a6f3f5b281e82111eeca
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-13 18:54:28 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.cpp
@@ -10,58 +10,56 @@ using namespace std;
 
 class Solution 
 {
-    public:
-    int isvalid(int maxPages, int a[], int n, int totalStudents){
-        
-        int stud = 1;
+    bool ispossible(int mid, int A[], int n, int k){
         int sum = 0;
+        
+        int kids = 1;
+        
         for(int i = 0 ; i < n ; i++){
-            sum += a[i];
-            if(sum > maxPages){
-                stud++;
-                sum = a[i];
+            sum += A[i];
+            if(sum > mid){
+                kids++;
+                sum = A[i];
             }
-            if(stud > totalStudents){
+            
+            if(kids > k){
                 return false;
             }
         }
         
         return true;
-        
     }
-        
+    public:
     //Function to find minimum number of pages.
-    int findPages(int A[], int N, int M) 
+    int findPages(int A[], int N, int k) 
     {
         //code here
-        if(M>N){
-            return -1;
-        }
         
         int start = 0;
+        int end = 0;
+        
         for(int i = 0 ; i < N ; i++){
             start = max(start, A[i]);
+            end += A[i];
         }
         
-        int end = 0;
-
-        for(int i = 0 ; i < N ; i++){
-            end+= A[i];
-        }
-        
-        int res = -1;
+        int ans = -1;
         while(start <= end){
-            int mid = start + (end-start)/2;
-            if(isvalid(mid, A, N, M)){
-                res= mid;
-                end = mid-1;
+            int mid = start + (end- start)/2;
+            
+            if(ispossible(mid, A, N, k )){
+                //go left
+                ans = mid;
+                end = mid -1;
             }
             else{
-                start = mid+1;
+                start = mid +1;
             }
         }
         
-        return res;
+        
+        return ans;
+        
     }
 };
 
