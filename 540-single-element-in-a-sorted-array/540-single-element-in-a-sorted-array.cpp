8945382d3f3dd0005029b3d65d4a79e6eb2cefb1
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-13 21:53:37 +0530

    Time: 57 ms (15.25%), Space: 22.4 MB (55.04%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,13 +1,40 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // linear search
-        for(int i= 0 ; i < nums.size()-1;i+=2){
-            if(nums[i] != nums[i+1]){
-                return nums[i];
+        // pls do this in logn pls
+       if(nums.size() == 1){
+           return nums[0];
+       }
+        
+        int l = 0;
+        int r = nums.size() -1;
+        
+        while(l<= r){
+            int mid = l + (r-l)/2;
+            
+            if(mid%2 != 0 && nums[mid] == nums[mid-1] || mid%2 == 0 && nums[mid] == nums[mid+1]){
+                // go right
+                l = mid +1;
+            }
+            else{
+                r = mid -1;
             }
         }
-        return nums[nums.size()-1]; // warna phir ab last element hu hoga hamara ans
+        
+        return nums[l];
+        
+        
+        
+        
+        
+        
+//         // linear search
+//         for(int i= 0 ; i < nums.size()-1;i+=2){
+//             if(nums[i] != nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return nums[nums.size()-1]; // warna phir ab last element hu hoga hamara ans
         
         
 //         // XOR SOLUTION
