bdb3ed74a68cf9c110a65d48c7fb94d25a706fe3
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-14 21:39:35 +0530

    Time: 14 ms (49.39%), Space: 10.1 MB (37.20%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -2,21 +2,42 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
+        // redo 
+        
+        // kth largeest ->>> min heap
+        priority_queue<int, vector<int>, greater<int>> pq; // minheap
+        
+        for(auto x : nums){
+            pq.push(x);
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        cout <<"done ---" << endl;
+        
+        return pq.top();
+        
+        
+        // trivial soln --> Sort the array and return nums[k-1] 
+        // TC - O(nlogn) , SC - O(1)
+        
+        
         // Kth element or "Largest" --> either of them suugest "HEAP"
         // after heap is decided, which heap(min/max) to use
         // thererfore "Largest" --> "MIN" Heap is to be used
         
+        // TC - O(nlogk), SC - O(k)
         
-        priority_queue<int, vector<int>, greater<int>> minh;
+//         priority_queue<int, vector<int>, greater<int>> minh;
         
-        for(auto x : nums){
-            minh.push(x);
-            if(minh.size() > k){
-                minh.pop();
-            }
-        }
+//         for(auto x : nums){
+//             minh.push(x);
+//             if(minh.size() > k){
+//                 minh.pop();
+//             }
+//         }
         
-        return minh.top();
+//         return minh.top();
         
     }
 };
