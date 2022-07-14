10d6a42199920f258ce27ded6d9fcb769c8523f9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-15 02:16:31 +0530

    Time: 30 ms (76.46%), Space: 14.4 MB (8.84%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h> 
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,50 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#define p pair<int, ListNode*>
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        priority_queue<p, vector<p>, greater<p> > pq;
-        
-        
         // min heap
-
-        //         for(int i = 0 ; i < lists.size() ; i++){
-//             ListNode* curr_list = lists[i]; // extracting current linked list
-            
-//             if(curr_list != NULL) // if element present in the linked list
-//             {
-//                 pq.push({curr_list -> val, curr_list}); // push into min heap
-//             }
-//         }
         
-        for(auto x : lists){
-            // cout << x << endl;
-            if(x!= nullptr){ // if list is empty, then dont put it in minheap
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq; // minheap
+        
+        for(auto &x : lists){
+            if(x != nullptr){
                 pq.push({x->val, x});
             }
-            
         }
         
-        ListNode* dummy = new ListNode(-1);
         
+        ListNode *dummy = new ListNode(-1);
         ListNode *ptr = dummy;
         
+        // iterate in the pq
         while(!pq.empty()){
-            p front = pq.top();
+            auto front = pq.top();
             pq.pop();
             
-            int x = front.first;
+            int val = front.first;
             
-            ptr->next = new ListNode(x);
+            ListNode *temp = new ListNode(val);
+            ptr->next = temp;
             ptr = ptr->next;
             
             if(front.second->next != nullptr){
                 pq.push({front.second->next->val, front.second->next});
             }
         }
+        
         
         return dummy->next;
         
