d3c9ac5ce7e0ccedf39495ba0a404f9f1202f80e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-15 09:47:01 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (39.16%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        // brute force
+        
+        // store all the words in a vector
+        // reverse the vector 
+        // push all the strings in the answer string with a space
+        
+        // PS -  pop out the last space 
+        
+        
+        vector<string> words;
+        stringstream ss (s);
+        string temp;
+        while(ss >>temp){
+            words.push_back(temp);
+        }
+        
+        reverse(words.begin(), words.end());
+        
+        string ans = "";
+        
+        for(auto &x : words){
+            ans += x;
+            ans += " ";
+        }
+        
+        
+        ans.pop_back() ;  // --> the extra space at the end
+        
+        return ans;
+        
+    }
+};
