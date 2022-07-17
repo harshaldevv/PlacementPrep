d56dc9eab2c421fc09c0862f61c698eb8a9b07c6
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-17 23:01:33 +0530

    Time: 36 ms (32.03%), Space: 16.5 MB (99.52%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -18,94 +18,31 @@ public:
 
 class Solution {
 public:
-    void helper(Node * root){
-        
-        // TC - O(n)
-        // SC - O(1) --> ignoring right now the recursive stack O(n)
-        
-        if(root == nullptr){
-            return ;
-        }
-        
-        if(root->left == nullptr && root->right == nullptr){
-            // leaf node
-            return ;
-        }
-        
-        
-        root->left->next = root->right;
-        
-        helper(root->left);
-        helper(root->right);
-        
-        return ;
-        
-    }
     
-    void connectInnerNodes(Node* root){
+    void dfs(Node *curr, Node* nxt){
+        // using the soln from discuss
         
-        // T(n) = 2* T(n/2) + O(n) --> at every node, we go left and right, so basically
-        // we are taking only the n/2 nodes.  --> recurrence is similar to merge sort
-        // Also for a particular node, we are doing O(n) work 
-        
-        //after solving, we get TC - O(nlogn)
-        // SC - O(1) --> if we ignore recursive stack of O(n)
-        
-        if(root == nullptr){
+        if(curr == nullptr){
             return ;
         }
         
+        curr->next = nxt;
+        dfs(curr->left, curr->right);
         
-        if(root->left == nullptr && root->right == nullptr){
-            // leaf node
-            return ;
+        if(curr->next == nullptr){
+            dfs(curr->right, NULL);
         }
-        
-        
-        Node *p = root->left;
-        Node *q = root->right;
-        
-        // cout << p->val << " , " << q->val << endl;
-        
-        p->next = q;
-        
-        while(p->right && q->left){
-            p->right->next = q->left;
-            p = p->right;
-            q = q->left;
+        else{
+            dfs(curr->right, curr->next->left);
         }
-        
-        connectInnerNodes(root->left);
-        connectInnerNodes(root->right);
         
         return ;
         
+            
     }
     
     Node* connect(Node* root) {
-        
-        if(root == nullptr){
-            return nullptr;
-        }
-        
-        
-        // level order traversal approach was TC - O(n), SC - O(n)
-        
-        // now optimally doing Tc - O(n), SC - O(1)
-        
-        
-        
-        // basically this code of mine will work in T(n) = O(nlogn) , SC - O(1) [recursive stack = O(n)]
-
-        helper(root); // --> takes O(n) time
-        
-        cout << "chal gaya helper" << endl;
-        // connectInnerNodes(root->left, root->right);
-        
-        connectInnerNodes(root);// --> takes TC - O(nlogn)
-        
-        
+        dfs(root, nullptr);
         return root;
-    
     }
 };
