a5661f2c796b31d7ff790ba5337fead52bd877ef
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-19 17:19:04 +0530

    Time: 58 ms (44.36%), Space: 12.3 MB (56.56%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>> &graph, int i , int j){
-        if( i < 0 || i >= graph.size() || j < 0 || j >= graph[0].size() || graph[i][j] != '1'){
-            return;
+    
+    bool isvalid(int i, int j, int m, int n){
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return false;
         }
         
-        graph[i][j] = '2'; // marking as visited;
+        return true;
+    }
+    
+    void doDFS(vector<vector<char>> &grid, int i , int j){
+        int m = grid.size();
+        int n = grid[0].size();
         
-        dfs( graph,  i   , j-1 );
-        dfs( graph,  i   , j+1 );
-        dfs( graph,  i-1 , j   );
-        dfs( graph,  i+1 , j   );
+        if(!isvalid(i,j,m,n)){
+            return ;
+        }
+        
+        if(grid[i][j] !=  '1'){
+            // already visited ('2') or water ('0')
+            return ;
+        }
+        
+        grid[i][j] = '2';
+        
+        int dir[5] = {1,0,-1,0,1};
+        
+        for(int k = 0 ; k < 4 ; k++){
+            int newx = i + dir[k];
+            int newy = j + dir[k+1];
+            doDFS(grid, newx, newy);
+        }
         
         return ;
+           
     }
     int numIslands(vector<vector<char>>& grid) {
         int count = 0;
         
-        for(int i = 0 ; i < grid.size(); i++){
+        for(int i = 0 ; i < grid.size() ; i++){
             for(int j = 0 ; j < grid[0].size() ; j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
+                    doDFS(grid, i, j);
                     count++;
                 }
             }
