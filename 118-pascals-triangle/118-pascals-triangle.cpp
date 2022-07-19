751181dab2d12d4a80983d9ad7b347f8383aac49
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-19 15:37:53 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,50 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int n) {
         vector<vector<int>> ans;
-        
-        return helper(numRows, ans);
-        
-    }
-    
-    vector<vector<int>> helper(int n, vector<vector<int>> res){
-        if(n==1){
-            vector<int> s;
-            s.push_back(1);
-            res.push_back(s);
-            return res;
-        }
-        if(n==2){
-            
-            vector<int> s1;
-            s1.push_back(1);
-            
-            vector<int> s2;
-            s2.push_back(1);
-            s2.push_back(1);
-            
-            res.push_back(s1);
-            res.push_back(s2);
-            
-            return res;
-        }
-        
-        
-        res = helper(n-1, res);
         
         vector<int> temp;
         temp.push_back(1);
-        // for loop
-        vector<int> last_vector = res.back();
-        for(int i = 0 ; i < last_vector.size() -1 ;i++){
-            int x = last_vector[i] + last_vector[i+1];
-            temp.push_back(x);
+        ans.push_back(temp);
+        
+        if(n == 1){
+            return ans;
         }
         
         temp.push_back(1);
+        ans.push_back(temp);
         
-        res.push_back(temp);
+        if(n == 2){
+            return ans;
+        }
+        // cout <<"here" << endl;
         
-        return res;
+        for(int i = 3 ; i <= n ;i++){
+            // cout << "i = " << i << endl;
+            vector<int> holder(i, 1);
+            // cout << "pika = " << ans.back().size() << endl;
+            for(int j = 1 ; j < ans.back().size() ; j++){
+                // cout << "j = " << j << endl;
+                holder[j] = ans.back()[j] + ans.back()[j-1];
+                // cout << "j = " << j << endl;
+            }
+            ans.push_back(holder);
+        }
+        
+        return ans;
     }
 };
