30295663973cb4b3f3809c02a3c172c7435ecaf7
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-21 00:13:33 +0530

    Time: 43 ms (30.62%), Space: 16.3 MB (92.64%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,42 +1,58 @@
 class MinStack {
-private:
-    stack<int> s;
-    stack<int> ss; // supporting stacks --> only stores the minimum elements in it
-
 public:
+    stack<long> st;
+    long  minEle = -1;
     MinStack() {
-        // minstack implementation with O(N) extra space
+        // min stack implementation with CONSTANT SPACE O(1)
         ;
     }
     
-    void push(int val) {
-        s.push(val);
+    void push(long val) {
         
-        if(ss.size() == 0 || val <= ss.top()){
-            ss.push(val);
+        if(st.size() == 0){
+            st.push(val);
+            minEle = val;
         }
-        return;
+        else if(val < minEle){
+            // push 2x - minele
+            st.push(2*val - minEle);
+            minEle = val;
+            
+        }
+        else{
+            st.push(val);
+        }
+        
+        return ;
         
     }
     
     void pop() {
-        int temp = s.top();
-        s.pop();
-        if(temp == ss.top()){
-            ss.pop();
+        // while popping,
+        // if st.top() < minele -->  update the minEle --> 
+        // as our st.top() being less than our minele is a "flag" for us
+        // that we gotta now go back to the minelement that was just before our current minelement
+        
+        if(st.top() < minEle){
+            minEle = 2*minEle - st.top();
         }
         
-        
+        st.pop();
     }
     
     int top() {
-        return s.top();
         
+        if(st.top() < minEle){
+            return minEle;
+        }
+        else{
+            return st.top();
+        }
+ 
     }
     
     int getMin() {
-        return ss.top();
-        
+        return minEle;
     }
 };
 
