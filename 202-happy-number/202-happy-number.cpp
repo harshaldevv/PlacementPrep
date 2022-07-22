5b63a3f61300769d2b74ff6665ae3d6e16f9eebd
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-23 03:04:22 +0530

    Time: 3 ms (62.21%), Space: 5.8 MB (84.47%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,31 +1,66 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int> s;
+    int next(int n){
+       int sum = 0;
+        while(n){
+            int temp = n%10;
+            sum += (temp*temp);
+            n/=10;
+        }
         
-        while(n != 1){
-            if(s.find(n) == s.end()){
-                // not found in our set
-                // therefore insert in the set
-                s.insert(n);
-            }
-            else{
-                // found in our set
-                // aka cycle detected
-                return false;
-            }
-            
-            int sum = 0;
-            while(n){
-                int temp = n%10;
-                sum += (temp*temp);
-                n/= 10;
-            }
-            
-            n = sum;
+        return sum ; // this is our next number 
+        
+        
+    }
+    bool isHappy(int n) {
+        // TC - O(n), SC - O(1)
+        // floyd cycle detection technique
+        
+        int slow = next(n);
+        int fast = next(next(n));
+        
+        while(slow != fast){
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        // collision ho gaya hai ab 
+        // therefore check ab collison jo hua hai woh "1" pe hua hai ya nahi
+        
+        if (fast == 1){
+            return true;
+        }
+        else{
+            return false;
         }
         
         
-        return true;
+//         // TC - O(n), SC - O(n)
+//         unordered_set<int> s;
+        
+//         while(n != 1){
+//             if(s.find(n) == s.end()){
+//                 // not found in our set
+//                 // therefore insert in the set
+//                 s.insert(n);
+//             }
+//             else{
+//                 // found in our set
+//                 // aka cycle detected
+//                 return false;
+//             }
+            
+//             int sum = 0;
+//             while(n){
+//                 int temp = n%10;
+//                 sum += (temp*temp);
+//                 n/= 10;
+//             }
+            
+//             n = sum;
+//         }
+        
+        
+//         return true;
     }
 };
