9193f73545224541aeeab9bae55867b7877074f8
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-28 10:15:36 +0530

    Time: 17 ms (41.11%), Space: 11.2 MB (72.37%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -22,7 +22,14 @@ public:
         // to arr.size(), it means our whole array is filled with zeroes,
         // therefore just return "0";
         
-        
+        // In this below while loop, we are trying to remove the trailing zeroes
+       // as soon as we find the first non zero character, we return the substring 
+        // of our answer string from that index to the end
+       // Ques ? Why did we iterate till "n-1" and why not "n" ? 
+        //--> because if our answer string = "0000", we want our answer = "0"
+       // ie we need one "0" right, therefore we go till the second last character 
+        // of the answer string and not the whole length of the answer string
+
         int i = 0;
         while(i< ans.size() -1){
             if(ans[i] != '0'){
