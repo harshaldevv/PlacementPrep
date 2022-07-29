f9cfeece8cec06607aaf00a01e2bb45c6f12103f
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-07-30 02:09:54 +0530

    Time: 7 ms (51.85%), Space: 8.7 MB (99.47%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& arr) {
-        int l = 0;
-        int r = arr.size() -1;
         int n = arr.size();
         
-        if(n==1){
-            //return arr[n-1];
+        if(n == 1){
             return 0;
         }
+        
+        int l = 0;
+        int r = n-1;
+        
+        
         while(l<=r){
-            int mid = l  + (r-l)/2;
+            int mid = l + (r-l)/2;
             
-            if(mid > 0 && mid < n-1){
-                // check here
+            
+            if(mid >0 && mid < n-1){
                 
-                int prev = mid - 1;
-                int nxt = mid + 1;
-                
-                if(arr[mid] > arr[prev] && arr[mid] > arr[nxt]){
+                if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                    // this is our peak element
                     return mid;
                 }
-                else if(arr[mid] > arr[prev]){
-                    // go right
+                else if(arr[mid] > arr[mid-1] ){
+                    // we should go in the ahead direction 
                     l = mid +1;
                 }
                 else{
-                    r = mid -1; 
+                    r = mid -1;
                 }
             }
             else if(mid == 0){
-                if(arr[0] > arr[1]){
-                    return 0;
-                }
-                else{
+                if(arr[1] >arr[0] ){
                     return 1;
                 }
+                else{
+                    return 0;
+                }
             }
-            else{
-                if(arr[n-2] > arr[n-1] ){
+            else if(mid == n-1){
+                if(arr[n-2] > arr[n-1]){
                     return n-2;
                 }
                 else{
                     return n-1;
                 }
+                
             }
         }
         
