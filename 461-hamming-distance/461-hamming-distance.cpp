25ad59ea773540aa8de24318c217122f3e656894
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 02:30:36 +0530

    Time: 5 ms (14.50%), Space: 5.9 MB (25.89%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -7,7 +7,23 @@ public:
         // therefore using the learning from the above ques
         // basically count the number of setbits in x^y 
         
-        return __builtin_popcount(x^y);
+        // return __builtin_popcount(x^y); --> counting set bits in x^y USING INBUILT FUNCTION
+        
+        // now counting set bits without  --> using the Brian Kernighan’s Algorithm
+        
+        int xorr = x^y;
+        int n = xorr;
+        
+        int ans = 0;
+        
+        while(n){
+            ans++;
+            n = (n & (n-1) );
+
+        }
+        
+        return ans;
+        
         
     }
 };
