05c5ffeeb9e767274cf6a63c556f9ffffc89ff43
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 04:00:44 +0530

    Time: 5 ms (81.00%), Space: 7.8 MB (87.52%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -20,17 +20,30 @@ public:
         
     }
     vector<int> countBits(int n) {
-        vector<int> ans(n+1, -1);
         
-        for(int i = 0 ; i <= n ;i++){
-            ans[i] = countSetBits(i);
+        // TC - O(N), SC - O(1)
+        vector<int> ans(n+1, 0);
+        
+        ans[0] = 0;
+        
+        for(int i = 1 ; i <= n ; i++){
+            ans[i] = ans[i/2] + i%2;   // i%2  = 0 for even and 1 for odd
+            // ans[i] = ans[i/2] + (i&1); // i&1 --> = 0 for even and 1 for odd;
         }
-        
-        // we run a for loop --> O(n) for the for loop
-        // and we take O(logn) for the countSetBit() function
-        // therefore TC = O(nlogn) , SC - O(1) --> no extra space used
-        
         return ans;
+        
+//         // TC = O(nlogn ), SC = O(1) --> is easy, as per follow up
+//         vector<int> ans(n+1, -1);
+        
+//         for(int i = 0 ; i <= n ;i++){
+//             ans[i] = countSetBits(i);
+//         }
+        
+//         // we run a for loop --> O(n) for the for loop
+//         // and we take O(logn) for the countSetBit() function
+//         // therefore TC = O(nlogn) , SC - O(1) --> no extra space used
+        
+//         return ans;
         
         
     }
