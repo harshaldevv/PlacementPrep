638c073fbcdcb89e880c27129c98ac2a19ec1eae
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 02:50:28 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (54.74%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int helper(vector<int> &nums, int i, int sum){
-        if(i == nums.size()){
+        if(i >= nums.size()){
             return sum;
         }
-        int include = helper(nums, i+1, sum ^nums[i]);
-        int exclude = helper(nums, i+1, sum);
-        return include + exclude;
+        
+        int include = helper(nums, i+1, sum ^nums[i]); // include the ith element
+        int exclude = helper(nums, i+1, sum); // exlcuding the ith element
+        
+        return include+exclude ; 
+        // Ab hume iska toh sum karna hoga
+        // cuz we hume sum karna hai include wala case + exclude wala case
+        
     }
     int subsetXORSum(vector<int>& nums) {
-        return helper(nums, 0, 0);
+        int i = 0;
+        int sum = 0;
+        return helper(nums, i, sum);
+        
     }
 };
