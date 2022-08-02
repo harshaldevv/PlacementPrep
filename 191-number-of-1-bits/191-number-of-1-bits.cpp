b1e4359b72d9a629b8ee8e05a7d8424664378c93
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 15:41:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.87%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
+    int countSetBits(uint32_t n){
+        int ans = 0;
+        
+        while(n){
+            ans++;
+            n = n&(n-1);
+        }
+        return ans;
+    }
     int hammingWeight(uint32_t n) {
         
-        return __builtin_popcount(n);
+        // return __builtin_popcount(n); --> using inbuilt function
+        
+        // without inbuilt func
+        return countSetBits(n);
         
     }
 };
