8ba800a72598a59f56039d86f4663593a6a7ad11
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 13:55:39 +0530

    Time: 4 ms (21.80%), Space: 5.8 MB (79.12%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findComplement(int num) {
+        // TC - O(N), SC - O(1)
         
         // basically toggle every bit
         // therefore do n ^ 1<<i
@@ -13,7 +14,9 @@ public:
        while(i>=0){
            if( ( n & (1<<i) ) != 0 ){
                // found MSB (set bit) --> if condition ki value ab 1 ho gyi hai
-               // kyuki 1 (MSB) & 1  (from 1<<i) --> in dono ka and equal to 1 hota hai
+               // kyuki 1 (MSB) & 1  (from 1<<i) --> in dono ka "and (&)" 
+               // equal to 1 hota hai
+               
                break;
            }
            i--;
