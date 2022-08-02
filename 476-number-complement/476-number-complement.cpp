8ecb43f59dd28e999435a5f563254c86d7d72e27
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-02 13:44:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.22%) - LeetHub

---
+++ 476-number-complement/476-number-complement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        
+        // basically toggle every bit
+        // therefore do n ^ 1<<i
+        
+        int n = num;
+        
+        // now we gotta find the MSB first
+        int i = 31;
+        
+       while(i>=0){
+           if( ( n & (1<<i) ) != 0 ){
+               // found MSB (set bit) --> if condition ki value ab 1 ho gyi hai
+               // kyuki 1 (MSB) & 1  (from 1<<i) --> in dono ka and equal to 1 hota hai
+               break;
+           }
+           i--;
+       }
+        
+        // now toggle each bit from this i onwards
+        for( ; i >=0 ; i--){
+            n = n ^ 1<<i;
+        }
+        
+        return n;
+        
+    }
+};
