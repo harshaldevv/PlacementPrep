f6336450519ab9597b51853888e1975a6522713f
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-04 01:34:05 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (63.50%) - LeetHub

--- 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
+++ 861-score-after-flipping-matrix/861-score-after-flipping-matrix.cpp
@@ -21,6 +21,26 @@ public:
     
    
     int matrixScore(vector<vector<int>>& grid) {
+        /*
+        
+        Intution behind the greedy approach -->
+        To maximise our answer from each row, we wish to have the first bit(left) se
+        1 --> therefore jin rows me jinka first bit aka first column me zero hai 
+        usme har row ke bits ko flip karde --> well hume technically toh first col ko hi flip 
+        karna chahiye tha but ques me likha hai row me kisi bit ko flip kia toh poori row flip hogi
+        
+        
+        Now for step 2 --> now we'll obviously avoid flipping the bits of the first column
+        Therefore ab hum jo bhi kuch karenge woh second column se chalu karenge
+        
+        Now we wish to maximise the number of 1s in this column, kyuki 0 hoga toh 
+        jab hum finally (2^something) karenge har row ke lie toh agar (2^something)*0 
+        karenge toh fyda nahi lekin (2^something)*1 karenge toh fyda hoga, hamara number 
+        increase hi hoga. Isliye har column me jao, agar number of ones is less than 
+        the "size of row /2" --> ie "numberOfOnes <= (rows/2)" --> toh is column me
+        flip all the bits 
+        
+        */
         
         int m = grid.size();
         int n = grid[0].size();
@@ -58,6 +78,7 @@ public:
             for(int j = 0 ; j < n ; j++){
                 if(grid[i][j] == 1){
                     sum += (grid[i][j])*(1<<(n-j-1));
+                    // 1<< n-j-1 --> means  2^(n-j-1);
                 }
             }
             // cout << "sum = " << sum << endl;
