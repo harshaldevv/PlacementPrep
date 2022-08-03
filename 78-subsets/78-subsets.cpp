d06a3d9d87732c39fcee6c676cf724f2c8900335
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-04 01:39:50 +0530

    Time: 0 ms (100.00%), Space: 10.9 MB (38.51%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,63 +1,34 @@
 class Solution {
 public:
-    void solve(vector<int> &nums, int i, vector<int> &holder, vector<vector<int>> &ans){
-        if(i == nums.size()){
+    
+    void helper(vector<int> &nums, int i, vector<int> &holder, vector<vector<int>> &ans){
+        
+        if(i>= nums.size()){
             ans.push_back(holder);
-            return;
+            return ;
         }
         
-        int e = nums[i];
+        // exclude
+        helper(nums, i+1, holder, ans);
         
         //include
-        holder.push_back(e);
-        solve(nums, i+1, holder, ans);
+        holder.push_back(nums[i]);
+        helper(nums, i+1, holder, ans);
         
-        
-        //exclude
+        //backtrack
         holder.pop_back();
-        solve(nums, i+1, holder, ans);
-        
         
         return ;
+            
+        
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
         
-//         vector<vector<int>> ans;
-        
-//         vector<int> holder;
-        
-//         solve(nums,0, holder, ans);
-        
-//         return ans;
-        
-        // RECURSIVE (BACKTRACK) METHOD ^^
-        
-        
-        // ITERATIVE METHOD --> OP
-        
-        // EXPLAINATION BELOW
-        /*
-        [] ----------> []
-        [1] ---------> [] ,  [1]
-        [1,2] -------> [] ,  [1] , [2] , [1,2]
-        [1,2,3] -----> [] ,  [1] , [2] , [1,2], [3], [1,3], [2,3], [1,2,3]
-        */
-        // basically upar walo ko sabko lelo without nums[i] (exclude case), then usko include karlo (nums[i])
-        
         vector<vector<int>> ans;
-        ans.push_back({});
-        
-        for(int i = 0 ; i < nums.size(); i++){
-            int sz = ans.size();
-            
-            for(int j = 0 ; j < sz ; j++){
-                vector<int> copy = ans[j];
-                copy.push_back(nums[i]);
-                ans.push_back(copy);
-            }
-        }
+        vector<int> holder;
+        helper(nums, 0, holder, ans );
         
         return ans;
+        
     }
 };
