b939761fce242c7c5017e01f6870287d5f7dbb35
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-09 21:10:49 +0530

    Time: 19 ms (23.39%), Space: 9.5 MB (86.76%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // every element appears three times except for one, which appears exactly once.
+        
+        int ans = 0;
+        for(int i = 0 ; i < 32 ; i++){
+            
+            int sum = 0;
+            
+            for(int j = 0 ; j < nums.size() ; j++){
+                if( (nums[j]>>i & 1) == 1 ){
+                    // i th bit is 1
+                    sum++;
+                }
+            }
+            
+            sum %= 3;
+            
+            if(sum != 0){
+                ans |= sum << i;
+            }
+            
+        }
+        
+        return ans;
+        
+        
+    }
+};
