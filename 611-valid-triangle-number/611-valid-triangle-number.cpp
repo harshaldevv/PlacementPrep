9478815f1b6f0ae49fcd2defb96aaa5861a1cf29
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-25 00:01:30 +0530

    Time: 609 ms (22.37%), Space: 12.9 MB (55.17%) - LeetHub

---
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int doBinarySearch(int l, int r, int target, vector<int> &nums){
+        int start = l;
+        while(l<=r){
+            
+            int mid = l + (r-l)/2;
+            
+            if(nums[mid] < target){
+                l = mid +1;
+            }
+            else{
+                r = mid -1;
+            }
+            
+        }
+        
+        return l - start;
+    }
+    
+    int triangleNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int cnt = 0;
+        
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = i +1 ; j < n ; j++){
+//                 for(int k = j +1  ; k < n ; k++){
+                    
+//                     int a = nums[i];
+//                     int b = nums[j];
+//                     int c = nums[k];
+                    
+//                     if(a+b > c && b +c > a && a+c > b){
+//                         cnt++;
+//                     }
+//                 }
+//             }
+//         }
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < n ; i++){
+            for(int j = i  +1 ; j < n ; j++){
+                int a = nums[i];
+                int b = nums[j];
+                
+                // binary search to find numbers less than a+b
+                // if found incremenet cnt
+                int temp = doBinarySearch(j+1, n-1, a+b , nums);
+                cnt += temp;
+                // cout << "temp = " << temp <<endl;
+            }
+        }
+        
+        return cnt;
+        
+    }
+};
