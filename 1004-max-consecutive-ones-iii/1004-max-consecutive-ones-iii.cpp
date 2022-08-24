7472f9875ac704a3bcb3ef737fb767670165126c
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-25 01:28:28 +0530

    Time: 72 ms (86.11%), Space: 55.4 MB (92.12%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,75 +1,34 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
         
         int l = 0;
         int r = 0;
-        int n = nums.size();
-        int zero = 0;
+        int zerocount = 0;
         
         int ans = 0;
         
-        while(r < n){
+        for( ; r < n ; r++){
             if(nums[r] == 0){
-                zero++;
+                zerocount++;
             }
             
-            if(zero > k){
-                
-                while(zero > k){
+            if(zerocount > k){
+                while(zerocount > k){
                     if(nums[l] == 0){
-                        zero--;
+                        zerocount--;
                     }
                     l++;
                 }
             }
             
-            ans = max(ans, r - l +1);
-            
-            r++;
-            
+            ans = max(ans, r-l+1);
         }
         
+        
+        
         return ans;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         int l = 0;
-        
-//         int n = nums.size();
-        
-//         int ans = 0;
-        
-//         int zero = 0;
-        
-//         for(int r = 0 ; r < n ; r++){
-//             if(nums[r] == 0){
-//                 zero++;
-//             }
-            
-//             while(zero > k){
-//                 if(nums[l] == 0){
-//                     zero--;
-//                 }
-//                 l++;
-//             }
-            
-//             ans = max(ans, r - l +1);
-//         }
-        
-//         return ans;
         
     }
 };
