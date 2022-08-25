3dc6993bdba6d06145d1639af08eb6a465de7170
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-08-25 13:52:47 +0530

    Time: 117 ms (75.44%), Space: 58.9 MB (80.58%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -9,20 +9,48 @@ public:
         int ans = min(nums[l], nums[r]) * (r-l);
         
        
+//         while(l<r){
+            
+//             int h = min(nums[l], nums[r]);
+//             ans = max(ans, (r-l)*h);
+            
+//             while(nums[l] <= h && l < r){
+//                 l++;
+//             }
+//             while(nums[r] <= h && l < r){
+//                 r--;
+//             }
+            
+            
+       
+//         }
+        
+        
+        // Both the above algos work -->
+        
         while(l<r){
             
             int h = min(nums[l], nums[r]);
             ans = max(ans, (r-l)*h);
             
-            while(nums[l] <= h && l < r){
+            // jo pointer's value aka nums[l] or nums[r] chota hai  
+            // , hum uske acc step lenge.
+            // upar wale algo se less complex types hai
+            //mtlb jis pointer pe merkeo min (ie H), ab hum
+            // uske aage ya piche jayenge
+            // agar L wala min tha, toh hum uske aage jayenge in the hope ki hume
+            // lamba pole mil jaye
+            // agar R wala min hota hai toh hum uske peeche (ie r--) jayenge 
+            // in the hope ki hume lamba pole mil jaye
+            
+            if(nums[l] < nums[r]){
                 l++;
             }
-            while(nums[r] <= h && l < r){
+            else{
                 r--;
             }
+
             
-            
-       
         }
         
         return ans;
