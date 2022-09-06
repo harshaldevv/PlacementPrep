fd8f473eaad39c049294a11e31b75e6568198f44
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-07 01:43:45 +0530

    Time: 7 ms (87.71%), Space: 11 MB (89.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,63 +1,42 @@
 class Solution {
 public:
     
-    void solve(int i, vector<int> &nums, int sum, vector<int> &holder, vector<vector<int>> &ans, int target){
+    void solve(int i, int sum, vector<int> &holder, vector<vector<int>> &ans, vector<int> &nums, int k){
         
-        if(sum == target){
+        if(i >= nums.size() || sum > k ){
+            return ;
+        }
+        
+        if(sum == k ){
             ans.push_back(holder);
             return ;
         }
         
-        if(sum > target || i >= nums.size()){
-            return ; // yahan koi fyda hai
-            // terminatino condition
-            // agar humari target hi greater than sum, ho gaya toh toh koi fyda hi nahi hai
-            // aage recursion calls lagani ki
-            
-            // aur agar i = nums.size(), ho gaya, toh aage koi fyda nahi jane ki cuz array ka end aa gaya
-        }
+        //include
+        holder.push_back(nums[i]);
+        solve(i, sum + nums[i], holder, ans, nums, k);
         
-        int e = nums[i];
-        // include
-        holder.push_back(e);
-        solve(i, nums, sum+e, holder, ans, target);
-        
-        // NOTE --> 
-        // is ques me kuch bhi nahi tha
-        // bas ye tha ki if you are including the element e, 
-        // then unlike question (subset, subset 2, combination)
-        // jahan hum include krne ke baad bhi i+1, kar dete hai
-        // yahan hum log i+1, nahi karenge (in case of inclusion of element e)
-        // the reason being becasue we are allowed to take a number any number of times
-        // by taking "i", instead of "i+1", we are still able to consider the numbs[i].
-        // had we had done i+1, then we would have lost the opportunity to include nums[i] in our
-        // sum again.
-        
-        
-        // but if we are not taking element e, then we can obviously go ahead with i+1;
-        
-        //exclude
+        // backtrack
         holder.pop_back();
-        solve(i+1, nums, sum, holder, ans, target);
+        
+        // exclude
+        solve(i+1, sum, holder, ans, nums, k);
         
         return ;
         
-        
     }
-    
-    
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        
-        // sort(nums.begin(), nums.end());
+    vector<vector<int>> combinationSum(vector<int>& nums, int k) {
         
         vector<vector<int>> ans;
         
         vector<int> holder;
         
-        int sum = 0;
+        int i = 0;
+        int s = 0;
         
-        solve(0, nums, sum, holder, ans, target);
+        solve(i, s, holder, ans , nums, k);
         
         return ans;
+        
     }
 };
