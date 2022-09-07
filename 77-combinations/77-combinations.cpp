9ebba1969cdbe15e40876cd7039799a2a96bec3e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-07 12:18:01 +0530

    Time: 51 ms (40.81%), Space: 18 MB (33.34%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,41 +1,48 @@
 class Solution {
 public:
-    
-   
-    
-    void solve(vector<int>&nums, int i, int n, int k, vector<int> &holder,  vector<vector<int>> &ans){
-        
+    void solve(int i,vector<int> &nums, vector<int> &holder,vector<vector<int>> &ans, int k){
         
         if(holder.size() == k){
             ans.push_back(holder);
             return ;
         }
         
-        if(i == n){
+        if(i >= nums.size()){
             return ;
         }
         
+        //exclude
+        solve(i+1, nums, holder, ans, k);
+        
         //include
         holder.push_back(nums[i]);
-        solve(nums, i+1, n, k, holder,ans);
+        solve(i+1, nums, holder, ans, k);
         
-        //exclude
+        //backtrack
         holder.pop_back();
-        solve(nums, i+1, n, k, holder, ans);
         
         return ;
+        
+        
     }
     vector<vector<int>> combine(int n, int k) {
         
-        vector<vector<int>> ans;
-        vector<int> holder; // empty set
         vector<int> nums;
         
-        for(int i = 1 ; i <= n ; i++){
+        for(int i =1 ; i <= n ; i++){
             nums.push_back(i);
         }
         
-        solve(nums, 0, n, k, holder, ans);
+        vector<vector<int>> ans;
+        
+        vector<int> holder;
+        
+        int i = 0;
+        
+        solve(i, nums, holder, ans, k);
+        
+        
         return ans;
+        
     }
 };
