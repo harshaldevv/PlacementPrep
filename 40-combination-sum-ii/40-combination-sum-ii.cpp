a842852776336dbc67139cbf939d83ba2ece4708
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-07 12:01:21 +0530

    Time: 15 ms (32.78%), Space: 14.9 MB (14.62%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,51 +1,49 @@
 class Solution {
 public:
-    void solve(int i, int sum, vector<int> &nums, vector<int> &holder, vector<vector<int>> &ans, int target, bool pre){
+    void solve(int i, int sum, vector<int> &holder, vector<vector<int>> &ans, vector<int> &nums,int k, bool prev){
         
-        if(sum == target){
+        if(sum == k){
             ans.push_back(holder);
             return ;
         }
         
-        if(sum> target || i >= nums.size()){
+        if(i >= nums.size() || sum > k){
             return ;
         }
         
-        int e = nums[i];
+        
         
         //exclude
-        solve(i+1, sum, nums, holder, ans, target, false);
-        if(i>0 && nums[i] == nums[i-1] && !pre){
+        solve(i+1, sum, holder, ans, nums, k, false);
+        
+        //if we ignore x , then we need to ignore all further instances of x
+        if(i >0 && nums[i] == nums[i-1] && !prev){
             return ;
         }
         
-        //key thing here
-        // if im ignoring x, then i have to ignore all the occurences all of x that come 
-        // aka i also wont take those occurencs of x
-        
         // include
-        holder.push_back(e);
-        solve(i+1, sum+e, nums, holder, ans, target, true);
+        holder.push_back(nums[i]);
+        solve(i+1, sum + nums[i] , holder, ans, nums, k, true);
         
+        //backtrack
         holder.pop_back();
         
-        // //exclude
-        // holder.pop_back();
-        // solve(i+1, sum, nums, holder, ans, target);
-        
         return ;
-        
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& nums, int k) {
+        
         sort(nums.begin(), nums.end());
         
+        
         vector<vector<int>> ans;
+        
         vector<int> holder;
+        
+        int i = 0;
         int sum = 0;
         
-        solve(0, sum, nums, holder, ans, target,false);
+        solve(i, sum, holder, ans, nums, k, false);
         
         return ans;
-        
     }
 };
