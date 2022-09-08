19351070d496b565006e492b140cfc584dd0ef41
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-09 01:04:57 +0530

    Time: 36 ms (75.02%), Space: 6.3 MB (95.16%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,71 +1,76 @@
 class Solution {
 public:
-    bool ispossible(vector<vector<char>> &board, int currRow, int currCol, char c){
-        
-        // entire  row me char c nahi hona chahiye except i, j
-        for(int col = 0 ; col < 9 ; col++){
-            if(board[currRow][col] == c){
+    
+    bool ispossible(vector<vector<char>> &board, int currrow, int currcol, char k){
+        // check this whole row
+        for(int i = 0 ; i < 9 ; i++){
+            if(board[currrow][i] == k){
                 return false;
             }
         }
         
-        // entire col me char c nahi hona chahiye except i, j
-        
-        for(int row = 0 ; row < 9 ; row++){
-            if(board[row][currCol] == c){
+        // check this whole col
+        for(int i = 0 ; i < 9  ; i++){
+            if(board[i][currcol] == k){
                 return false;
             }
         }
         
+        // check in the 3x3 box
         
-        // us 3x3 me char c nahi hona chahiye except i, j
-        int desiredRow = (currRow/3)*3;
-        int desiredCol = (currCol/3)*3;
-        
-        for(int i = desiredRow ; i < desiredRow +3 ; i++){
-            for(int j = desiredCol ; j < desiredCol +3 ; j++ ){
-                if(board[i][j] == c){
+        int x = 3 * (currrow/3);
+        int y = 3 * (currcol/3);
+        for(int i = 0 ; i < 3 ; i++){
+            for(int j = 0 ; j < 3 ; j++){
+                if(board[ x + i ][ y + j ] == k){
                     return false;
                 }
             }
         }
         
-        
-        
         return true;
         
+        
     }
-    bool solve(vector<vector<char>> &board){
-        for(int i = 0 ; i< 9 ; i++){
+    bool solve(vector<vector<char>> &board, int n){
+        
+        for(int i = 0 ; i < 9 ; i++){
             for(int j = 0 ; j < 9 ; j++){
+                
+                // cout << i << " , " << j << endl;
+                
                 if(board[i][j] == '.'){
-                    for(char c = '1' ; c <= '9' ; c++){
-                        if(ispossible(board, i, j, c)){
-                            board[i][j] = c;
+                    
+                    for(char k = '1' ; k <= '9' ; k++ ){
+                        
+                        if(ispossible(board, i, j, k)){
+                            board[i][j] = k;
                             
-                            if(solve(board)){
+                            if(solve(board, n)){
                                 return true;
+                                // dont backtrack now , cuz the ques asked for "one" 
+                                // valid sudoku 
+                                // had the ques asked for all possible sudokus then we
+                                // hum ye solve() wale ke baad backtrack pe bhi jate
                             }
-                            else{
-                                board[i][j] = '.';
-                            }
+                            
+                            board[i][j] = '.';
                         }
                     }
-                    return false; 
-                    // we checked all numbers from 1 to 9, but none of them worked
-                    // therefore we are returning false 
+                    return false;
                 }
             }
         }
         
         return true;
+        
     }
     void solveSudoku(vector<vector<char>>& board) {
         int n = board.size();
-        int m = board[0].size();
         
+        solve(board, n);
         
-        solve(board);
+        return ;
         
     }
 };
