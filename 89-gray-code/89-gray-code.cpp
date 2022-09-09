91479517df3c6c305695ecfc6f491910ce117397
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-09 08:28:57 +0530

    Time: 20 ms (37.55%), Space: 11.8 MB (63.76%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,50 +1,14 @@
 class Solution {
 public:
-    vector<string> code(int n){
-        if(n==1){
-            return {"0", "1"};
-        }
+    vector<int> grayCode(int n) {
+        int p = 1 << n ;
         
-        vector<string> prev = code(n-1);
-        
-        vector<string> ans;
-        
-        for(auto x : prev){
-            string s = "0" + x;
-            ans.push_back(s);
-        }
-        for(int i = prev.size() -1 ; i >= 0 ; i--){
-            string s = "1" + prev[i];
-            ans.push_back(s);
+        vector<int> ans;
+        for(int i = 0 ; i < p ; i++){
+            ans.push_back(i^(i/2));
         }
         
         return ans;
-    }
-    vector<int> grayCode(int n) {
-        // recursive --> too much likhna
-        
-//         vector<string> stringAns;
-//         stringAns = code(n);
-        
-//         vector<int> ans;
-//         for(auto x : stringAns){
-//             int numb = stoi(x,0,2);
-//             ans.push_back(numb);
-//         }
-        
-//         return ans;
-        
-        
-
-        // iterative code
-        
-        vector<int> v;
-        long long int p=pow(2,n);
-        cout << "p = " << p << endl;
-        for(int i=0; i<p; i++){
-            v.push_back(i^(i/2));
-        }
-        return v;
         
     }
 };
