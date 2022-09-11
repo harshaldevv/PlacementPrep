89bda208a8c834dc9dd09b96919725c04e173d73
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-11 22:24:01 +0530

    Time: 612 ms (7.26%), Space: 6 MB (51.65%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,92 +1,67 @@
 class Solution {
 public:
     
-    void nextPermutations (vector<int> &nums){
+    void nextPerm(vector<int> &nums){
+        // left small, right big
         int n = nums.size();
-        // we want left small , right big
         
-        int i = n-2;
+        int i =  n-2;
         
-        while( i >= 0 && nums[i] >= nums[i+1]){
-            i--;
-        }
-        
-        if( i == -1){
-            // nums is reverse sorted
-            reverse(nums.begin(), nums.end());
-            return ;
-        }
-        
-        //find just bigger number than nums[i];
-        
-        int next = n-1;
-        for( ; next > i ; next--){
-            if(nums[next] > nums[i]){
+        for( ; i >= 0 ; i--){
+            if(nums[i] < nums[i+1]){
                 break;
             }
         }
         
-        // swap nums[i] and nums[next]
-        swap(nums[i], nums[next]);
+        if(i==-1){
+            //reverse sorted
+            reverse(nums.begin(), nums.end());
+            return ;
+        }
         
-        // reverse from i +1 to end
+        // next bigger number 
+        int j = n-1;
+        
+        for( ; j > i ; j--){
+            if(nums[j] > nums[i]){
+                break;
+            }
+        }
+        
+        // swap
+        swap(nums[i], nums[j]);
+        
         reverse(nums.begin() + i +1 , nums.end());
         
         return ;
+        
     }
     string getPermutation(int n, int k) {
         
-        //doing on 11th july using next permutations cuz thats the first approach that struck me
         vector<int> nums;
+        
+        // string N = to_string(n);
+        
         for(int i = 1 ; i <= n ; i++){
             nums.push_back(i);
         }
         
-        for(int i = 0; i < k-1 ; i++){
-            nextPermutations(nums);
+        // for(auto & x : nums){
+        //     cout << x << " ";
+        // }
+        
+        for(int i = 0 ; i < k -1; i++){
+            nextPerm(nums);
         }
         
         string ans = "";
-        for(auto &x : nums){
-            ans += to_string(x) ;
+        
+        for(auto & x : nums){
+            ans += to_string(x);
         }
         
         return ans;
         
         
-//         vector<int> numbers;
-//         for(int i = 1 ; i <= n ; i++){
-//             numbers.push_back(i);
-//         }
-        
-//         int f = 1;
-//         vector<int> fact;
-//         fact.push_back(1);
-//         for(int i = 1 ; i <= n ; i++){
-//             f*=i;
-//             fact.push_back(f);
-//         }
-         
-        
-//         k--;
-        
-//         string ans = "";
-        
-//         while(n>0){
-            
-//             int factorial = fact[n-1];
-            
-//             int index = k/factorial;
-//             // cout << "index = " << index << endl;
-//             ans+= to_string(numbers[index]);
-            
-//             numbers.erase(numbers.begin() + index);
-//             n--;
-//             k=k%factorial;
-
-//         }
-        
-//         return ans;
-
     }
 };
