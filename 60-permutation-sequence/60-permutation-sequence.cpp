8d020b0c0fbce85405f9f0a2df7748a00d3528d9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-11 23:33:04 +0530

    Time: 4 ms (49.14%), Space: 6 MB (82.01%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
-    
-    void nextPerm(vector<int> &nums){
-        // left small, right big
-        int n = nums.size();
-        
-        int i =  n-2;
-        
-        for( ; i >= 0 ; i--){
-            if(nums[i] < nums[i+1]){
-                break;
-            }
-        }
-        
-        if(i==-1){
-            //reverse sorted
-            reverse(nums.begin(), nums.end());
-            return ;
-        }
-        
-        // next bigger number 
-        int j = n-1;
-        
-        for( ; j > i ; j--){
-            if(nums[j] > nums[i]){
-                break;
-            }
-        }
-        
-        // swap
-        swap(nums[i], nums[j]);
-        
-        reverse(nums.begin() + i +1 , nums.end());
-        
-        return ;
-        
-    }
     string getPermutation(int n, int k) {
+        vector<int> numb;
         
-        vector<int> nums;
-        
-        // string N = to_string(n);
-        
-        for(int i = 1 ; i <= n ; i++){
-            nums.push_back(i);
+        for(int i=1 ; i <= n ; i++){
+            numb.push_back(i);
         }
         
-        // for(auto & x : nums){
-        //     cout << x << " ";
-        // }
+        vector<int> fact;
+        int f = 1;
+        fact.push_back(f); // 0! is equal to 1
         
-        for(int i = 0 ; i < k -1; i++){
-            nextPerm(nums);
+        for(int i =1 ; i <= n ; i++){
+            f *= i;
+            fact.push_back(f);
         }
+        
+        k--;
         
         string ans = "";
         
-        for(auto & x : nums){
-            ans += to_string(x);
+        while(n){
+            int factt = fact[n-1];
+            int index = k / factt;
+            ans += to_string(numb[index]);
+            numb.erase(numb.begin() + index);
+            
+            k %= factt;
+            n--;
         }
         
         return ans;
-        
         
     }
 };
