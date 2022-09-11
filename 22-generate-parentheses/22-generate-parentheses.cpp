e7d3b8be62de59ba6180410fd29038b7c38a6525
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-12 01:11:58 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (84.06%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,64 +1,54 @@
 class Solution {
 public:
-    void helper(int open, int close, string &holder, vector<string> &ans){
-        cout << "open, close = " << open << " , " << close << endl;
-        if (open == close && open == 0 && close == 0 ){
-            // stopping / base condition
+    
+    void solve(int open, int close, string &holder, vector<string> &ans){
+        
+        if(open < 0){
+            return ;
+        }
+        if(close < 0){
+            return ;
+        }
+        if(open == 0 && close == 0){
             ans.push_back(holder);
             return ;
         }
         
-        if(open < 0 ){
-            // -> warna open minus 1 , minus 2, aise jaata rahega, therefore but 
-            // we wish to stop as soon as we get open < 0
-            
-            // we have to stop after we have zero opening brackets
-            return ;
-        }
         
+        // case I 
         if(open == close){
             holder.push_back('(');
-            helper(open-1, close, holder, ans);
+            open--;
+            solve(open, close, holder, ans);
             holder.pop_back();
         }
-        else if(open > close){
-            // ye toh hoga hi nahi condition, bcz we are always using up 
-            // an open bracket
-        }
-        else if(open < close){
-            // now we have to 2 options -> either insert open bracket or insert
-            // a closing brakcet
+        else{
+            // unequal hai open and close
+            // 2 cases ---> solve(open -1, close) & solve(open, close -1);
             
-            // insert an opening bracket
             holder.push_back('(');
-            helper(open-1, close, holder, ans);
+            solve(open -1, close, holder, ans);
+            
             holder.pop_back();
             
-            // insert an closing bracket
             holder.push_back(')');
-            helper(open, close-1, holder, ans);
+            solve(open, close-1, holder, ans);
+            
             holder.pop_back();
             
         }
         
         return ;
     }
-    
     vector<string> generateParenthesis(int n) {
-        // jab bhi close > open --> we get 2 choices --> ie closing wali choice bhi mil rahi hai
-        // why -? Open ka bracket hoga tabhi toh close ka brakcet lagaoge.
-        
-        // if open == close --> we only get the open wali choice. close bracket wali choice 
-        // nahi hai. Whyso ? Becuase if we use a close brcket, then after using that 
-        // closed bracket we'll get an unbalanced string (right after) we've used 
-        // the closing brakcet, which is an undesirable output/situation. 
-        
-        int openBrackets = n;
-        int closeBrackets = n;
         vector<string> ans;
         string holder = "";
+        int open = n;
+        int close = n;
         
-        helper(openBrackets, closeBrackets, holder, ans );
+        solve(open, close, holder, ans);
+        
         return ans;
+        
     }
 };
