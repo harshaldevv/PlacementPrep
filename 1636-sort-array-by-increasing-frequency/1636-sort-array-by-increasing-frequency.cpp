14d4a65e0a93cd32d2aa14be89f1a72f3619a323
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-13 19:03:15 +0530

    Time: 28 ms (7.23%), Space: 11.1 MB (87.99%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,47 +1,32 @@
-#define pii pair<int, int>
 class Solution {
 public:
-    class compare_fun{
-        public :
-        bool operator()(pii p1, pii p2){
-            if(p1.first == p2.first){
-                return p1.second < p2.second;
-            }
-            return p1.first > p2.first;
-        }
-    };
+    
+    
+    
     vector<int> frequencySort(vector<int>& nums) {
+        
         unordered_map<int, int> mp;
-        for(auto &x : nums){
+        
+        for(auto & x : nums){
             mp[x]++;
         }
         
-        //min heap;
+        // vector<int> ans;
         
-        priority_queue<pii, vector<pii>, compare_fun > minheap;
+        // need a custom comparator cuz If multiple values have 
+        // the same frequency, sort them in decreasing order.
         
-        for(auto &x : mp){
-            int elem = x.first;
-            int freq = x.second;
-            minheap.push({freq, elem});
-        }
-        
-        
-        vector<int> ans;
-        while(minheap.size() > 0){
-            auto TOP = minheap.top();
-            minheap.pop();
-            int f = TOP.first;
-            int e = TOP.second;
-            while(f--){
-                ans.push_back(e);
+        // min heap
+        sort(nums.begin(), nums.end(), [&mp](auto &a, auto &b) -> bool {
+            if(mp[a] == mp[b]){
+                return a > b;
             }
+            else{
+                return mp[a]  < mp[b];
+            }
+        } );
+                
             
-        }
-        
-        return ans;
-        
-        
-        
+            return nums;
     }
 };
