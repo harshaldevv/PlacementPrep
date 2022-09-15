7dafaddabb97fe52ef1c5c711ce658a5cb6794e5
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-15 16:34:09 +0530

    Time: 32 ms (28.11%), Space: 13.3 MB (83.51%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sum(self, a, b):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        
+        # return a + b
+        return a + b
+        
