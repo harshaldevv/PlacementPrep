b31e38b56a28b5209d23fcb65a3cb29e82ec2437
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-15 19:32:35 +0530

    Time: 3 ms (92.77%), Space: 8.8 MB (89.59%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& arr) {
-        int n = arr.size();
+    
+    int getPeak(vector<int> &nums){
         
-        if(n == 1){
+        int n = nums.size();
+        if(n==1){
             return 0;
         }
         
@@ -14,15 +15,16 @@ public:
         while(l<=r){
             int mid = l + (r-l)/2;
             
-            
-            if(mid >0 && mid < n-1){
+            if(mid !=  0  && mid != n-1){
+                // mid belongs from 1 to n - 1
+                int prev = nums[mid-1];
+                int curr = nums[mid +1];
                 
-                if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
-                    // this is our peak element
+                if(nums[mid] > prev && nums[mid]  > curr){
                     return mid;
                 }
-                else if(arr[mid] > arr[mid-1] ){
-                    // we should go in the ahead direction 
+                else if(nums[mid] > prev && nums[mid] < curr){
+                    // go right
                     l = mid +1;
                 }
                 else{
@@ -30,24 +32,34 @@ public:
                 }
             }
             else if(mid == 0){
-                if(arr[1] >arr[0] ){
-                    return 1;
+                if(nums[0] > nums[1]){
+                    return 0;
                 }
                 else{
-                    return 0;
+                    return 1;
                 }
             }
             else if(mid == n-1){
-                if(arr[n-2] > arr[n-1]){
-                    return n-2;
-                }
-                else{
+                if(nums[n-1] > nums[n-2]){
                     return n-1;
                 }
-                
+                else{
+                    return n-2;
+                }
             }
         }
         
+        
         return -1;
+        
+    }
+    int findPeakElement(vector<int>& nums) {
+        
+       
+        int peak = getPeak(nums);
+        
+        // return nums[peak];
+        return peak;
+        
     }
 };
