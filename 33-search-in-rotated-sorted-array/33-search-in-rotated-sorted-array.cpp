49746a3639f521bcfdd8dbc6f5b682360fd0f1bd
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-19 12:47:36 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (29.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,44 +2,25 @@ class Solution {
 public:
     int getPivot(vector<int> &arr){
         int n = arr.size();
-        int l = 0;
-        int r = n -1;
+        int l =0;
+        int r = n-1;
         
-        while(l <= r ){
-            
-            if(arr[l ] <= arr[r]){
+        while(l<=r){
+
+            if(arr[l] <= arr[r]){
                 return l;
             }
             
             int mid = l + (r-l)/2;
             
-            int prev = (mid -1 + n)%n;
-            int next = (mid +1 ) %n;
+            int prev = (mid -1 + n )%n;
+            int next = (mid+1)%n;
             
-            if(arr[mid ] <= arr[prev] && arr[mid] <= arr[ next ]){
+            if(arr[mid] <= arr[prev] && arr[mid] <= arr[prev]){
                 return mid;
             }
+            // go in unsorted part now
             else if(arr[l] <= arr[mid]){
-                // go in right half
-                l = mid + 1;
-            }
-            else{
-                r = mid -1;
-            }
-        }
-        
-        return 0;
-    }
-    
-    int BS(vector<int> &arr, int l, int r, int target){
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if(arr[mid] < target){
-                // go right
                 l = mid +1;
             }
             else{
@@ -49,17 +30,31 @@ public:
         
         return -1;
     }
+    int BS(int l, int r , vector<int> &arr, int target ){
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                r = mid -1;
+            }
+            else{
+                l = mid +1;
+            }
+        }
+        
+        return -1;
+    }
     int search(vector<int>& nums, int target) {
         
-        // rotated sorted array --> means find pivot sabse pehle toh
         int pivot = getPivot(nums);
-        cout << "pivot = "  << pivot << endl;
-        
-        
-        int ans = BS(nums, 0, pivot -1, target);
+        cout << "pivot = " << pivot << endl;
+        int ans = BS(0, pivot-1, nums, target );
         
         if(ans == -1){
-            return BS(nums, pivot, nums.size() -1, target);
+            return BS(pivot, nums.size() - 1, nums, target);
         }
         else{
             return ans;
