09c6968f6141a0a37270a7864079952502c8098b
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-20 12:00:38 +0530

    Time: 57 ms (65.38%), Space: 89.4 MB (64.55%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -5,52 +5,49 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        if(n1>n2){
+        if(n2 < n1){
             return findMedianSortedArrays(nums2, nums1);
         }
         
-        int length = n1 + n2 ;
+        // cout << "here222" << endl;
+       
         
         int l = 0;
+        int len = n1 + n2 ;
         int r = n1;
         
         while(l<=r){
+            
             int mid = l + (r-l)/2;
             
             int cut1 = mid;
-            int cut2 = (length+1)/2 - cut1;
+            int cut2 = (n1 + n2 + 1)/2 - cut1;
             
-            int l1, l2, r1, r2;
-         
+            int l1 = (cut1 -1 >= 0) ? nums1[cut1-1]: INT_MIN;
+            int l2 = (cut2 -1 >= 0) ? nums2[cut2-1]: INT_MIN;
+            int r1 = (cut1 < n1) ? nums1[cut1]: INT_MAX;
+            int r2 = (cut2 < n2) ? nums2[cut2]: INT_MAX;
             
-            l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1] ;
-            l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1] ;
             
-            r1 = cut1 == n1 ? INT_MAX : nums1[cut1] ;
-            r2 = cut2 == n2 ? INT_MAX : nums2[cut2] ;
-            
-            if(l1<=r2 && l2 <= r1){
-                // if even
-                if(length %2 == 0){
-                    double ans = max(l1, l2) + min(r1, r2);
-                    return ans/2;
+            if(l1 <= r2 && l2 <= r1){
+                if( ((len + 1) %2) == 0 ){
+                    // odd
+                    return max(l1, l2);
                 }
                 else{
-                    return (double)(max(l1, l2));
+                    return ( max(l1, l2 ) + min(r1, r2) ) /2.0 ;
                 }
-                
             }
-            else if(l1>r2){
-                // make cut1 go left
+            else if(l1 > r2){
                 r = mid -1;
             }
             else{
-                // l2 > r1
                 l = mid +1;
             }
+               
         }
         
-        return 0.0;
+        return -1;
         
     }
 };
