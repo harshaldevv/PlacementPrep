9bd0dca90b566f5d1b5e320f3efdfcb625a13503
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-20 14:10:30 +0530

    Time: 114 ms (87.55%), Space: 13 MB (6.08%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,49 +1,30 @@
 class Solution {
 public:
-    
-    int doBinarySearch(vector<int> &nums, int target, int l, int r){
-        // find all X st X < target 
-        int start = l;
-        int ans = 0;
-        
-        while(l <= r){
-            
-            int mid = l + (r-l)/2;
-            
-            if( nums[mid] >= target){
-                
-                r = mid -1;
-            }
-            else if(nums[mid]  < target){
-                // ans = mid;
-                l = mid +1;
-            }
-        }
-        
-        return l - start ;
-    }
     int triangleNumber(vector<int>& nums) {
-        
         sort(nums.begin(), nums.end());
-        
         int n = nums.size();
-        
         int ans = 0;
-        
-        for(int i = 0 ; i <  n ; i++){
-            for(int j = i +1 ; j < n ; j++){
+        for(int i = 2 ; i < n ; i++){
+            
+            int c = nums[i];
+            
+            int l = 0;
+            int r = i-1;
+            
+            while(l<r){
+                int a = nums[l];
+                int b = nums[r];
                 
-                int a = nums[i];
-                int b = nums[j];
-                
-                // c < a + B find
-                // find all c such that c < sum
-                
-                ans += doBinarySearch(nums, a+b, j+1, nums.size() -1);
+                if(a + b > c){
+                    ans += (r - l );
+                    r--;
+                }
+                else{
+                    l++;
+                }
             }
         }
         
         return ans;
-        
     }
 };
