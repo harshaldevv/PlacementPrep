1b9efe4ce972cccd8783965fdf3da2a231132210
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-21 17:21:25 +0530

    Time: 203 ms (20.42%), Space: 58.9 MB (80.15%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,61 +1,24 @@
 class Solution {
 public:
-    int maxArea(vector<int>& nums) {
-        int n = nums.size();
-        
+    int maxArea(vector<int>& height) {
+        int n = height.size();
         int l = 0;
         int r = n-1;
-        
-        int ans = min(nums[l], nums[r]) * (r-l);
-        
-       
-//         while(l<r){
-            
-//             int h = min(nums[l], nums[r]);
-//             ans = max(ans, (r-l)*h);
-            
-//             while(nums[l] <= h && l < r){
-//                 l++;
-//             }
-//             while(nums[r] <= h && l < r){
-//                 r--;
-//             }
-            
-            
-       
-//         }
-        
-        
-        // Both the above algos work -->
+        int ans = INT_MIN;
         
         while(l<r){
+            int temp = min(height[l], height[r]) * (r-l);
+            ans = max(ans, temp );
             
-            int h = min(nums[l], nums[r]);
-            ans = max(ans, (r-l)*h);
-            
-            // jo pointer's value aka nums[l] or nums[r] chota hai  
-            // , hum uske acc step lenge.
-            // upar wale algo se less complex types hai
-            //mtlb jis pointer pe merkeo min (ie H), ab hum
-            // uske aage ya piche jayenge
-            // agar L wala min tha, toh hum uske aage jayenge in the hope ki hume
-            // lamba pole mil jaye
-            // agar R wala min hota hai toh hum uske peeche (ie r--) jayenge 
-            // in the hope ki hume lamba pole mil jaye
-            
-            if(nums[l] < nums[r]){
+            if(height[l] < height[r]){
                 l++;
             }
             else{
                 r--;
             }
-
-            
         }
         
         return ans;
-        
-        
         
     }
 };
