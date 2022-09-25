63faa00036c1643f1d1d059f28e7cc040dbedc25
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-25 22:19:10 +0530

    Time: 44 ms (10.16%), Space: 18.5 MB (36.63%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         
-        int curr = 0;
-        for(int i = 1 ; i < nums.size() ; i++){
-            if(nums[i] != nums[curr]){
-                nums[curr+1] = nums[i];
-                curr++;
-            }
-        }
-        
-        return curr +1 ; // +1 bcz curr toh indexing ke lie use ho raha hai and zero 
-        // based indexing ha so yeah , isliye 1 based me lane ke lie +1 kar dia
+        int i = 0;
+        for (int n : nums)
+            if (i < 1 || n > nums[i-1])
+                nums[i++] = n;
+        return i;
         
     }
 };
