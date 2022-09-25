5413710749eb3f2d3d42b92ecc44ea8aa82c37dd
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-25 21:31:24 +0530

    Time: 14 ms (82.06%), Space: 18.5 MB (10.92%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         
         int curr = 0;
         for(int i = 1 ; i < nums.size() ; i++){
-            if(nums[i] != nums[i-1]){
+            if(nums[i] != nums[curr]){
                 nums[curr+1] = nums[i];
                 curr++;
             }
