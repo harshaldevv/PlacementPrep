0604dfb313a051b5c30364f95228a3c86ea48a83
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-09-25 12:38:09 +0530

    Time: 340 ms (23.29%), Space: 46.4 MB (33.18%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
@@ -3,49 +3,33 @@ public:
     
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         
-        // https://www.interviewbit.com/problems/subarrays-with-distinct-integers/
-        
-        
-        // trick --> Exact(k) = atMost(k) - atMost(k-1);
-        // very HARD ques :")
-        
-        
-        return subaaraysWithAtMost_K_Distinct(k, nums)- subaaraysWithAtMost_K_Distinct(k-1, nums);
-        
-        /*
-        https://leetcode.com/problems/subarrays-with-k-different-integers/discuss/640450/c%2B%2B-code-easy-solution-using-map-and-sliding-window-with-approach
-        
-        */
-            
-        
+        return helper(k, nums) - helper(k-1, nums);
     }
     
-    int subaaraysWithAtMost_K_Distinct(int k , vector<int> &nums){
-        
+    int helper (int k, vector<int> &nums){
         int l = 0;
         int r = 0;
         int n = nums.size();
-        
-        int cnt = 0;
+        int ans = 0;
         
         unordered_map<int, int> mp;
-        
-        while(r<n){
-            
-            mp[nums[r]]++;
-            
+        while(r < n){
+            auto x = nums[r];
+            mp[x]++;
             while(mp.size() > k){
-                mp[nums[l]]--;
-                if(mp[nums[l]] == 0){
-                    mp.erase(nums[l]);
-                }
+                auto prev = nums[l];
+                mp[prev]--;
                 l++;
+                if(mp[prev] == 0){
+                    mp.erase(prev);
+                }
             }
             
-            cnt += (r-l +1);
+            // cout << "r , l = " << r << " " << l << endl;
+            ans += ( r - l +1);
+            
             r++;
         }
-        
-        return cnt;
+        return ans;
     }
 };
