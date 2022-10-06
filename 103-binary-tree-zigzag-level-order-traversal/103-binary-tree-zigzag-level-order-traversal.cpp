ac760ef370e31da5796378296ddb7f9ccdbb5276
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-10-06 11:45:16 +0530

    Time: 6 ms (46.74%), Space: 12 MB (75.03%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,9 +12,7 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        // do level order traversal
-        // and then reverse every 2nd sublist in it
-        
+        bool zigzag = true;
         
         vector<vector<int>> ans;
         
@@ -22,130 +20,45 @@ public:
             return ans;
         }
         
-        queue<TreeNode*> q;
+        
+        queue<TreeNode*>q;
+        
         q.push(root);
-        bool toRev = false;
         
-//         while(!q.empty()){
-//             int sz = q.size();
+        while(!q.empty()){
             
-//             vector<int> level;
-            
-//             while(sz--){
-//                 auto front = q.front();
-//                 q.pop();
+            auto sz = q.size();
+            vector<int> level(sz,-1);
+            for(int i = 0 ; i < sz ; i++){
+                auto front = q.front();
+                q.pop();
                 
-//                 level.push_back(front->val);
+                // level.push_back(front->val);
                 
-//                 if(front->left){
-//                     q.push(front->left);
-//                 }
-                
-//                 if(front->right){
-//                     q.push(front->right);
-//                 }
-//             }
-            
-//             if(toRev){
-//                 reverse(level.begin(), level.end());
-//                 toRev = false;
-//             }
-//             else{
-//                 //push level as it is 
-//                 toRev = true;
-//             }
-            
-//             ans.push_back(level);
-//         }
-        
-        // ^^ this approach used reverse() 
-        
-        // below approach without using reverse()
-        
-//         while(!q.empty()){
-            
-//             int sz = q.size();
-//             vector<int>level(sz, -1);
-            
-//             for(int i = 0 ; i < sz ; i++){
-                
-//                 auto front = q.front();
-//                 q.pop();
-                
-//                 if(!toRev){
-//                     level[i] = front->val;
-//                 }
-//                 else{
-//                     level[sz - 1 - i] = front->val;
-//                 }
-                
-//                 if(front->left){
-//                     q.push(front->left);
-//                 }
-                
-//                 if(front->right){
-//                     q.push(front->right);
-//                 }
-//             }
-            
-//             toRev = !toRev;
-//             ans.push_back(level);
-            
-//         } --> OP
-        
-        
-        //now actually doing zig zag traversal
-        
-        deque<TreeNode*> dq;
-        dq.push_back(root);
-        bool left2Right = true;
-        
-        while(!dq.empty()){
-            int sz = dq.size();
-            
-            vector<int> level;
-            
-            for(int i = 0 ; i< sz ; i++){
-                
-                if(!left2Right){
-                    auto front = dq.back();
-                    dq.pop_back();
-                    
-                    level.push_back(front->val);
-                    
-                    if(front->right){
-                        dq.push_front(front->right);
-                    }
-                    
-                    if(front->left){
-                        dq.push_front(front->left);
-                    }
+                if(zigzag){
+                    level[i] = front->val;
                     
                 }
-                else{
-                    auto front = dq.front();
-                    dq.pop_front();
-                    
-                    level.push_back(front->val);
-                    
-                    if(front->left){
-                        dq.push_back(front->left);
-                    }
-                    
-                    if(front->right){
-                        dq.push_back(front->right);
-                    }
+                else{   
+                    level[sz - 1 - i] = front->val;
                     
                 }
                 
+                if(front->left){
+                    q.push(front->left);
+                }
+
+                if(front->right){
+                    q.push(front->right);
+                }
             }
             
-            
             ans.push_back(level);
-            left2Right = !left2Right ;
             
+            zigzag = !zigzag;
         }
         
         return ans;
+        
     }
 };
