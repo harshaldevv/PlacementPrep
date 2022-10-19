5a3ab6d62626114c3023774fa4b71957dff5dd0a
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-10-19 18:50:15 +0530

    Time: 28 ms (83.87%), Space: 18 MB (36.66%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     void helper(int numb, vector<int> &holder, vector<vector<int>> &ans, int n, int k){
         if(k == 0){
+            // this condn helps me to get my answer 
             ans.push_back(holder);
             return ;
         }
         
         if(numb > n){
+            // stopping condition for further recursions
             return ;
         }
         
-        //exclude
-        
+        //exclude (exclude the current number , therefore no decrease in the size of holder ie "k")
         helper(numb +1, holder, ans, n, k);
         
         
-        //include
+        //include (include the current number , therefore decrease in the size of holder by 1, ie "k = k-1")
         holder.push_back(numb);
         helper(numb +1, holder, ans, n, k-1);
         
