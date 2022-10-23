95c569185c32cc60877e82b42d11fb9ed2f39683
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-10-23 15:18:23 +0530

    Time: 161 ms (5.67%), Space: 43.8 MB (5.03%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -1,12 +1,42 @@
 class Solution {
 public:
+    
+    vector<string> helper(int n){
+        if(n==1){
+            return {"0", "1"};
+        }
+        
+        vector<string> temp = helper(n-1);
+        
+        vector<string> ans ;
+        
+        for(int i = 0 ; i < temp.size() ; i++){
+            string s = temp[i] + "0";
+            ans.push_back(s);
+        }
+        
+        
+        for(int i = temp.size() -1 ; i >= 0  ; i--){
+            string s = temp[i] + "1";
+            ans.push_back(s);
+        }
+        
+        return ans;
+        
+        
+        
+    }
     vector<int> grayCode(int n) {
-        int p = pow(2, n);
+        
+        if(n==1){
+            return {0,1};
+        }
+        
+        vector<string> temp = helper(n);
         
         vector<int> ans;
-        
-        for(int i = 0 ; i < p ; i++){
-            ans.push_back(i^i/2);
+        for(auto x : temp){
+            ans.push_back(stoi(x,0,2));
         }
         
         return ans;
