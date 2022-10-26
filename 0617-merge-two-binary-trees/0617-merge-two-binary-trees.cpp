63aa82a311b0b588ffe3030281c930db2c99a0e0
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-10-27 00:35:22 +0530

    Time: 35 ms (97.63%), Space: 32.4 MB (61.66%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -11,53 +11,39 @@
  */
 class Solution {
 public:
+    
     TreeNode* merge(TreeNode *p, TreeNode *q){
-        if(!p && !q){
-            return nullptr;
+        
+        if(!p && q){
+            return q;
         }
         
         if(p && !q){
             return p;
         }
         
-        if(!p && q){
-            return q;
+        
+        if(!p && !q){
+            
+            return nullptr;
+            
         }
-        
-        
-        // dono are non null
-        // sum up
         
         p->val += q->val;
         
-        TreeNode *leftTree = merge(p->left, q->left);
-        
-        TreeNode *rightTree = merge(p->right, q->right);
-        
-        p->left = leftTree;
-        p->right = rightTree;
+        p->left = merge(p->left, q->left);
+        p->right = merge(p->right, q->right);
         
         return p;
+        
     }
-    
     TreeNode* mergeTrees(TreeNode* p, TreeNode* q) {
-        if(!p && !q){
-            return nullptr;
-        }
         
-        if(p &&!q){
-            return p;
-        }
         
-        if(!p && q){
-            return q;
-        }
         
         return merge(p,q);
         
         // return p;
-        
-        
         
     }
 };
