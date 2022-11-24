fa8997142728c7efb68a222e9523ee8ed4fd1daa
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-24 14:23:18 +0530

    Time: 55 ms (7.79%), Space: 12.3 MB (15.97%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,41 +1,73 @@
 class Solution {
 public:
-    
-    bool dfs(vector<vector<int>> &grid, int i, int j){
-        if(i <0  || j < 0 || i >= grid.size() || j>= grid[0].size() ){
-            return false ;
+    bool isValid(int &i, int &j, int &m , int &n){
+        return i >=0 && i < m && j >=0 && j < n;
+    }
+    void doDFS(vector<vector<int>> &grid, int i, int j, int &m , int &n){
+        if(!isValid(i,j,m,n) || grid[i][j] != 0){
+            return ;
         }
-        if(grid[i][j] != 0){
-            return true; // either land or already visited
+        
+        grid[i][j] = 2;
+        
+        
+        vector<int> dir{1,0,-1,0,1};
+        
+        for(int k = 0 ; k < 4 ; k++){
+            doDFS(grid, i + dir[k], j + dir[k+1], m, n);
         }
         
-        grid[i][j] = 2; // marking land as viisted
-        
-        bool top = dfs(grid, i-1, j); 
-        bool down = dfs(grid, i+1, j);
-        bool left = dfs(grid, i, j-1);
-        bool right = dfs(grid, i, j+1);
-        
-        return top && down && left && right;
+        return ;
         
     }
     int closedIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int count = 0;
+        // 0 = land
+        // 1 = water
         
-        for(int i = 0 ; i < grid.size()  ; i++){
-            for(int j = 0 ; j < grid[0].size() ; j++){
-                if(grid[i][j] == 0){
-                    if(dfs(grid, i, j)){
-                        //cout << " i , j = " << i << " , " << j << endl;
-                        count++;
-                    }
-                    
+        //border wale 0s ko exclude karo abhi
+        for(int i = 0 ; i < m ; i++){
+            if(grid[i][0] == 0) {
+                // left column or right column
+                doDFS(grid, i, 0, m, n);
+            }
+        }
+        
+        for(int i = 0 ; i < m ; i++){
+            if(grid[i][n-1] == 0) {
+                doDFS(grid, i, n-1, m, n);
+            }
+        }
+        
+        for(int j = 0; j < n ; j++){
+            if(grid[0][j] == 0 ){
+                // first row or last row
+                doDFS(grid, 0, j, m, n);
+            }
+        }
+        
+        for(int j = 0; j < n ; j++){
+            if(grid[m-1][j] == 0 ){
+                // first row or last row
+                doDFS(grid, m-1, j, m, n);
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i =0 ; i < m ; i++){
+            for(int j = 0 ; j < n ;j++){
+                if(grid[i][j]== 0){
+                    doDFS(grid, i, j, m, n);
+                    ans++;
                 }
             }
         }
         
-        return count;
+        return ans;
+        
         
     }
 };
