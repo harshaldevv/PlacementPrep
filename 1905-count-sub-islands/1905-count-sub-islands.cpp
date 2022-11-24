f0163fd41e3fbd0bc6977a3345a7bb5b363716b2
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-24 15:48:15 +0530

    Time: 1236 ms (5.65%), Space: 150.1 MB (15.58%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -1,52 +1,57 @@
 class Solution {
 public:
-    bool add;
-    void dfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j){
-        if(i < 0 || j < 0 || i >= grid2.size() || j>= grid2[0].size()){
+    bool isvalid(int &i, int &j, int &m, int &n){
+        return i >=0 && i < m && j >=0 && j < n;
+    }
+    
+    void dfs(vector<vector<int>> &grid2, vector<vector<int>> &grid1, int i, int j, int &m, int &n, bool &something){
+        
+        if(!isvalid(i,j,m,n)){
             return ;
         }
+        
         if(grid2[i][j] != 1){
-            // either water or already visited;
             return ;
         }
+        
         if(grid1[i][j] == 0){
-            // useless piece of island
-            add = false;
+            // useless piece
+            //but we'll still have to continue our recursion
+            something = false;
         }
         
         grid2[i][j] = 2;
         
-        // dfs(grid1, grid2, i+1, j);
-        // dfs(grid1, grid2, i, j-1);
-        // dfs(grid1, grid2, i-1, j);
-        // dfs(grid1, grid2, i, j+1);
         
-        int dir[] = {1, 0, -1, 0, 1};
-        for(int k = 0 ; k <4 ; k++){
-            dfs(grid1, grid2, i +dir[k], j + dir[k+1]);
+        vector<int> dir{1,0,-1,0,1};
+        
+        for(int k = 0 ; k < 4 ; k++){
+            dfs(grid2, grid1, i + dir[k], j + dir[k+1], m, n, something);
         }
+        
+        
         return ;
+        
     }
+    
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        
-        int m = grid2.size();
-        int n = grid2[0].size();
-        
-        int count = 0;
+        int m = grid1.size();
+        int n = grid1[0].size();
+        int ans = 0;
         
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(grid2[i][j] == 1){
-                    add = true;
-                    dfs(grid1, grid2, i, j);
-                    if(add){
-                        count++;
+                    bool something = true;
+                    dfs(grid2, grid1, i, j, m, n, something);
+                    if(something){
+                        ans++;
                     }
+                    
                 }
             }
         }
         
-        return count;
-        
+        return ans;
     }
 };
