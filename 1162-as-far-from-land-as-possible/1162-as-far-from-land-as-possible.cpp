7278809aa7de66aae492e03640af4b755e2008f9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-24 20:03:04 +0530

    Time: 241 ms (20.35%), Space: 48.9 MB (9.74%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,61 +1,57 @@
 class Solution {
 public:
-    bool isValid(int i, int j, int n, vector<vector<int>>& grid ){
-        if(i>= 0 && j >=0 && i < n && j < n && grid[i][j] == 0){
-            return true;
-        }
-        return false;
+    bool isvalid(int &i, int &j, int &m, int &n){
+        return i>=0 && i < m && j >= 0 && j < n;
     }
     int maxDistance(vector<vector<int>>& grid) {
-        int ans;
         
-        int n = grid.size();
-        
-        queue<pair<int, int>> q;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j< n ; j++){
-                if(grid[i][j] == 1){ 
-                    // why land cell se search chalu
-                    
-                    // do something
-                    // do bfs
-                    q.push({i,j});
+        int m = grid.size();
+        int n = grid[0].size();
+        int land = 0; // land = 1 
+        int water = 0; // water = 0
+        queue<vector<int>> q;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 1){
+                    land++;
+                    q.push({i,j,0});
+                }
+                else{
+                    water++;
                 }
             }
         }
-        if(q.size() == n*n || q.size() == 0){ 
-            // sara land hi land hai ya sara pani pani hai
+        
+        if(land == 0 || water == 0){
             return -1;
         }
         
-        ans = 0;
+        int maxdist = 0;
+        
+        vector<int> dir{1,0,-1,0,1};
         
         while(!q.empty()){
-            int sz = q.size();
-            ans++;
+            auto sz = q.size();
+            maxdist++;
             while(sz--){
-                auto p = q.front();
+                auto front = q.front();
                 q.pop();
+                int currx = front[0];
+                int curry = front[1];
                 
-                int x = p.first;
-                int y = p.second;
-                
-                int dir[] = {1,0,-1, 0, 1};
                 for(int k = 0 ; k < 4 ; k++){
-                    int newX = x + dir[k];
-                    int newY = y + dir[k+1];
+                    int newx = currx + dir[k];
+                    int newy = curry + dir[k+1];
                     
-                    if(isValid(newX, newY, n, grid)){
-                        // is valid checks ki grid ke bahar toh nahi chala gaya
-                        //and ki newX and newY pe pani haina 
-                        q.push({newX, newY});
-                        grid[newX][newY] = 2; // marking that water cell as visited
+                    if(isvalid(newx, newy, m,n) && grid[newx][newy] == 0){
+                        q.push({newx, newy});
+                        grid[newx][newy] = 2;
                     }
                 }
             }
         }
         
-        return ans-1;
+        return maxdist-1;
         
     }
 };
