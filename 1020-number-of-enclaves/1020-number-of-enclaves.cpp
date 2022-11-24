3f8fec741e714e01d4bdf00d9c8851a8d9a25ee9
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-24 14:47:11 +0530

    Time: 96 ms (79.96%), Space: 54.3 MB (5.02%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,58 +1,54 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<int>> &grid, int i, int j){
-        if( i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
-            return;
-        }
-        if(grid[i][j] != 1){
-            return ; // either visited or sea
+    bool isValid(int &i, int &j, int &m, int &n){
+        return i >=0 && i < m && j >=0 && j < n;
+    }
+    void doDFS(vector<vector<int>> &grid, int i, int j, int &m, int &n){
+        if(!isValid(i,j,m,n) || grid[i][j] != 1){
+            // either not valid coordinates or visited already
+            return ;
         }
         
         grid[i][j] = 2;
         
-        int dir[] = {1,0,-1,0,1}; // dir array
+        vector<int> dir{1,0,-1,0,1};
         
-        for(int k = 0 ; k < 4 ; k++){
-            dfs(grid, i + dir[k], j + dir[k+1]);
+        for(int k =0 ; k <4 ; k++){
+            doDFS(grid, i + dir[k], j + dir[k+1], m,n);
         }
         
         return ;
-    }
-    
-    int numEnclaves(vector<vector<int>>& grid) {
         
-
+        
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
-        // we fill/visit all those cells with 1 which are at boundary or
-        // are somehow connected to a boundary cell which has value = 1
+        //mark visited to all the 1s that are at the boundary and those 1s that are connected to such
+        // 1s (jo boundary pe 1s hai)
+        
+        int ans = 0;
+        
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if( i== 0 || j == 0 || i == m-1 || j == n-1){
-                    dfs(grid, i, j);
+                if(i == 0 || i== m-1 || j == 0 || j == n-1){
+                    if(grid[i][j] == 1){
+                        doDFS(grid, i, j, m, n);
+                    }
                 }
             }
         }
         
-        // now that we've visited and marked those cells,
-        //we'll just count the remaining number of land with cell value = 1
-        // koi dfs nahi lagana yahan
-        
-        // had it been ki count the number of components, toh dfs lagata 
-        // like in --> https://leetcode.com/submissions/detail/708895770/
-            
-        int count = 0;
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+            for(int j = 0 ; j <n ; j++){
                 if(grid[i][j] == 1){
-                    count++;
-                    
+                    ans++;
                 }
             }
         }
-        return count;
+        
+        return ans;
         
     }
 };
