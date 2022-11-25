3c9ec8afc9dad395234ffd2907b4d5a1587ed4bd
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-26 00:46:52 +0530

    Time: 18 ms (30.37%), Space: 13.7 MB (13.80%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -6,63 +6,62 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int t = -1;
-        queue<vector<int>> q; // we will always push rotten oranges ONLY (even during BFS)
-        int rottenCount = 0;
-        int freshCount = 0;
-        for(int i = 0 ; i < m ; i++){
+        
+        queue<vector<int>> q;
+        int fresh =0 ;
+        
+        for(int i = 0; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == 2){
-                    // rotten
-                    rottenCount++;
+                if(grid[i][j] == 2)   {
+                    
+                 // rotten orange
                     q.push({i,j});
-                }
+                 }
                 else if(grid[i][j] == 1){
-                    freshCount++;
+                    fresh++;
                 }
             }
         }
         
-        vector<int> dir{1,0,-1,0,1};
+        if(fresh == 0){
+            return 0;
+        }
         
+        int t = 0;
+        vector<int> dir{1,0,-1,0,1};
         while(!q.empty()){
-            auto sz = q.size();
-            bool rotten = false ; // captures if we rot some oranges or not
+            int sz = q.size();
+            t++;
             while(sz--){
                 auto front = q.front();
                 q.pop();
                 int currx = front[0];
                 int curry = front[1];
                 
-                for(int k = 0 ; k < 4 ; k++){
+                for(int k = 0 ; k <4 ; k++){
                     int newx = currx + dir[k];
                     int newy = curry + dir[k+1];
                     
-                    if(isvalid(newx, newy, m,n)){
-                        if(grid[newx][newy] == 1){
-                            grid[newx][newy] = 2;
-                            freshCount--;
-                            rotten = true;
-                            q.push({newx, newy});
-                        }                        
-                    }   
+                    if(isvalid(newx, newy, m,n) && grid[newx][newy] == 1){
+                        grid[newx][newy] = 2;
+                        fresh--;
+                        q.push({newx, newy});
+                    }
                 }
+                
             }
-            
-            t++;
         }
         
-        if(freshCount == 0){
-            if(t==-1){
-                return 0;
-            }
-            else{
-                return t;
-            }
-        }
-        else{
+        // cout << "fresh = " << fresh << endl;
+        if(fresh >0){
+            // cout << "here" << endl;
             return -1;
         }
+        else{
+            return t-1;
+        }
+        
+        
         
     }
 };
