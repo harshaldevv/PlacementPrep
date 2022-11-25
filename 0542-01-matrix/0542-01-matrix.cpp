7de41a6341e6858d87e3b4b55c6336ace0a55017
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-26 02:27:54 +0530

    Time: 207 ms (20.65%), Space: 57.9 MB (5.98%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool isvalid(int &i, int &j, int &m, int &n){
-        return i>=0 && i < m && j >=0 && j< n;
+    bool isvalid(int i, int j, int m, int n){
+        return i>= 0 && i < m && j >=0 && j < n;
     }
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<int>> ans(m, vector<int> (n,-1));
-        queue<vector<int>> q;
+        vector<vector<int>> ans(m, vector<int>(n, 0));
         
-        // push all 1s
-        for(int i = 0 ; i < m ; i++){
+        queue<vector<int>> q;
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        
+        for(int i = 0 ; i < m ;i++){
             for(int j = 0 ; j < n ; j++){
+                
                 if(grid[i][j] == 0){
-                    ans[i][j] = 0;
                     q.push({i,j});
+                    vis[i][j] = true;
                 }
             }
         }
-        
         vector<int> dir{1,0,-1,0,1};
-        
-        int steps = 0;
+        int dist = 0;
         
         while(!q.empty()){
-            auto sz = q.size();
-            
+            int sz = q.size();
             while(sz--){
                 auto front = q.front();
                 q.pop();
@@ -35,18 +34,22 @@ public:
                 int currx = front[0];
                 int curry = front[1];
                 
-                for(int k = 0 ; k < 4 ; k++){
+                
+                for(int k = 0 ; k <4 ; k++){
                     int newx = currx + dir[k];
                     int newy = curry + dir[k+1];
                     
-                    if(isvalid(newx, newy, m, n) && ans[newx][newy] == -1){
+                    if(isvalid(newx,newy,m,n) &&  !vis[newx][newy]){
                         ans[newx][newy] = ans[currx][curry] +1;
+                        vis[newx][newy] = true;
                         q.push({newx, newy});
                     }
-                }
+                    
+                }   
             }
         }
         
         return ans;
+        
     }
 };
