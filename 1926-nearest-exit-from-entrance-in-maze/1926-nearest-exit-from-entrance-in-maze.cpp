5e022f86de2804d374aa32c57dafb4bbb2401389
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-25 15:20:42 +0530

    Time: 457 ms (7.92%), Space: 42.2 MB (11.20%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,94 +1,60 @@
 class Solution {
 public:
-    bool isvalid(int i, int j ,vector<vector<char>>& maze ){
-        if(i < 0 || j < 0 || i >= maze.size() || j >= maze[0].size()){
-            return false;
-        }
-        return true;
+    bool isvalid(int &i, int &j, int &m, int &n){
+        return i>=0 && i< m && j >= 0 && j < n;
     }
-    
-    bool boundarycell(int i, int j, vector<vector<char>>& maze  ){
-        int m = maze.size();
-        int n = maze[0].size();
-        int dir[] = {+1, 0, -1, 0, +1};
-        
-        if(maze[i][j] == '.'){
-            for(int k = 0 ; k < 4 ; k++){
-                int newx = i + dir[k];
-                int newy = j + dir[k+1];
-                
-                if(isvalid(newx, newy, maze ) == false){
-                    // matlab grid se bahar chala gaya newx newy
-                    // therefore i,j is a boundary cell
-                    return true;
-                }
-            }
-            
-            
-            // if(i == 0 || j == 0 || i == m-1 || j == n-1){
-            //     return true;
-            // }
-        }
-        
-        return false;
-        
+    bool isborder(int &i, int &j,int &m, int &n){
+        return (i == 0 || i == m-1 ||j == 0 || j == n-1);
     }
-    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+    int nearestExit(vector<vector<char>>& grid, vector<int>& entrance) {
+        // "+" -> wall
+        // "." -> empty
+        int m = grid.size();
+        int n = grid[0].size();
         
-        int m = maze.size();
-        int n = maze[0].size();
+        //bfs
+        int ox = entrance[0];
+        int oy = entrance[1];
         
-        int x = entrance[0];
-        int y = entrance[1];
         
         vector<vector<bool>> vis(m, vector<bool>(n, false));
+        vector<int> dir{1,0,-1,0,1};
         
         
-        queue<pair<int, int>> q;
-        q.push({x,y});
-        vis[x][y] = true;
+        queue<vector<int>> q;
+        q.push({ox,oy});
+        vis[ox][oy] = true;
         
         int steps = 0;
-        
-        int dir[5] = { +1, 0, -1, 0, +1};
-        
         while(!q.empty()){
             int sz = q.size();
             steps++;
+            
             while(sz--){
-                auto f = q.front();
+                
+                auto front = q.front();
                 q.pop();
-                
-                int currx = f.first;
-                int curry = f.second;
-                
-                // cout << "current = ( " << currx << " , " << curry << " )" << endl; 
-                
-                if( (currx != x) || (curry != y)){
-                    // cout << " here " << currx << " , " << curry << endl;
-                    if(boundarycell(currx, curry, maze)){
-                        return steps-1;
-                    }
-                }
-                // cout <<"hello " << endl;
-                
+                int currx = front[0];
+                int curry = front[1];
                 
                 for(int k = 0 ; k < 4 ; k++){
                     int newx = currx + dir[k];
                     int newy = curry + dir[k+1];
                     
-                    if(isvalid(newx, newy,maze) && maze[newx][newy] != '+' && !vis[newx][newy]){
-                        q.push({newx, newy});
+                    if(isvalid(newx,newy,m,n) && !vis[newx][newy] && grid[newx][newy] == '.'){
+                        
+                        if(isborder(newx, newy, m,n)){
+                            return steps;
+                        }
+                        
                         vis[newx][newy] = true;
+                        q.push({newx, newy});
                     }
                 }
-                
             }
         }
         
         return -1;
-        
-        
-        
+
     }
 };
