c557aa3bd34d840a5f6efb6da83279888f4330a2
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-26 17:32:30 +0530

    Time: 61 ms (42.26%), Space: 13.3 MB (99.54%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
-    bool biparatite(int &curr, vector<vector<int>> &graph, vector<int> &color){
+    
+    bool biparatite(int curr, vector<vector<int>> &graph, vector<int> &color){
+        
         if(color[curr] == -1){
             color[curr] = 1;
         }
         
         for(auto &next : graph[curr]){
-            if(color[next] == color[curr]){
-                return false;
-            }
-            else if(color[next] == -1){
-                // color it ("next")
+            if(color[next] == -1){
                 color[next] = 1 - color[curr];
                 if(!biparatite(next, graph, color)){
                     return false;
                 }
             }
+            else if(color[next] == color[curr] ){
+                return false;
+            }
         }
         
         return true;
     }
+    
     bool isBipartite(vector<vector<int>>& graph) {
+        
         int n = graph.size();
-        vector<int> color(n,-1);
+        
+        vector<int> color(n, -1);
+        
         
         for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
                 if(!biparatite(i, graph, color)){
-                    return false;
+                    return false; 
                 }
             }
         }
