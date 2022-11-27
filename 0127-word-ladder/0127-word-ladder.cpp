80a22d5fcf533194e5fa4584f37b2965f455c566
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-11-27 14:07:07 +0530

    Time: 159 ms (70.80%), Space: 15.6 MB (40.60%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> set;
+        unordered_set<string> vis;
+        
+        for(auto &x : wordList){
+            set.insert(x);
+        }
+        
+        if(set.find(endWord) == set.end()){
+            // nahi mila wordlist me
+            return 0;
+        }
+       
+        int ans = 0;
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        while(!q.empty()){
+            int sz = q.size();
+            ans++;
+            
+            while(sz--){
+                auto front= q.front();
+                q.pop();
+                
+                if(front == endWord){
+                    return ans;
+                }
+                
+                set.erase(front);
+                
+                for(int i = 0 ; i < front.size() ; i++){
+                    string temp = front;
+                    for( char c = 'a' ; c <= 'z' ; c++){
+                        temp[i] = c;
+                        if(set.find(temp) != set.end() && vis.find(temp) == vis.end()){
+                            q.push(temp);
+                            vis.insert(temp);    
+                        }
+                    }
+                }
+            }
+        }
+        
+        return 0;
+
+    }
+};
