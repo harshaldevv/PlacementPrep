e943854489359221a1986f75adaa6ad0a1243fe2
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2022-12-06 17:01:03 +0530

    Time: 586 ms (54.67%), Space: 7.9 MB (94.66%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,54 +1,80 @@
 class Solution {
 public:
-    bool isvalid(int &i, int &j, int &m, int &n){
-        return i >=0 && i < m && j >=0 && j < n;
+    bool isvalid(vector<vector<char>> &board, int i, int j){
+        int m = board.size();
+        int n = board[0].size();
+        
+        if( i < 0 || j < 0 || i >= m || j>= n){
+            return false;
+        }
+        
+        return true;
     }
-    bool dfs(int &i, int &j, string &word, vector<vector<char>> &board, int wordIndex, int &m, int &n){
+    bool startDfs(vector<vector<char>> &board, int i, int j, int indx, string &word){
         
-        char originalWord = board[i][j];
         
-        // cout << "i, j, char = " << i <<", " << j << ", " << board[i][j] << endl;
+        char originalChar = board[i][j];
         
-        board[i][j] = '#';
+        // if(originalChar != word[indx]){
+        //     return false;
+        // }
         
-        if(wordIndex  == word.size()){
+        if(indx +1 == word.size()){
             return true;
         }
         
-        vector<int> dir({1,0,-1,0,1});
+        board[i][j] = '#'; // marking the cell visited by leaving our nishani
+        
+        int dir[] = {1,0,-1,0,1};
+        
         for(int k = 0 ; k < 4 ; k++){
             int newx = i + dir[k];
             int newy = j + dir[k+1];
             
-            if(isvalid(newx, newy,m ,n) && board[newx][newy] != '#' && board[newx][newy] == word[wordIndex]){
-                if(dfs(newx, newy, word, board, wordIndex +1, m, n)){
+            if(isvalid(board, newx, newy) ){
+                
+                // indx + 1 < word.size() && board[newx][newy] == word[indx +1] 
+                if(indx + 1 == word.size()){
                     return true;
                 }
+
+                if(board[newx][newy] == word[indx +1] ){
+                    bool temp = startDfs(board, newx, newy, indx+1, word);
+                    if(temp == true){
+                        return true;
+                    }
+                }
+                
             }
         }
         
-        board[i][j] = originalWord;
+        board[i][j] = originalChar ; // backtrack step
+        
         return false;
-        
-        
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m = board.size();
         int n = board[0].size();
         
+        if(word == ""){
+            return false; // kya hi search kare matrix me
+        }
+        
+        
+        
         bool ans = false;
-        for(int i = 0 ; i < m ; i++){
+        int indx = 0;
+        for(int i = 0 ; i < m ;i++){
             for(int j = 0 ; j < n ; j++){
                 if(board[i][j] == word[0]){
-                    ans = dfs(i, j, word, board, 0+1, m, n);
+                    ans = startDfs(board, i,j , indx, word);
                     if(ans == true){
-                        return true;
+                        return ans;
                     }
                 }
             }
         }
         
         return ans;
-        
     }
 };
