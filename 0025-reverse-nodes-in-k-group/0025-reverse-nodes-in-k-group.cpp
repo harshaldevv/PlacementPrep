c53669286a00395b0143d1891c5be91d32bb3658
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-01-10 15:09:18 +0530

    Time: 19 ms (75.78%), Space: 11.6 MB (9.06%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -12,37 +12,42 @@ class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         
-        if(head == NULL){
-            return NULL;
-        }
+        ListNode *dummy = new ListNode(-1);
         
-        ListNode *cursor = head;
+        dummy->next = head;
         
-        for(int i = 0 ; i < k ; i++){
-            if(cursor == NULL){
-                return head;
+        ListNode *peeche = dummy;
+        ListNode *aage = head;
+        
+        
+        ListNode *curr = aage;
+        ListNode *prev = peeche;
+        ListNode *nxt = nullptr;
+        while(aage){
+            ListNode *cursor = aage;
+            for(int i = 0 ; i < k ; i++){
+                if(cursor == NULL){
+                    return dummy->next;
+                }
+                cursor = cursor->next;
             }
-            cursor = cursor->next;
+            
+            //we have K nodes to reverse
+            
+            for(int i = 0 ; i < k ; i++){
+                nxt = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = nxt;
+            }
+            
+            aage->next = curr;
+            peeche->next = prev;
+            
+            peeche = aage;
+            aage = aage->next;
         }
         
-        //iska mtlb ye hai ki we have K nodes
-        
-        // now we need to reverse them
-        
-        ListNode *curr = head;
-        ListNode *prev = nullptr;
-        ListNode *nxt;
-        
-        for(int i = 0 ; i < k ; i++){
-            nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
-        }
-        
-        head->next = reverseKGroup(curr, k);
-        
-        return prev;
-   
+        return dummy->next;
     }
 };
