e9b457820ce8f1d80f6b51d40b1cbfaedc7530d2
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-15 23:22:41 +0530

    Time: 8 ms (68.82%), Space: 10.3 MB (35.13%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,52 +2,17 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         
-        // one  pass;  ->     
-        unordered_set<int> s;
-    
-        for(int i = 0 ; i < arr.size() ; i++){
-            if(s.count(arr[i]*2) > 0 || (arr[i] %2 == 0 && s.count(arr[i]/2) >0 )){
+        //hci researchh
+        
+        
+         unordered_set<int> seen;
+        for (int num : arr) {
+            if (seen.count(num * 2) || (num % 2 == 0 && seen.count(num / 2))) {
                 return true;
             }
-            s.insert(arr[i]);
+            seen.insert(num);
         }
-        
-//         the reason we also check for i / 2 is because we may have an array like 
-//         [7,1,14,11] and if we just said n * 2 in seen, 14 isn't in seen yet so it would 
-//         return false instead of true. So when we get to 14, 7 is already in it so our 
-//         i/2 check will return True. That's why it can be done in one pass
-            
-                
         return false;
-
-        
-        
-        // 2 pass would be ki pehle sako set me daaldo then 
-        // and then iterate the map
-        // keep in mind the ZERO CASE
-        
-//         unordered_map<int, int> mp;
-        
-//         for(int i = 0 ; i < arr.size() ; i++){
-//             mp[arr[i]]++;
-//         }
-        
-//         for(int i = 0; i < arr.size() ; i++){
-//             if(arr[i] != arr[i]*2){
-//                 if(mp.find(arr[i]*2) != mp.end()){
-//                     return true;
-//                 }
-//             }
-//             else if(arr[i] == 0){  // is case me arr[i] == arr[i]*2
-//                 if(mp[arr[i]] > 1){
-//                     return true;
-//                 }
-//             }
-//         }
-        
-//         return false;
-        
-
         
     }
 };
