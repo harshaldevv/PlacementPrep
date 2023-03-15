c3bee919b68b24b0b2959629dffc643f0161051e
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-16 01:50:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (22.83%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int minimumSum(int num) {
+        //hci research
+        string str_num = to_string(num);
+    sort(str_num.begin(), str_num.end());
+
+    int min_sum = INT_MAX;
+
+    do {
+        int new1 = stoi(str_num.substr(0, 2));
+        int new2 = stoi(str_num.substr(2));
+
+        min_sum = min(min_sum, new1 + new2);
+    } while (next_permutation(str_num.begin(), str_num.end()));
         
-        vector<int> v;
-        
-        int x = num;
-        while(x!=0){
-            int r = x%10;
-            v.push_back(r);
-            x/=10;
-        }
-        
-//         reverse(v.begin(), v.end());
-        
-//         int a = v[0];
-//         int b = v[1];
-//         int c = v[2];
-//         int d = v[3];
-        
-        sort(v.begin(), v.end());
-        
-        int a = v[0]*10 + v[2];
-        int b = v[1]*10 + v[3];
-        
-        return a+ b;
+        return min_sum;
         
     }
 };
