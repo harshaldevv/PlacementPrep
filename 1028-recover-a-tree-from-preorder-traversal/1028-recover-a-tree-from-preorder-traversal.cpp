f33a545e5058757b886aa36959918f3c149dda22
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-16 01:40:09 +0530

    Time: 18 ms (83.00%), Space: 19.6 MB (50.72%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -9,37 +9,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
     TreeNode* recoverFromPreorder(string traversal) {
-        
-        // hci research
-        
-        
-        stack<TreeNode*> stk; // Use a stack to store the current node at each depth
-        int i = 0; // index of traversal string
-        while (i < traversal.size()) {
-            int depth = 0;
-            while (traversal[i] == '-') { // Calculate the depth of the current node
-                ++depth;
-                ++i;
-            }
-            int num = 0;
-            while (i < traversal.size() && traversal[i] != '-') { // Read the value of the current node
-                num = num * 10 + (traversal[i] - '0');
-                ++i;
-            }
-            TreeNode* cur = new TreeNode(num);
-            if (depth == stk.size()) { // The current node is the left child of the top node in the stack
-                if (!stk.empty()) stk.top()->left = cur;
-            } else { // The current node is a right child, need to pop nodes from the stack until we find the parent
-                while (depth < stk.size()) stk.pop();
-                stk.top()->right = cur;
-            }
-            stk.push(cur); // Push the current node into the stack
+        //hci research
+    stack<TreeNode*> stk;
+    int i = 0;
+    while (i < traversal.size()) {
+        int level = 0;
+        while (traversal[i] == '-') {
+            level++;
+            i++;
         }
-        while (stk.size() > 1) stk.pop(); // The root is the only node left in the stack
-        return stk.top();
+        int j = i;
+        while (j < traversal.size() && traversal[j] != '-') {
+            j++;
+        }
+        int val = stoi(traversal.substr(i, j - i));
+        TreeNode* node = new TreeNode(val);
+        if (level == stk.size()) {
+            if (!stk.empty()) {
+                stk.top()->left = node;
+            }
+        } else {
+            while (level != stk.size()) {
+                stk.pop();
+            }
+            stk.top()->right = node;
+        }
+        stk.push(node);
+        i = j;
+    }
+    while (stk.size() > 1) {
+        stk.pop();
+    }
+    return stk.top();
     }
 };
