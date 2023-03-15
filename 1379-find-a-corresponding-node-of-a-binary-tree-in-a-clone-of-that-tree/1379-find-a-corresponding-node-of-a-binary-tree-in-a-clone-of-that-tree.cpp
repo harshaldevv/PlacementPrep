f5a3865ed8fe236d275f70000be514a2430aa949
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-16 01:11:46 +0530

    Time: 567 ms (41.79%), Space: 163.8 MB (83.78%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.cpp
@@ -10,24 +10,23 @@
 
 class Solution {
 public:
-     void dfs(TreeNode* og, TreeNode* copy, TreeNode* target, TreeNode*& ans) {
-        if(!og) return;
-        
-        if(og == target) {
-            ans = copy;
-        }
-        
-        dfs(og->left, copy->left, target, ans);
-        dfs(og->right, copy->right, target, ans);
-        
-        return;
-    }
-    
-public:
     TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
         
-        TreeNode* ans = nullptr;
-        dfs(original, cloned, target, ans);
-        return ans;
+        if (original == NULL) {
+            return NULL;
+        }
+        
+
+        if (original == target) {
+            return cloned;
+        }
+
+        TreeNode* left = getTargetCopy(original->left, cloned->left, target);
+        if (left != NULL) {
+            return left;
+        }
+
+        return getTargetCopy(original->right, cloned->right, target);
+        
     }
 };
