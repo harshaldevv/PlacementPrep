007b7f82dfb71a5e9ed103036f4a95b268675712
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-16 11:46:53 +0530

    Time: 199 ms (83.45%), Space: 68.3 MB (48.84%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& e) {
-        return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+    int findCenter(vector<vector<int>>& edges) {
+        
+        //hci research
+        
+         int n = edges.size() + 1;
+        vector<int> counts(n + 1, 0);
+        for (auto& e : edges) {
+            counts[e[0]]++;
+            counts[e[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (counts[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1; // not found
+        
     }
 };
