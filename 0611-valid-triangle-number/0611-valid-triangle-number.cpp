5f759b3e97f8d5c87f8d11cf16b4f5af00cba4a4
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-22 17:15:59 +0530

    Time: 128 ms (44.64%), Space: 13 MB (59.53%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    
-    int BS(int l, int r, int sum, vector<int> &nums){
-        int start = l;
-        while(l<= r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] < sum){
-                l = mid +1;
-            }
-            else{
-                r = mid -1;
-            }
-        }
-        
-        return l - start;
-        
-        
-    }
     int triangleNumber(vector<int>& nums) {
+        
         sort(nums.begin(), nums.end());
+        
         int n = nums.size();
         int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i +1 ; j < n ; j++){
-                int a = nums[i];
-                int b = nums[j];
-                
-                ans += BS(j+1, n-1, a+b, nums);
+        
+        for(int k = 2 ; k < n ; k++){
+            int i = 0;
+            int j = k-1;
+            
+            while(i<j){
+                if(nums[i] + nums[j] > nums[k]){
+                    ans+= (j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
             }
         }
         
