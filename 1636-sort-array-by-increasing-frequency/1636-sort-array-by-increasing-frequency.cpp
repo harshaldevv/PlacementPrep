fedb4a1b3b05ba6fd5a0df56e4ab4c10e5c82946
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-23 15:49:13 +0530

    Time: 11 ms (49.61%), Space: 11.2 MB (87.00%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    class compareFunc{
+        public:
+        bool operator()(pair<int,int> &a, pair<int,int> &b){
+            if(a.first== b.first){
+                return a.second < b.second;
+            }
+            else{
+                return a.first > b.first;
+            }
+            
+        }
+    };
     vector<int> frequencySort(vector<int>& nums) {
         
         unordered_map<int,int> mp;
@@ -8,8 +20,8 @@ public:
             mp[x]++;
         }
         
-        
-        sort(nums.begin(), nums.end(), [&](auto &a, auto &b) -> bool{
+        // hitesh way
+        sort(nums.begin(), nums.end(), [&](auto &a, auto &b)->bool{
             if(mp[a] == mp[b]){
                 return a > b;
             }
@@ -17,7 +29,29 @@ public:
                 return mp[a] < mp[b];
             }
         });
-        
         return nums;
+        
+//         //Myway
+//         priority_queue< pair<int,int> , vector<pair<int,int>>, compareFunc> pq;
+//         for(auto &x : mp){
+//             pq.push({x.second, x.first});
+//         }
+//         vector<int> ans;
+//         while(!pq.empty()){
+//             int f = pq.top().first;
+//             int e = pq.top().second;
+//             while(f--){
+//                 ans.push_back(e);    
+//             }
+            
+//             pq.pop();
+//         }
+        
+//         return ans;
+        
+        
+        
+        
+        
     }
 };
