911f4558a65ff2f2a76acdae74609abd71be9382
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-23 15:17:44 +0530

    Time: 8 ms (66.93%), Space: 11.2 MB (69.38%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    
-    
-    
     vector<int> frequencySort(vector<int>& nums) {
         
-        unordered_map<int, int> mp;
+        unordered_map<int,int> mp;
         
-        for(auto & x : nums){
+        for(auto &x :nums){
             mp[x]++;
         }
         
-        sort(nums.begin(), nums.end(), [&mp](auto &a, auto &b) -> bool {
+        
+        sort(nums.begin(), nums.end(), [&](auto &a, auto &b) -> bool{
             if(mp[a] == mp[b]){
                 return a > b;
             }
             else{
-                return mp[a]  < mp[b];
+                return mp[a] < mp[b];
             }
-        } );
-                
-            
+        });
+        
         return nums;
     }
 };
