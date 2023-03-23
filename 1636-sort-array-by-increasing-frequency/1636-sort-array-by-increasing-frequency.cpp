f5ad723067f06dae5a2112f1ff23616f5341c448
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-23 15:49:40 +0530

    Time: 4 ms (89.28%), Space: 11.4 MB (41.04%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -20,34 +20,34 @@ public:
             mp[x]++;
         }
         
-        // hitesh way
-        sort(nums.begin(), nums.end(), [&](auto &a, auto &b)->bool{
-            if(mp[a] == mp[b]){
-                return a > b;
-            }
-            else{
-                return mp[a] < mp[b];
-            }
-        });
-        return nums;
+        // // hitesh way
+        // sort(nums.begin(), nums.end(), [&](auto &a, auto &b)->bool{
+        //     if(mp[a] == mp[b]){
+        //         return a > b;
+        //     }
+        //     else{
+        //         return mp[a] < mp[b];
+        //     }
+        // });
+        // return nums;
         
-//         //Myway
-//         priority_queue< pair<int,int> , vector<pair<int,int>>, compareFunc> pq;
-//         for(auto &x : mp){
-//             pq.push({x.second, x.first});
-//         }
-//         vector<int> ans;
-//         while(!pq.empty()){
-//             int f = pq.top().first;
-//             int e = pq.top().second;
-//             while(f--){
-//                 ans.push_back(e);    
-//             }
+        //Myway
+        priority_queue< pair<int,int> , vector<pair<int,int>>, compareFunc> pq;
+        for(auto &x : mp){
+            pq.push({x.second, x.first});
+        }
+        vector<int> ans;
+        while(!pq.empty()){
+            int f = pq.top().first;
+            int e = pq.top().second;
+            while(f--){
+                ans.push_back(e);    
+            }
             
-//             pq.pop();
-//         }
+            pq.pop();
+        }
         
-//         return ans;
+        return ans;
         
         
         
