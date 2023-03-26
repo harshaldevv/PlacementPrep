cd7d7ccf40fb29c4682ac1463ce0bff0b01a6984
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-26 18:41:18 +0530

    Time: 275 ms (8.07%), Space: 66.7 MB (50.93%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,60 +1,49 @@
 class Solution {
 public:
-    bool isValid(vector<int>& bloomDay, int m, int k, int days){
-        int b = 0; // --> number of bouqets i can make 
-        int d = 0;
-        int flowersBloomed = 0;
-        bool together = true;
+    bool check(int &mid, vector<int> &nums, int &m, int &k){
+        int bouq = 0; // total no of bouqets we make 
+        int f = 0; // flowers in our bouq so far
         
-        for(int i = 0 ; i < bloomDay.size() ; i++){
-            if(bloomDay[i] <= days){
-                flowersBloomed++;
-                
-                if(flowersBloomed == k){
-                    b++;
-                    flowersBloomed = 0;
-                }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i] <= mid){
+                f++;
             }
             else{
-                flowersBloomed = 0;
+                f = 0; // reset counter -> kyuki we wanted 
+                // K flowers together, ek saath,
+                // jo ab hume nahi mile kyuki ek unbloomed flower aa gaya
+                // hence reset kr dia counter
+            }
+            
+            if(f == k){
+                bouq++;
+                f = 0;
+            }
+            if(bouq >= m){
+                return true;
             }
         }
-        
-        if(b >= m){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
+        return false;
     }
-    
     int minDays(vector<int>& bloomDay, int m, int k) {
+        long long l = 1;
+        long long r = INT_MAX-100;
+        int ans = -1;
         
-        if(m > (float)bloomDay.size()/k){
-            return -1;
-        }
         
-        int start = 1;
-        int end =  1000000000; //accumulate(bloomDay.begin(), bloomDay.end(), 0);
         
-        int res = -1;
-        
-        while(start <= end){
+        while(l<=r){
+            int mid = l + (r-l)/2;
             
-            int mid = start + (end-start)/2;
-            // mid signifies the number of days to blossom for the whole batch
-            
-            if(isValid(bloomDay, m, k , mid) == true){
-                // go left
-                res = mid;
-                end = mid-1;
+            if(check(mid, bloomDay, m, k)){
+                ans = mid;
+                r = mid -1;
             }
             else{
-                start = mid +1;
+                l = mid +1;
             }
         }
         
-        return res;
+        return ans;
     }
 };
