a510111012227145c95cc0c4486db729e6feabfe
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-30 13:57:42 +0530

    Time: 46 ms (63.37%), Space: 17.8 MB (71.99%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,63 +1,40 @@
 class Solution {
 public:
-    bool isvalid(int &i, int &j, int &m ,int &n){
-        return i >= 0 && i < m && j >= 0 && j < n;
-    }
-    
-    void dfs(vector<vector<int>> &heights, int i, int j, vector<vector<bool>> &ocean){
-        int m = heights.size();
-        int n = heights[0].size();
-        
-        if(!isvalid(i,j,m,n)){
-            return ;
-        }
-        
-        if(ocean[i][j] == true){
-            return ; // already visited
-        }
-        
-        ocean[i][j] = true;
-        
-        vector<int> dir{1,0,-1, 0, 1};
-        for(int k = 0 ; k < 4 ; k++){
-            int newx = i + dir[k];
-            int newy = j + dir[k+1];
-            if(isvalid(newx, newy, m,n) && !ocean[newx][newy] && heights[newx][newy] >= heights[i][j]){
-                dfs(heights, newx, newy, ocean);                
-            }   
-        }
-        return ;
-    }
-    
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        
-        vector<vector<int>> ans;
-        
-        int m = heights.size();
-        int n = heights[0].size();
-        
-        vector<vector<bool>> alt(m, vector<bool>(n, false));
+        //hci research
+        vector<vector<int>> res;
+        if (heights.empty()) return res;
+        int m = heights.size(), n = heights[0].size();
         vector<vector<bool>> pacific(m, vector<bool>(n, false));
-        
-        for(int i = 0 ; i < m ; i++){
-            dfs(heights, i, 0, pacific);
-            dfs(heights, i, n-1, alt);
+        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n - 1);
         }
-        
-        for(int j = 0 ; j < n ; j++){
-            dfs(heights, 0, j, pacific);
-            dfs(heights, m-1, j, alt);
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j);
+            dfs(heights, atlantic, m - 1, j);
         }
-        
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(alt[i][j] && pacific[i][j]){
-                    ans.push_back({i,j});
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    res.push_back({i, j});
                 }
             }
         }
-        
-        return ans;
-        
+        return res;
+    }
+    
+    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& visited, int i, int j) {
+        int m = heights.size(), n = heights[0].size();
+        visited[i][j] = true;
+        int dx[4] = {0, 0, 1, -1};
+        int dy[4] = {1, -1, 0, 0};
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k], y = j + dy[k];
+            if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y] && heights[x][y] >= heights[i][j]) {
+                dfs(heights, visited, x, y);
+            }
+        }
     }
 };
