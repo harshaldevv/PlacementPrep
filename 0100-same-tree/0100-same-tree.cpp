477021a0c575fd515a18673869b3168adb481994
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-30 14:43:14 +0530

    Time: 4 ms (24.24%), Space: 10.2 MB (9.21%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -4,33 +4,30 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        if(!p && q){
-            return false;
-        }
-        
-        if(p && !q){
-            return false;
-        }
-        
-        if(!p && !q){
+        //hci research 
+        // If both trees are NULL, then they are the same
+        if (p == NULL && q == NULL) {
             return true;
         }
         
-        
-        if(p->val != q->val){
+        // If one of the trees is NULL, but not the other, then they are not the same
+        if (p == NULL || q == NULL) {
             return false;
         }
         
-        return isSameTree(p->left, q->left) &&  isSameTree(p->right, q->right);
+        // If the root nodes of both trees have different values, then they are not the same
+        if (p->val != q->val) {
+            return false;
+        }
         
+        // Recursively check if the left subtrees and right subtrees are the same
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
