82b5a29f65899f9cbc359a969e40c7ed7e9861a3
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-30 12:29:41 +0530

    Time: 1125 ms (18.10%), Space: 8.1 MB (24.45%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,80 +1,30 @@
 class Solution {
 public:
-    bool isvalid(vector<vector<char>> &board, int i, int j){
-        int m = board.size();
-        int n = board[0].size();
+    bool exist(vector<vector<char>>& board, string word) {
         
-        if( i < 0 || j < 0 || i >= m || j>= n){
-            return false;
-        }
-        
-        return true;
-    }
-    bool startDfs(vector<vector<char>> &board, int i, int j, int indx, string &word){
-        
-        
-        char originalChar = board[i][j];
-        
-        // if(originalChar != word[indx]){
-        //     return false;
-        // }
-        
-        if(indx +1 == word.size()){
-            return true;
-        }
-        
-        board[i][j] = '#'; // marking the cell visited by leaving our nishani
-        
-        int dir[] = {1,0,-1,0,1};
-        
-        for(int k = 0 ; k < 4 ; k++){
-            int newx = i + dir[k];
-            int newy = j + dir[k+1];
-            
-            if(isvalid(board, newx, newy) ){
-                
-                // indx + 1 < word.size() && board[newx][newy] == word[indx +1] 
-                if(indx + 1 == word.size()){
+        //hci research 
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0] && dfs(board, word, visited, i, j, 0))
                     return true;
-                }
-
-                if(board[newx][newy] == word[indx +1] ){
-                    bool temp = startDfs(board, newx, newy, indx+1, word);
-                    if(temp == true){
-                        return true;
-                    }
-                }
-                
             }
         }
-        
-        board[i][j] = originalChar ; // backtrack step
-        
         return false;
     }
-    bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        if(word == ""){
-            return false; // kya hi search kare matrix me
-        }
-        
-        
-        
-        bool ans = false;
-        int indx = 0;
-        for(int i = 0 ; i < m ;i++){
-            for(int j = 0 ; j < n ; j++){
-                if(board[i][j] == word[0]){
-                    ans = startDfs(board, i,j , indx, word);
-                    if(ans == true){
-                        return ans;
-                    }
-                }
-            }
-        }
-        
-        return ans;
+    
+    bool dfs(vector<vector<char>>& board, string word, vector<vector<bool>>& visited, int i, int j, int k) {
+        if (k == word.length())
+            return true;
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || visited[i][j] || board[i][j] != word[k])
+            return false;
+        visited[i][j] = true;
+        bool res = dfs(board, word, visited, i-1, j, k+1) ||
+                   dfs(board, word, visited, i+1, j, k+1) ||
+                   dfs(board, word, visited, i, j-1, k+1) ||
+                   dfs(board, word, visited, i, j+1, k+1);
+        visited[i][j] = false;
+        return res;
     }
 };
