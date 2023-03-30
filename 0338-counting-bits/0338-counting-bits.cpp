59690160dbb5d72829918a2fd8eec8e830534389
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-30 12:00:17 +0530

    Time: 4 ms (82.46%), Space: 7.7 MB (90.01%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        
         //hci research
-        vector<int> dp(n+1);
-        dp[0] = 0;
-        for(int i=1;i<=n;i++){
-            dp[i] = dp[i&(i-1)]+1;
+        vector<int> ans(n + 1, 0);
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i & (i - 1)] + 1;
         }
-        return dp;
-        
+        return ans;
     }
 };
