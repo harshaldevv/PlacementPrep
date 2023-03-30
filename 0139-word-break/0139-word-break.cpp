44088965861e6476afe42be6dff465e4c8466e27
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-03-30 14:21:22 +0530

    Time: 8 ms (69.86%), Space: 9.7 MB (62.37%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        int n = s.length();
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        vector<bool> dp(n + 1, false);
+        dp[0] = true; // empty string is always in the dictionary
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+        
+    }
+};
