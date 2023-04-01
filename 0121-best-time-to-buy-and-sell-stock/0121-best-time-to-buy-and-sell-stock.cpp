927b4f5a8d991274ed1d1f38e911255950e73a72
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-04-02 02:36:30 +0530

    Time: 149 ms (23.83%), Space: 93.3 MB (91.75%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int maxprofit = 0;
+        int n = prices.size();
+        int minSoFar = INT_MAX;
         
-        //hci research 
-        int min_price = INT_MAX;
-        int max_profit = 0;
-
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < min_price) {
-                min_price = prices[i];
-            } else if (prices[i] - min_price > max_profit) {
-                max_profit = prices[i] - min_price;
-            }
+        for(int i = 0 ; i < n ; i++){
+            minSoFar = min(prices[i], minSoFar);
+            int profitToday = prices[i] - minSoFar;
+            maxprofit = max(profitToday, maxprofit);
         }
-
-        return max_profit;
+        
+        return maxprofit;
+        
     }
 };
