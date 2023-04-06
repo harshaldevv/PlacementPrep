e47d740b4833fcfa17c3bcb9b11b4872dd6d90ca
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-04-06 12:30:57 +0530

    Time: 109 ms (82.50%), Space: 61.2 MB (93.74%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // Hashmap approach
-        // TC- O(N)
-        // SC - O(N)
-        
-        // Sum approach --> Sum of array - n(n+1)/2 , where n = nums.size() -1
-        // here it fails on testcase -> [2,2,2,2,2]
-        
-        
-        // Linked List approach --> cycle , slow fast
-        
         int slow = nums[0];
         int fast = nums[0];
         
-        while(true){
+        do{
             slow = nums[slow];
             fast = nums[nums[fast]];
-            if(slow == fast){
-                break;
-            }
         }
+        while(slow != fast);
         
         fast = nums[0];
+        
         while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
         }
         
         return slow;
-        
     }
 };
