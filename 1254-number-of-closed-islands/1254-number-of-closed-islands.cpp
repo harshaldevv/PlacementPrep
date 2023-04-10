b713ef54d4d0630cbbbc638c13f61eaeeec0c0b2
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-04-11 01:22:37 +0530

    Time: 21 ms (16.38%), Space: 11.5 MB (19.85%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,74 +1,61 @@
 class Solution {
 public:
-    bool isValid(int &i, int &j, int &m , int &n){
-        return i >=0 && i < m && j >=0 && j < n;
+    bool isvalid(int i, int j, int m, int n){
+        if(i <0 || j < 0 || i >= m || j >= n){
+            return false;
+        }
+        return true;
     }
-    void doDFS(vector<vector<int>> &grid, int i, int j, int &m , int &n){
-        if(!isValid(i,j,m,n) || grid[i][j] != 0){
+    
+    void dfs(int x,int y,int m, int n, vector<vector<int>> &grid){
+        if(!isvalid(x,y,m,n)){
             return ;
         }
         
-        grid[i][j] = 2;
+        grid[x][y] = 2; // marking visited
         
+        vector<int> dir{-1,0,1,0,-1};
         
-        vector<int> dir{1,0,-1,0,1};
-        
-        for(int k = 0 ; k < 4 ; k++){
-            doDFS(grid, i + dir[k], j + dir[k+1], m, n);
+        for(int i = 0 ; i < 4 ; i++){
+            int newx = x + dir[i];
+            int newy = y + dir[i+1];
+            
+            if(isvalid(newx, newy, m, n) && grid[newx][newy] == 0){
+                dfs(newx, newy, m, n, grid);
+            }
         }
         
         return ;
+        
         
     }
     int closedIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         
-        // 0 = land
-        // 1 = water
-        
-        //border wale 0s ko exclude karo abhi
+        //all zero at boundary
         for(int i = 0 ; i < m ; i++){
-            if(grid[i][0] == 0) {
-                // left column 
-                doDFS(grid, i, 0, m, n);
-            }
-        }
-        
-        for(int i = 0 ; i < m ; i++){
-            if(grid[i][n-1] == 0) {
-                 // right column
-                doDFS(grid, i, n-1, m, n);
-            }
-        }
-        
-        for(int j = 0; j < n ; j++){
-            if(grid[0][j] == 0 ){
-                // first row 
-                doDFS(grid, 0, j, m, n);
-            }
-        }
-        
-        for(int j = 0; j < n ; j++){
-            if(grid[m-1][j] == 0 ){
-                // last row
-                doDFS(grid, m-1, j, m, n);
+            for(int j = 0 ; j < n ; j++){
+                if(i==0 || j == 0 || i == m-1 || j == n-1){
+                    if(grid[i][j] == 0){
+                        // dfs krke mark out krdo
+                        dfs(i,j, m, n, grid);
+                    }
+                }
             }
         }
         
         int ans = 0;
-        
-        for(int i =0 ; i < m ; i++){
-            for(int j = 0 ; j < n ;j++){
-                if(grid[i][j]== 0){
-                    doDFS(grid, i, j, m, n);
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 0){
+                    //
+                    dfs(i,j,m,n, grid);
                     ans++;
                 }
             }
         }
         
         return ans;
-        
-        
     }
 };
