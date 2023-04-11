25b0e5ab7966a728ea8c9809b9e6b693fa92fbb8
Harshal Dev (Dholchike) <55681046+Dholchike@users.noreply.github.com>
2023-04-12 01:27:31 +0530

    Time: 205 ms (12.70%), Space: 49.2 MB (7.13%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool isvalid(int &i, int &j, int &m, int &n){
+    bool isvalid(int i, int j, int m ,int n){
         return i>=0 && i < m && j >= 0 && j < n;
     }
     int maxDistance(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
-        int land = 0; // land = 1 
-        int water = 0; // water = 0
+        
         queue<vector<int>> q;
+        int land = 0;
+        int water = 0;
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 if(grid[i][j] == 1){
+                    // land
                     land++;
-                    q.push({i,j,0});
+                    q.push({i,j});
                 }
                 else{
                     water++;
@@ -26,32 +28,33 @@ public:
             return -1;
         }
         
-        int maxdist = 0;
-        
-        vector<int> dir{1,0,-1,0,1};
+        int ans = -1;
+        vector<int> dir{-1,0,1,0,-1};
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
         
         while(!q.empty()){
-            auto sz = q.size();
-            maxdist++;
+            int sz = q.size();
+            ans++;
             while(sz--){
-                auto front = q.front();
-                q.pop();
-                int currx = front[0];
-                int curry = front[1];
+                auto front = q.front(); q.pop();
+                
+                int x = front[0];
+                int y = front[1];
+                
                 
                 for(int k = 0 ; k < 4 ; k++){
-                    int newx = currx + dir[k];
-                    int newy = curry + dir[k+1];
+                    int newx = x + dir[k];
+                    int newy = y + dir[k+1];
                     
-                    if(isvalid(newx, newy, m,n) && grid[newx][newy] == 0){
+                    if(isvalid(newx, newy, m, n) && !vis[newx][newy] && grid[newx][newy] == 0){
                         q.push({newx, newy});
-                        grid[newx][newy] = 2;
+                        vis[newx][newy] = true;
                     }
                 }
             }
         }
         
-        return maxdist-1;
+        return ans;
         
     }
 };
