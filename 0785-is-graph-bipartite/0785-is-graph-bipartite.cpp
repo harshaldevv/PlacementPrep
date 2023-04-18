4161dbb28e4007ca591e2e698ed1fdaa11fc564b
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-04-18 20:21:24 +0530

    Time: 20 ms (92.67%), Space: 13.3 MB (99.78%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    
-    bool biparatite(int curr, vector<vector<int>> &graph, vector<int> &color){
-        
+    bool bipar(int curr, int par, vector<int> &color, vector<vector<int>> &graph){
         if(color[curr] == -1){
             color[curr] = 1;
         }
         
         for(auto &next : graph[curr]){
             if(color[next] == -1){
-                color[next] = 1 - color[curr];
-                if(!biparatite(next, graph, color)){
+                //unvisited
+                int colorToBe = 1 - color[curr];
+                color[next] = colorToBe;
+                if(!bipar(next, curr, color, graph)){
                     return false;
                 }
             }
-            else if(color[next] == color[curr] ){
+            else if(color[next] == color[curr]){
                 return false;
             }
         }
         
         return true;
     }
-    
     bool isBipartite(vector<vector<int>>& graph) {
         
         int n = graph.size();
         
-        vector<int> color(n, -1);
-        
-        //DFS method. next will do by BFS way
+        vector<int> color(n+1, -1);
         
         for(int i = 0 ; i < n ; i++){
             if(color[i] == -1){
-                if(!biparatite(i, graph, color)){
-                    return false; 
+                if(!bipar(i, -1, color, graph)){
+                    return false;
                 }
             }
         }
