e0f75ed753a558d8930924d1e294db5b6f66e998
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-21 19:04:23 +0530

    Time: 9 ms (5.99%), Space: 7.8 MB (41.57%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -25,8 +25,59 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return f(m-1, n-1, grid, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        // return f(m-1, n-1, grid, dp);
         
+        //tabulation
+        
+        if(grid[0][0] == 1){
+            // obstacle hai starting point pe hi so cant move ahead
+            return 0;
+        }
+        
+        // dp[0][0] = 1;
+        
+        //filling the row
+        for(int i = 0 ; i < n ; i++){
+            cout << "i = " << i << " ; ";
+            if(grid[0][i] == 1){
+                cout << "hrer";
+                dp[0][i] = 0;
+                cout << "hrersdsds";
+                break;
+            }
+            else{
+                dp[0][i] = 1;
+            }
+        }
+        
+        //filling the col
+        for(int i = 0 ; i < m ; i++){
+            // cout << "j = " << i << " ; ";
+            if(grid[i][0] == 1){
+                dp[i][0] = 0;
+                break;
+            }
+            else{
+                dp[i][0] = 1;
+            }
+        }
+        
+        
+        
+        
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else{
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+    
+        return dp[m-1][n-1];
     }
 };
+  
