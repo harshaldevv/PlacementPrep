61e47cabaaccea303a9f174b0ed533b8ec983758
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-21 13:38:43 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (82.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -23,35 +23,66 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
+        
+        // //memoization
         // vector<int> dp(n+1, -1);
         // return f(n-1, nums, dp);
         
         
-        // do tabulation
-        //steps 
-        // 1) initialise a dp array of size n+1
-        // 2) write the bases cases from the memoization
-        // 3) run a for loop 
-        // 4) replace f(i), f(i-1).. etc with dp[i], dp[i-1],...
-        // 5) return dp[n-1] (instead of dp[n] because we are dealing with indexes, 
-        //    therefore for "n", we get answer at index n-1
-        vector<int> dp(n+1, -1);
+//         // do tabulation
+//         //steps 
+//         // 1) initialise a dp array of size n+1
+//         // 2) write the bases cases from the memoization
+//         // 3) run a for loop 
+//         // 4) replace f(i), f(i-1).. etc with dp[i], dp[i-1],...
+//         // 5) return dp[n-1] (instead of dp[n] because we are dealing with indexes, 
+//         //    therefore for "n", we get answer at index n-1
+//         vector<int> dp(n+1, -1);
+        
+//         if(n==1){
+//             return nums[0];
+//         }
+        
+//         dp[0] = nums[0];
+//         dp[1] = max(nums[0], nums[1]);
+        
+//         for(int i = 2 ; i <  n ; i++){
+//             int left = dp[i-1];
+//             int right = dp[i-2] + nums[i];
+            
+//             dp[i] = max(left, right);
+//         }
+        
+//         return dp[n-1];
+
+        // space optimisation
+        
+        
         
         if(n==1){
             return nums[0];
         }
         
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        
+        
+        int prev2 = nums[0];
+        int prev = max(nums[0], nums[1]);
+        int curr = -1;
         
         for(int i = 2 ; i <  n ; i++){
-            int left = dp[i-1];
-            int right = dp[i-2] + nums[i];
+            int left = prev;
+            int right = prev2 + nums[i];
             
-            dp[i] = max(left, right);
+            curr = max(left, right);
+            
+            prev2 = prev;
+            prev = curr;
+            
         }
         
-        return dp[n-1];
+        return prev;
+
+        
         
     }
 };
