c337b2f136a513e69a6c28e04a1bcc6466afb5f2
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-21 13:12:35 +0530

    Time: 6 ms (9.32%), Space: 7.8 MB (55.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        
-        //hci research
-         int n = nums.size();
-        if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
-
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+    int f(int i, vector<int> &nums, vector<int> &dp){
+        if(i == 0){
+            return nums[0];
         }
-
-        return dp[n-1];
+        else if(i == 1){
+            return max(nums[0], nums[1]);
+        }
+        
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        
+        int left = f(i-1, nums, dp);
+        int right = f(i-2, nums, dp) + nums[i];
+        
+        return dp[i] =  max(left, right);
+        
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        return f(n-1, nums, dp);
         
     }
 };
