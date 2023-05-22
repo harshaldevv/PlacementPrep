a17e7accb87eee0e351b476f768afe20c77898fa
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-23 00:55:13 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -1,112 +1,72 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 class Solution{
 
   public:
-	int minDifference(int arr[], int n)  { 
+	int minDifference(int nums[], int n)  { 
 	    // Your code goes here
-	    if(n==0){
-	        return 0;
-	    }
 	    
-	    int sum = 0;  //maxRange of S1
-	    
-	    for(int i = 0 ; i < n ; i++){
-	        sum+=arr[i];
-	    }
-// 	    5
-// 20 19 18 20 16
+	    int s = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            int x = nums[i];
+            s+=x;
+        }
+        
+        bool dp[n][s+1000];
+        memset(dp, false, sizeof(dp));
+        
+        // cout << "s = " << s << endl;
+        
+        for(int i = 0 ; i < n ; i++){
+            dp[i][0] = true;
+        }
+        
+        
+        if(nums[0] >= 0 && nums[0] <= s ){
+            dp[0][nums[0]]  = true;    
+        }
+        
+        
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j <=s ; j++ ){
+                
+                bool notTake = dp[i-1][j];
+        
+                bool take = false;
 
-	    int rows = n+1;
-	    int cols = floor(sum/2) +1;
-	    bool t[rows][cols];
-	    
-	    // initlaization
-	    
-	    for(int i = 0 ; i < rows; i++){
-	        t[i][0] = true; 
-	    }
-	    
-	    for(int j = 0 ;  j < cols ; j++){
-	        t[0][j] = false;
-	    }
-	    
-	    t[0][0] = true;
-	    
-	    for(int i = 1 ; i < rows ; i++){
-	        for(int j = 1 ; j< cols ; j++){
-	            if(arr[i-1] <= j){
-	                // include/ exclude
-	                t[i][j] = t[i-1][j] || t[i-1][j- arr[i-1]];
-	            }
-	            else{
-	                t[i][j] = t[i-1][j];
-	            }
-	        }
-	    }
-	    
-	    // get the last row of the matrix
-	    
-	   // cout << "cols = " << cols << endl;
-	    
-	    for(int i = cols -1; i>=0 ; i--){
-	        if(t[n][i] == true){
-	           // cout << t[n][i] << " ";
-	            return abs(sum - 2*i); //t[n][i];
-	           // return max(t[n][i], t[n][i-1]);
-	        }
-	    }
-	    
-	    return 69;
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	   // bool t[n+1][sum+1];
-	    
-	   // for(int i = 0 ; i < n+1 ; i++){
-	   //     t[i][0] = true;
-	   // }
-	   // for(int j = 0 ; j < sum+1; j++){
-	   //     t[0][j] = false;
-	   // }
-	    
-	   // t[0][0] = true;
-	    
-	   // for(int i = 1 ; i < n+1 ; i++){
-	   //     for(int j = 1 ; j < sum +1 ; j++){
-	   //         if(arr[i-1] <= j){
-    //                 t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-	   //         }
-	   //         else{
-	   //             t[i][j] = t[i-1][j];
-	   //         }
-	   //     }
-	   // }
-	    
-	   // //get the last row of the matrix with cells that have true
-	   // vector<int> v;
-	    
-	   // for(int j = 0 ; j< sum+1 ; j++){
-	   //     if(t[n][j] == true){
-	   //         v.push_back(j);
-	   //     }
-	   // }
-	    
-	   // return abs(sum - 2*v[v.size()/2]);
+                if(j >= nums[i]){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] = notTake | take;
+                
+            }
+        }
+        
+        
+        
+        // find true in the last row from right to left.
+        
+        int maxS1 = 0;
+        for(int j = s/2  ; j >=0 ; j--){
+            if(dp[n-1][j] == true){
+                maxS1 = j;
+                break;
+            }
+        }
+        
+        // cout << "maxS1 = " << maxS1 << endl;
+        
+        return abs(s - 2*maxS1);
 	} 
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() 
 {
    
@@ -129,4 +89,5 @@ int main()
 	     
     }
     return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
