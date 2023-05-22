355e8d21f26dc8568f51b9cdf16bdc77afab805f
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-22 17:43:49 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -1,49 +1,53 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 //Initial template for C++
 
 #include<bits/stdc++.h> 
 using namespace std; 
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 
 class Solution{   
 public:
+    bool f(int i, int j, vector<int> &arr, vector<vector<int>> &dp){
+        if(j == 0){
+            return true;
+        }
+        
+        
+        if(i == 0){
+           return (arr[0] == j);
+        }
+        
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        bool notpick = f(i-1, j, arr, dp);
+        
+        bool take = false;
+        
+        if(j >= arr[i]){
+            take = f(i-1, j- arr[i], arr, dp);
+        }
+        
+        return dp[i][j] = notpick | take;
+    }
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
+        
         int n = arr.size();
-        int W = sum;
         
-        bool t[n+1][W+1];
+        int k = sum;
         
-        // filling first row
-        for(int j = 0 ; j < W+1 ; j++){
-            t[0][j] = false;
-        }
+        vector<vector<int>> dp(n+1, vector<int>(k+1, -1));
         
-        
-        for(int i = 0 ; i < n +1 ; i++){
-            t[i][0] = true;
-        }
-        
-        // filling matrix
-        
-        for(int i =1 ; i < n+1; i++){
-            for(int j = 1 ; j < W+1; j++){
-                if(arr[i-1] <= j){
-                    t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
-                }
-                else{
-                    t[i][j] = t[i-1][j];
-                }
-            }
-        }
-        
-        return t[n][W];
+        return f(n-1, k, arr, dp);
     }
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() 
 { 
     int t;
@@ -63,4 +67,5 @@ int main()
     }
     return 0; 
 } 
-  // } Driver Code Ends
+
+// } Driver Code Ends
