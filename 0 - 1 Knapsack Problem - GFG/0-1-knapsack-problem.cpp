825327bbc072ee8fb6de7588824825b780aa070f
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-23 13:39:35 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -1,43 +1,71 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 using namespace std;
 
 
- // } Driver Code Ends
-class Solution
-{
+// } Driver Code Ends
+class Solution{
+    
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n) 
-    { 
-       // Your code here
-       int t[n+1][W+1];
-       
-       
-       for(int i = 0 ; i < n+1; i++){
-           for(int j = 0 ;  j < W+1 ; j++){
-               if(i == 0 || j == 0){
-                   t[i][j] = 0;
-               }
-           }
-       }
-       
-       for(int i = 1 ; i < n+1 ; i++){
-           for(int j = 1; j < W+1 ; j++){
-               if(wt[i-1] <= j){
-                   t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
-               }
-               else{
-                   t[i][j] = t[i-1][j];
-               }
-           }
-       }
-       
-       return t[n][W];
+    int f(int i, int j, int wt[], int val[], int n, vector<vector<int>> &dp){
+        if(j == 0){
+            return 0;
+        }
+        
+        if(i == 0){
+            if(j >= wt[i]){
+                return val[i];
+            }
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int notTake = f(i-1, j, wt, val, n, dp) + 0;
+        
+        int take = 0;
+        
+        if( j >= wt[i]){
+            take = f(i-1, j - wt[i], wt, val, n, dp ) + val[i];
+        }
+        
+        return dp[i][j] =  max(take, notTake);
     }
+    int knapSack(int W, int wt[], int val[], int n) { 
+        vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
+        return f(n-1, W, wt, val, n, dp);
+    //   // Your code here
+    //   int t[n+1][W+1];
+       
+       
+    //   for(int i = 0 ; i < n+1; i++){
+    //       for(int j = 0 ;  j < W+1 ; j++){
+    //           if(i == 0 || j == 0){
+    //               t[i][j] = 0;
+    //           }
+    //       }
+    //   }
+       
+    //   for(int i = 1 ; i < n+1 ; i++){
+    //       for(int j = 1; j < W+1 ; j++){
+    //           if(wt[i-1] <= j){
+    //               t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]], t[i-1][j]);
+    //           }
+    //           else{
+    //               t[i][j] = t[i-1][j];
+    //           }
+    //       }
+    //   }
+       
+    //   return t[n][W];
+    }
+    
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 
 int main()
  {
@@ -66,4 +94,5 @@ int main()
         
     }
 	return 0;
-}  // } Driver Code Ends
+}
+// } Driver Code Ends
