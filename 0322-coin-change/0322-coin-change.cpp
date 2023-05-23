c8dd8e7b2794607c97a657d98ef44446ad8b81dd
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-23 15:12:27 +0530

    Time: 110 ms (56.13%), Space: 45.6 MB (27.92%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,50 @@
-#include <limits>
-
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
+    
+    int f(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
+        if(j == 0){
+            return 0;
+        }
         
-        vector<int> dp(amount+1, numeric_limits<int>::max());
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++) {
-            for(int j = 0; j < coins.size(); j++) {
-                if(coins[j] <= i && dp[i-coins[j]] != numeric_limits<int>::max()) {
-                    dp[i] = min(dp[i], dp[i-coins[j]] + 1);
-                }
+        if(i == 0){
+            if(j%nums[i] == 0){
+                return j/nums[i];
+            }
+            else{
+                return 1e5;
+                // agar nahi ban paa raha , toh no of coins req = INT MAX
+                // kyuki jitna bhi try karlo, nahi banega
             }
         }
-        return dp[amount] == numeric_limits<int>::max() ? -1 : dp[amount];
-
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int nottake = f(i-1, j, nums, dp );
+        
+        int take = INT_MAX;
+        
+        if(j >= nums[i]){
+            take = 1 +  f(i, j- nums[i], nums, dp) ;
+        }
+        
+        return dp[i][j] = min(take, nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        
+        // knapsack
+        int n = coins.size();
+        
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        //striver method
+        int ans =  f(n-1, amount, coins, dp);
+        
+        if(ans >= 1e5){
+            return -1;
+        }
+        
+        return ans;
+        
     }
 };
