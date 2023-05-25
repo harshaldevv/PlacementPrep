b3bb3993c3c549c0247b33fd0e94b2b3354cafd7
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 03:00:33 +0530

    Time: 16 ms (11.93%), Space: 9.7 MB (85.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,63 +23,63 @@ public:
         // vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
         // return f(m-1, n-1, grid, dp);
         
-        //tabulation
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = grid[0][0];
-                }
-                else{
-                    int up = INT_MAX;
-                    int left = INT_MAX;
-                    
-                    if( i >= 1){
-                        up = dp[i-1][j];
-                    }
-                    
-                    if(j >= 1){
-                        left = dp[i][j-1];
-                    }
-                    
-                    dp[i][j] = min(up, left) + grid[i][j];
-                }
-            }
-        }
-        
-        return dp[m-1][n-1];
-        
-//         //tabulation with space optimisation
-        
-//         vector<int> curr(n, 0);
-//         vector<int> prev(n, 0);
-        
+//         //tabulation
+//         vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
 //         for(int i = 0 ; i < m ; i++){
 //             for(int j = 0 ; j < n ; j++){
 //                 if(i == 0 && j == 0){
-//                     curr[j] = grid[0][0];
+//                     dp[i][j] = grid[0][0];
 //                 }
 //                 else{
-                    
 //                     int up = INT_MAX;
 //                     int left = INT_MAX;
                     
 //                     if( i >= 1){
-//                         up = prev[j];
+//                         up = dp[i-1][j];
 //                     }
                     
 //                     if(j >= 1){
-//                         left = curr[j-1];
+//                         left = dp[i][j-1];
 //                     }
                     
-//                     curr[j] = min(up, left) + grid[i][j];
+//                     dp[i][j] = min(up, left) + grid[i][j];
 //                 }
 //             }
-            
-//             prev = curr;
 //         }
         
-//         return prev[n-1];
+//         return dp[m-1][n-1];
+        
+        //tabulation with space optimisation
+        
+        vector<int> curr(n, 0);
+        vector<int> prev(n, 0);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 && j == 0){
+                    curr[j] = grid[0][0];
+                }
+                else{
+                    
+                    int up = INT_MAX;
+                    int left = INT_MAX;
+                    
+                    if( i >= 1){
+                        up = prev[j];
+                    }
+                    
+                    if(j >= 1){
+                        left = curr[j-1];
+                    }
+                    
+                    curr[j] = min(up, left) + grid[i][j];
+                }
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[n-1];
         
         
     }
