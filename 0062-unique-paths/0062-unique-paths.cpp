27e679544169d87911341771f791d403cc17d37b
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 01:40:33 +0530

    Time: 5 ms (18.34%), Space: 6.6 MB (28.62%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -25,7 +25,32 @@ public:
     int uniquePaths(int m, int n) {
         
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return f(m-1, n-1, dp);
+        // return f(m-1, n-1, dp);
+        
+        
+        dp[0][0] = 1;
+        
+        for(int i = 0 ; i < m ; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            dp[0][i] = 1;   
+        }
+        
+        for(int i = 1 ; i< m; i++){
+            for(int j = 1 ; j < n ; j++){
+                
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+                
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
         
     }
 };
