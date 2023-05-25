2877fc2172b3bd324348001af922c675c1f8c025
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 02:49:05 +0530

    Time: 13 ms (27.90%), Space: 10.1 MB (48.03%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,56 +1,25 @@
 class Solution {
 public:
     int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i == 0 && j == 0){
-            return grid[0][0];
-        }
-        else if(i < 0 || j < 0){
-            return INT_MAX; // IMPORTANT
+        if(i == 0 & j == 0){
+            return grid[i][j];
         }
         
-        if(dp[i][j] != -1){
+        if(i < 0 || j < 0 ){
+            return INT_MAX;
+        }
+        if(dp[i][j]!= -1){
             return dp[i][j];
         }
+        int up = f(i-1, j, grid, dp);
+        int left = f(i, j-1 , grid, dp);
         
-        return dp[i][j] =  grid[i][j] + min( f(i-1, j, grid, dp), f(i, j-1, grid, dp)) ;
+        return dp[i][j] = min(up, left) + grid[i][j];
     }
     int minPathSum(vector<vector<int>>& grid) {
-        
         int m = grid.size();
         int n = grid[0].size();
-        // vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        // return f(m-1, n-1, grid, dp);
-        
-        // tabulization
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                
-                if(i == 0 && j == 0){
-                    dp[i][j] = grid[0][0];
-                }
-                else{
-                    int up = INT_MAX;
-                    int left = INT_MAX;
-                    
-                    if(i > 0){
-                        up = dp[i-1][j];
-                    }
-                    
-                    if(j > 0){
-                        left = dp[i][j-1];
-                    }
-                    
-                    dp[i][j] =  grid[i][j] + min( up, left );
-                    // cout << dp[i][j] << endl;
-                }
-            }
-        }
-        
-        return dp[m-1][n-1];
-        
-        
-        
+        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+        return f(m-1, n-1, grid, dp);
     }
 };
