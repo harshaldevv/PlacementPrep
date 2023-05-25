f4de337db8f2aa32ed404245105a77f8c28ab15f
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 02:44:56 +0530

    Time: 7 ms (27.87%), Space: 7.7 MB (66.90%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -34,9 +34,84 @@ public:
         
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+//         // vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+//         // return f(m-1, n-1, grid, dp);
         
-        return f(m-1, n-1, grid, dp);
+        
+//         //base case
+        
+//         if(grid[0][0] == 1){
+//             return 0;
+//         }
+//         // tabulation
+//         for(int i = 0 ; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+                
+//                 if(grid[i][j] == 1){
+//                     //stone
+//                     dp[i][j] =  0;
+//                 }
+//                 else if(i == 0 && j == 0){
+//                     dp[i][j] =1 ;
+//                 }
+//                 else{
+//                     int up = 0;
+//                     int left = 0;
+                    
+//                     if(i >=1 ){
+//                         up = dp[i-1][j];
+//                     }
+                    
+//                     if(j >=1 ){
+//                         left = dp[i][j-1];
+//                     }
+                    
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+        
+        
+//         return dp[m-1][n-1];
+        
+        
+        // tabulation with space optimisation
+        
+        vector<int> curr(n, 0);
+        vector<int> prev(n, 0);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                
+                if(grid[i][j] == 1){
+                    //stone
+                    curr[j] =  0;
+                }
+                else if(i == 0 && j == 0){
+                    curr[j] = 1 ;
+                }
+                else{
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i >=1 ){
+                        up = prev[j];
+                    }
+                    
+                    if(j >=1 ){
+                        left = curr[j-1];
+                    }
+                    
+                    curr[j] = up + left;
+                }
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[n-1];
+        
+        
         
     }
 };
