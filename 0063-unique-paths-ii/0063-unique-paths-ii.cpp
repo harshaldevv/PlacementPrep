b6aa466ab8e898a073fcec4081a8a52243329238
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 02:11:50 +0530

    Time: 8 ms (13.47%), Space: 7.8 MB (38.96%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,71 +1,42 @@
 class Solution {
 public:
-    
-    int f(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i == 0 & j == 0 && grid[i][j] == 0){
-            // obstacle toh nahi haina ye check kia grid[i][j] == 0 se
-            return 1;
+    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp){
+        if(i ==0 && j == 0){
+            if(grid[i][j] == 1){
+                //stone
+                return 0;
+            }
+            else{
+                return 1;
+            }
         }
-        else if(i < 0 || j < 0){
-            return 0;
-        }
-        else if(grid[i][j] == 1){
+        
+         if(i < 0 || j < 0 ){
             return 0;
         }
         
+        if(grid[i][j] == 1){
+                //stone
+            return 0;
+        }
+        
+       
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
+        int up = f(i-1, j, grid, dp);
+        int left = f(i, j-1, grid, dp);
         
-        return dp[i][j] = f(i-1, j, grid, dp) + f(i, j-1, grid, dp);
+        return dp[i][j] = up + left;
     }
-    
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        
         int m = grid.size();
         int n = grid[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        // return f(m-1, n-1, grid, dp);
+        return f(m-1, n-1, grid, dp);
         
-        //tabulation
-        
-        if(grid[0][0] == 1){
-            // obstacle hai starting point pe hi so cant move ahead
-            return 0;
-        }
-        
-        
-        
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == 1){
-                    dp[i][j] = 0;
-                }
-                else if(i == 0 & j == 0 ){
-                    dp[i][j] = 1;
-                }
-                else{
-                    int up = 0;
-                    int left = 0;
-                    
-                    if(i > 0){
-                        up = dp[i-1][j];
-                    }
-                    
-                    if(j > 0){
-                        left =  dp[i][j-1];
-                    }
-                    
-                    dp[i][j] =  up + left;
-                    
-                }
-                
-                // cout << "dp[" << i << ", " << j << "] = " << dp[i][j] << endl;
-            }
-        }
-    
-        return dp[m-1][n-1];
     }
 };
-  
