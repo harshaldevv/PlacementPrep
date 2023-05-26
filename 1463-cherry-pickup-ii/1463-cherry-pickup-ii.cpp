997b05e67e2dfbe212d58c6a106b908f8e47a669
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 22:46:21 +0530

    Time: 275 ms (13.06%), Space: 17.5 MB (10.04%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -41,9 +41,57 @@ public:
         int n = grid[0].size();
         
         
+        // vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int>(n+1, -1))); //mine
+        // return f(0,0, n-1, grid, m, n, dp);
+        
+        //tabulation
+        // i-> 0 to m-1 -->now i = m-1 to 0
+        
         vector<vector<vector<int>>> dp(m+1, vector<vector<int>>(n+1, vector<int>(n+1, -1))); //mine
+        for(int j1 = 0 ; j1 < n ; j1++){
+            for(int j2 = 0 ; j2 < n ; j2++){
+                if(j1 == j2){
+                    dp[m-1][j1][j2] = grid[m-1][j1];
+                }
+                else{
+                    dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+                }
+            }
+        }
+        
+        for(int i = m-2 ; i >= 0 ; i--){
+            for(int j1 = 0 ; j1 < n ; j1++){
+                for(int j2 = 0 ; j2 < n ; j2++){
+                    
+                    int ans = INT_MIN;
+                    for(int x = -1 ; x <= 1 ; x++){
+                        for(int y = -1; y <= 1 ; y++){
+                            int val = 0;
+                            
+                            if(j1+x >= 0 && j1+ x < n && j2+y >= 0 && j2+y < n){
+                                val = dp[i+1][j1+x][j2+y];    
+                            }
+                            else{
+                                val = INT_MIN;
+                            }
+                            
+                            if(j1 == j2){
+                                val += grid[i][j1];
+                            }
+                            else{
+                                val += (grid[i][j1] + grid[i][j2]);
+                            }
 
-        return f(0,0, n-1, grid, m, n, dp);
+                            ans = max(ans, val);
+                        }
+                    }
+                    
+                    dp[i][j1][j2] = ans;
+                }
+            }
+        }
+        
+        return dp[0][0][n-1];
         
     }
 };
