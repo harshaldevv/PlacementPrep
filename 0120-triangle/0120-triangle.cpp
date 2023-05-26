c72a8b3b60a94ed37c18ec5fdd8f4faaceed6c80
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 10:29:26 +0530

    Time: 7 ms (64.05%), Space: 8.5 MB (90.84%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -24,27 +24,50 @@ public:
         // return f(0,0, triangle, n, dp);
         
         
-        //tabulation done
-        // we go from n-1 to 0 (in tabulation) as we went from 0 to n-1 in memoization.
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        for(int i = n-1 ; i >=0 ; i--){
-            for(int j = i ; j >= 0 ; j--){
-                if(i == n-1){
-                    dp[i][j] = triangle[i][j];
-                }
-                else{
-                    int down = dp[i+1][j];
-                    int dia = dp[i+1][j+1];
+//         //tabulation done
+//         // we go from n-1 to 0 (in tabulation) as we went from 0 to n-1 in memoization.
+//         vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+//         for(int i = n-1 ; i >=0 ; i--){
+//             for(int j = i ; j >= 0 ; j--){
+//                 if(i == n-1){
+//                     dp[i][j] = triangle[i][j];
+//                 }
+//                 else{
+//                     int down = dp[i+1][j];
+//                     int dia = dp[i+1][j+1];
 
-                    dp[i][j] = min(down, dia) + triangle[i][j];
+//                     dp[i][j] = min(down, dia) + triangle[i][j];
                     
-                }
-            }
+//                 }
+//             }
+//         }
+        
+//         return dp[0][0];
+        
+        
+        //tabulation with space optimisation 
+        // we go from n-1 to 0 (in tabulation) as we went from 0 to n-1 in memoization.
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        
+        vector<int> next(n, 0);
+        vector<int> curr(n, 0);
+        
+        for(int i = 0 ; i < n ; i++){
+            next[i] = triangle[n-1][i];
         }
         
-        return dp[0][0];
+        for(int i = n-2 ; i >=0 ; i--){
+            for(int j = i ; j >= 0 ; j--){
+         
+                    int down = next[j];
+                    int dia = next[j+1];
+
+                    curr[j] = min(down, dia) + triangle[i][j];
+            }   
+            next = curr;
+        }
         
-        
+        return next[0];
         
     }
 };
