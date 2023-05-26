ab3cbc1adb4f6ef8f3bec51ddc6d13aed5ca8ce4
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 09:29:36 +0530

    Time: 123 ms (6.89%), Space: 9 MB (30.44%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& triangle, int m, int n, vector<vector<int>> &dp){
-        if(i == m-1 ){
-            return triangle[i][j];
-        }
-        else if(j > n){
-            //redudant base case
-            // we will never go out of boundary even if we move diagnoally
-            // kyuki 0,0 -> 1,1-> 2,2----> hamesha triangle ke andar rahenge.
-            return INT_MAX;
+    
+    int f(int i, int j, vector<vector<int>> &t, int &n, vector<vector<int>> &dp){
+        cout << "i,j " << i << " , " << j << endl;
+        if(i == n-1){
+            return t[i][j];
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        int down =  triangle[i][j] + f(i+1, j, triangle, m, n, dp);
-        int diagonal =  triangle[i][j] +  f(i+1, j+1, triangle, m, n, dp);
+        int down = f(i+1, j, t, n, dp);
+        int dia = f(i+1, j+1, t, n, dp);
         
-        return dp[i][j] = min(down, diagonal) ;
-        
+        return dp[i][j] = (min(down, dia) + t[i][j]);
         
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        //this question is a "Fixed Starting Point and Variable Ending Point"
-        // theefore instead of from last row (variable), bcz we have multiple starting points 
-        // from the last row
-        // but we have only one starting point from the top row.. hence we choose the first row
+        int n = triangle.size();
         
-        // therefore f(0,0) = minimum path sum from 0,0 to the last row
-        
-        //f(0,0) = min( f(1, 0) , f(1, 1) )
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return f(0,0, triangle, n, dp);
         
         
-        
-        int m = triangle.size();
-        int n = triangle[m-1].size();
-        
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return f(0, 0, triangle, m, n, dp);
         
         
     }
