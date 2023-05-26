e545106dfa9965f2f30c8aadd38dba47b38bcccd
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 10:45:07 +0530

    Time: 22 ms (14.78%), Space: 10.5 MB (6.52%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    
-    int f(int i, int j, vector<vector<int>>& matrix, vector<vector<int>> &dp, int &m, int &n){
-        if(i == 0 && j >=0 && j < n){
-            return matrix[i][j];
+    int f(int i, int j, vector<vector<int>> &grid, int &m, int &n, vector<vector<int>> &dp){
+        if(i == 0 && j >= 0 && j < n ){
+            return grid[i][j];
         }
-        else if(j < 0 || j >= n || i < 0){
-            return INT_MAX;
+        
+        //more bases cases
+        if(i < 0 || i >= m || j < 0 || j >= n){
+            return 1e7;
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        int up = f(i-1, j, matrix, dp, m, n) ;
-        int left = f(i-1, j-1, matrix, dp, m, n);
-        int right = f(i-1, j+1, matrix, dp, m, n);
+        int up = f(i-1, j, grid, m, n, dp);
+        int left = f(i-1, j-1, grid, m, n, dp);
+        int right = f(i-1, j+1, grid, m, n, dp);
         
-        return dp[i][j] = min({up, left, right}) + matrix[i][j];
+        return dp[i][j] = min({up, left, right}) + grid[i][j];
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
         int m = matrix.size();
         int n = matrix[0].size();
         
         int ans = INT_MAX;
-        
         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
         
         for(int j = 0 ; j < n ; j++){
-            ans = min(ans, f(m-1, j, matrix, dp, m, n));
+            int temp = f(m-1, j, matrix, m, n, dp);
+            ans = min(ans, temp);
         }
-        
         return ans;
+
         
     }
 };
