7c8e3e11462ea741c78e94c450a0364f4f80e090
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-26 17:08:37 +0530

    Time: 176 ms (34.48%), Space: 17.5 MB (8.42%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    int f(int i, int j1, int j2, vector<vector<int>> &grid, int &m, int &n, vector<vector<vector<int>>> &dp){
+        
+        if(j1 < 0 || j2 < 0 || j1 >= n || j2 >= n || i >= m){
+            return 0;
+        }
+        
+        if(i == m-1){
+            if(j1 == j2){
+                return grid[i][j1];
+            }
+            else{
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
+        
+        if(dp[i][j1][j2] != -1){
+            return dp[i][j1][j2];
+        }
+        
+        int maxi = 0;
+        for(int x = -1 ; x <= 1 ; x++){
+            for(int y = -1 ; y <= 1 ; y++){
+                int val = f(i+1, j1 + x,  j2 + y, grid, m, n, dp);
+                
+                if(j1 == j2){
+                    val += grid[i][j1];
+                }
+                else{
+                    val += grid[i][j1] + grid[i][j2];
+                }
+                
+                maxi= max(maxi,val);
+            }
+        }
+        
+        return dp[i][j1][j2] = maxi;
+        
+        
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        vector < vector < vector < int >>> dp(m+1, vector < vector < int >> (n+1, vector < int> (n+1, -1)));
+        return f(0, 0, n-1, grid, m ,n, dp);
+        
+    }
+};
