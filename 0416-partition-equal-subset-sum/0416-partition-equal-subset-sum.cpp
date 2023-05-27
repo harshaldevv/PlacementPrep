7599f9763ddc1fd4f1c78174a9ba3efd4444c145
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-27 11:25:58 +0530

    Time: 206 ms (78.32%), Space: 13.8 MB (59.00%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -36,8 +36,35 @@ public:
         }
         
         
-        vector<vector<int>> dp(n+1, vector<int> (s/2 +1, -1));
-        return f(n-1, s/2, nums, dp);
+        // vector<vector<int>> dp(n+1, vector<int> (s/2 +1, -1));
+        // return f(n-1, s/2, nums, dp);
+        
+        bool dp[n+1][s +1];
+        
+        memset(dp, false, sizeof(dp));
+        //basecase
+        for(int i = 0 ;i < n ; i++){
+            dp[i][0] = true;
+        }
+        
+        dp[0][nums[0]] = true;
+        
+        int tar = s/2;
+        for(int i = 1 ; i < n; i++){
+            for(int j= 1 ; j <= tar; j++){
+                
+                bool nottake = dp[i-1][j];
+                bool take = false;
+                if(j >= nums[i]){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] =  take | nottake;
+                
+            }
+        }
+        
+        return dp[n-1][tar];
         
     }
 };
