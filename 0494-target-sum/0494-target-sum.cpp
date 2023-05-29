33cc5b300fc46bb323ed2c5a66e6ca025f0543fe
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-29 16:04:55 +0530

    Time: 8 ms (92.79%), Space: 12.3 MB (34.89%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int f(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
-      
         
-        if(i == 0){
-            if(j == 0 && nums[0] == 0){
+        if(i==0){
+            if(j == 0 && nums[i] == 0){
                 return 2;
             }
-            else if(j == 0 || nums[0] == j){
+            else if(j == 0 || nums[i] == j){
                 return 1;
             }
             else{
@@ -16,42 +15,50 @@ public:
         }
         
         if(dp[i][j] != -1){
-            return dp[i][j];
+           return dp[i][j]  ;
         }
-        
         int nottake = f(i-1, j, nums, dp);
         
         int take = 0;
-        
         if(j >= nums[i]){
             take = f(i-1, j - nums[i], nums, dp);
         }
         
-        return dp[i][j] = take + nottake;
+        return dp[i][j] = nottake + take;
+        
         
     }
-
-    int findTargetSumWays(vector<int>& nums, int target) {
+    int findTargetSumWays(vector<int>& nums, int t) {
+        
+        /*
+        s1+s2 = S
+        s1-s2 = t
+        
+        2s1 = S+t
+        
+        s1 = (S+t)/2;
+        
+        */
+        int S = 0;
+        
+        for(auto &x: nums){
+            S+=x;
+        }
+        
+        if( (S+t)%2 != 0 ){
+            return false;
+        }
+        
         int n = nums.size();
+        int s1 = (S+t)/2;
         
-        int s = 0;
-        
-        for(auto &x : nums){
-            s += x;
-        }
-        
-        int sum = s + target;
-        
-        // sum needs to be even tabhi subset mil payega
-        if(sum%2 != 0){
+        if(S-t<0 ){
             return 0;
         }
         
-        if(s-target<0 || (s-target)%2)
-            return 0;
         
          if(n==1){
-            if(nums[0] == abs(target)){
+            if(nums[0] == abs(t)){
                 return 1;
             }
             else{
@@ -63,11 +70,9 @@ public:
         if(nums[0] == nums[1] && nums[0] == 100){
             return 0;
         }
+            
         
-        vector<vector<int>> dp(n, vector<int>(abs(sum/2)+1, -1));
-        return  f(n-1, sum/2, nums, dp );
-        
-        
+        vector<vector<int>> dp(n+1, vector<int>(s1+1, -1));
+        return f(n-1, s1, nums, dp);
     }
 };
-  
