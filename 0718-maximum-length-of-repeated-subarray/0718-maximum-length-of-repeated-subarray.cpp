4caa40c94922a23aeb9a4b9731d036b74ce753c6
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-29 20:02:27 +0530

    Time: 208 ms (75.48%), Space: 85.8 MB (9.84%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -9,18 +9,23 @@ public:
         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
         //base case handled via our dp array ki intialisation
         // dp[0][j]= 0 and dp[i][0] = 0 for all i and j
+        int bada = max(n1, n2);
         
+        vector<int> curr(bada+1, 0);
+        vector<int> prev(bada+1, 0);
         int ans = 0;
         for(int i =1 ; i<= n1 ; i++){
             for(int j =1 ; j <= n2 ; j++){
                 if(nums1[i-1] == nums2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans = max(ans, dp[i][j]);
+                    curr[j] = 1 + prev[j-1];
+                    ans = max(ans, curr[j]);
                 }
                 else{
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }
             }
+            
+            prev = curr;
         }
         
         return ans;
