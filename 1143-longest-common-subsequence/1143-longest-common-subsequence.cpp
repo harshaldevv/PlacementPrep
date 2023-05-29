59e113276c9825c6b42919508d4bd8ed2cb5b4e9
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-29 18:27:36 +0530

    Time: 91 ms (28.44%), Space: 24.3 MB (62.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -24,8 +24,36 @@ public:
         int n1 = s1.size();
         int n2 = s2.size();
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return f(n1-1, n2-1, s1, s2, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return f(n1-1, n2-1, s1, s2, dp);
+        
+        //tabulation
+        
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        
+        // base case -> if either of the string's length is 0, toh ans = 0
+        // ie dp[0][j] = 0 and dp[i][0] = 0 
+        // we've already handled this by initialising dp with 0
+        
+        for(int i = 1 ; i <= n1 ; i++){
+            for(int j = 1 ; j <= n2 ; j++){
+                
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    int a = dp[i-1][j];
+                    int b = dp[i][j -1];
+
+                    dp[i][j] = max(a, b);
+                }   
+            }
+        }
+        
+        return dp[n1][n2];
+        
+        
+        
         
     }
 };
