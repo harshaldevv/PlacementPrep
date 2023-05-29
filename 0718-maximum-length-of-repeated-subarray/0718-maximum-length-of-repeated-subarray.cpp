d7a022cb195642602f4cc8054abceac7d47653f9
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-29 20:10:22 +0530

    Time: 191 ms (78.76%), Space: 10.9 MB (97.79%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -6,24 +6,24 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
-        //base case handled via our dp array ki intialisation
-        // dp[0][j]= 0 and dp[i][0] = 0 for all i and j
+//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+//         //base case handled via our dp array ki intialisation
+//         // dp[0][j]= 0 and dp[i][0] = 0 for all i and j
         
-        int ans = 0;
-        for(int i =1 ; i<= n1 ; i++){
-            for(int j =1 ; j <= n2 ; j++){
-                if(nums1[i-1] == nums2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans = max(ans, dp[i][j]);
-                }
-                else{
-                    dp[i][j] = 0;
-                }
-            }
-        }
+//         int ans = 0;
+//         for(int i =1 ; i<= n1 ; i++){
+//             for(int j =1 ; j <= n2 ; j++){
+//                 if(nums1[i-1] == nums2[j-1]){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                     ans = max(ans, dp[i][j]);
+//                 }
+//                 else{
+//                     dp[i][j] = 0;
+//                 }
+//             }
+//         }
         
-        // return ans;
+//         return ans;
         
         //space optimisation dp -> using 2 1D array
         
@@ -39,7 +39,7 @@ public:
             for(int j =1 ; j <= n2 ; j++){
                 if(nums1[i-1] == nums2[j-1]){
                     curr[j] = 1 + prev[j-1];
-                    ANS = max(ans, curr[j]);
+                    ANS = max(ANS, curr[j]);
                 }
                 else{
                     curr[j] = 0;
