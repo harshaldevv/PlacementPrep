aae75784f26cba622f3de58e416a2c6cf37efa19
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-30 17:24:56 +0530

    Time: 12 ms (74.91%), Space: 9 MB (41.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -29,8 +29,38 @@ public:
         
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return f(n1, n2, word1, word2, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return f(n1, n2, word1, word2, dp);
         
+        
+        //tabulation
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        
+        for(int j = 0 ; j <= n2 ; j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i = 0 ; i <= n1 ; i++){
+            dp[i][0] = i;
+        }
+        
+        
+        for(int i= 1 ; i <= n1 ; i++){
+            for(int j = 1 ; j <= n2 ; j++){
+                 if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert = dp[i][j-1];
+                    int delet = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+
+                    dp[i][j] = min({insert, delet, replace}) +1;
+                }
+            }
+        }
+        
+        return dp[n1][n2];
+
     }
 };
