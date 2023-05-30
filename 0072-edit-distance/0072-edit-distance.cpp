9562df342b361eb3affb8e16aaffc7f9be4b1db3
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-30 17:07:22 +0530

    Time: 13 ms (69.46%), Space: 9.4 MB (19.63%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+        if(i<0){
+            return j+1;
+        }
+        
+        if(j<0){
+            return i+1;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1[i] == s2[j]){
+            return dp[i][j] = f(i-1, j-1, s1, s2, dp);
+        }
+        else{
+            int insert = f(i, j-1, s1, s2, dp);
+            int delet = f(i-1, j, s1, s2, dp);
+            int replace = f(i-1, j-1, s1, s2, dp);
+            
+            return dp[i][j] = min({insert, delet, replace}) +1;
+        }
+        
+    }
+    int minDistance(string word1, string word2) {
+        
+        int n1 = word1.size();
+        int n2 = word2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return f(n1-1, n2-1, word1, word2, dp);
+        
+    }
+};
