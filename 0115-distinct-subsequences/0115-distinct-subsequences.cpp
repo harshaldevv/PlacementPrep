a4023bfdbc1755127e9a8b3c04ad6bb141529d65
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-30 14:35:30 +0530

    Time: 43 ms (73.48%), Space: 18.6 MB (46.81%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+       if(j < 0){
+           return 1;
+       }
+        if(i<0){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1[i] == s2[j]){
+            return dp[i][j] = f(i-1, j-1, s1, s2, dp) + f(i-1, j, s1, s2, dp);
+        }
+        else{
+            return dp[i][j] =  f(i-1, j, s1, s2, dp);
+        }
+       
+    }
+    int numDistinct(string s, string t) {
+        //lcs pattern detected -> 2/3 or even 3/3 matching -> aditya verma
+        
+        int n1 = s.size();
+        int n2 = t.size();
+        
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return f(n1 -1 , n2 - 1 , s, t, dp);
+        
+    }
+};
