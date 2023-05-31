52c9e565e9f7a2450801ecd84ba42cd197bfd055
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-31 12:28:35 +0530

    Time: 7 ms (72.18%), Space: 12.9 MB (99.74%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int f(int i, vector<int> &prices, bool youcanBuy){
+        
+        if(i>=prices.size()){
+            return 0;
+            //end of the days
+        }
+        
+        if(youcanBuy){
+            //two options -> buy or not buy
+            int notBuy = f(i+1, prices, true);
+            int iBuy = -prices[i] + f(i+1, prices, false);
+            return max(notBuy, iBuy);
+        }
+        else{
+            //sell or not sell
+            int notsell = f(i+1, prices, false);
+            int sell = prices[i] + f(i+1, prices, true);
+            
+            return max(sell, notsell);
+        }
+        
+        
+        
+        
+    }
+    int maxProfit(vector<int>& prices) {
+        //maybe knapsack pattern
+        int n = prices.size();
+//         bool youcanBuy = true;
+        
+//         vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        
+//         return f(0, prices, youcanBuy, dp);
+        
+        int ans = 0;
+        for(int i = 1 ; i < n ;i++){
+            if(prices[i] >= prices[i-1]){
+                ans += prices[i] - prices[i-1];
+            }
+        }
+        
+        return ans;
+        
+    }
+};
