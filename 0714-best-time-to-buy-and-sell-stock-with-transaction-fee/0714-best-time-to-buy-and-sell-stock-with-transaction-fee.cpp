efcdf466797fae4cfb51bf2f35b8636f905d3838
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-01 00:11:46 +0530

    Time: 137 ms (63.46%), Space: 55 MB (78.82%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -65,9 +65,6 @@ public:
         vector<int> after(3, 0);
         vector<int> curr(3, 0);
         
-        // i -> 0 to n-1 --> here n-1 to 0;
-        // 1 --> 0 to 1
-        
         for(int i = n-1 ; i>= 0; i--){
             for(int j = 0 ; j <= 1 ; j++){
                 
