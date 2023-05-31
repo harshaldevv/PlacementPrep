ba1260d0597168ae9aac26570f2f17c98c249d07
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-31 18:51:35 +0530

    Time: 1201 ms (5.01%), Space: 314.6 MB (5.01%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -31,12 +31,43 @@ public:
         int n = prices.size();
         
         // max buy 2 times
-        bool youcanbuy = true;
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(3, -1)));
-        return f(0, youcanbuy, 0, prices, dp);
+        // bool youcanbuy = true;
+        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(3, -1)));
+        // return f(0, youcanbuy, 0, prices, dp);
         
         //tabulation
         
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(5, vector<int>(5, -1)));
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(4, vector<int>(4, -1)));
+        
+        //base case
+        dp[n][0][0] = 0;
+        dp[n][0][1] = 0;
+        dp[n][0][2] = 0;
+        
+        dp[n][1][0] = 0;
+        dp[n][1][1] = 0;
+        dp[n][1][2] = 0;
+        
+        for(int i = n-1 ; i>= 0 ; i--){ // index
+            for(int j = 0; j<= 1 ; j++){ // youcanbuy
+                for(int k = 0; k <= 2 ; k++){ //count
+                    
+                    if(j){
+                        // buy, not buy
+                        int buy = -prices[i] + dp[i+1][false][k+1];
+                        int notbuy = dp[i+1][true][k];
+                        dp[i][j][k] = max(buy, notbuy);
+                    }
+                    else{
+                        //sell, notsell
+                        int sell = +prices[i] + dp[i+1][true][k];
+                        int notsell  = dp[i+1][false][k];
+                        dp[i][j][k] = max(sell, notsell);
+                    }
+                }
+            }
+        }
+        
+        return dp[0][1][0];
     }
 };
