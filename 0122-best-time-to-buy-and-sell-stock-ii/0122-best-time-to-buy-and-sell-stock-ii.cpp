c22bd2bf98e429e6a55bb0dad25e9fa590a58248
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-31 17:44:49 +0530

    Time: 7 ms (72.18%), Space: 13 MB (54.10%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -32,25 +32,26 @@ public:
     int maxProfit(vector<int>& prices) {
         //maybe knapsack pattern
         int n = prices.size();
-        bool youcanBuy = true;
-        vector<vector<int>> dp(n+1, vector<int>(2, -1));        
-        return f(0, prices, youcanBuy, dp);
+        // bool youcanBuy = true;
+        // vector<vector<int>> dp(n+1, vector<int>(2, -1));        
+        // return f(0, prices, youcanBuy, dp);
         
         
         /*
+        GREEDY METHOD
         There is a classical saying in the stock market - "Buy the Dip".
         Same concept you need to apply here
         Compare two consecutive days and if you are making a profit, 
         just buy on 1st day and sell on 2nd day
         */
-//         int ans = 0;
-//         for(int i = 1 ; i < n ;i++){
-//             if(prices[i] >= prices[i-1]){
-//                 ans += prices[i] - prices[i-1];
-//             }
-//         }
+        int ans = 0;
+        for(int i = 1 ; i < n ;i++){
+            if(prices[i] >= prices[i-1]){
+                ans += prices[i] - prices[i-1];
+            }
+        }
         
-//         return ans;
+        return ans;
         
     }
 };
