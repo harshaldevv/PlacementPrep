e12c79a24bac206a9ec4becbf16340f194045f9b
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-05-31 22:50:17 +0530

    Time: 8 ms (58.19%), Space: 15.2 MB (27.00%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -37,21 +37,44 @@ public:
         // return f(0, prices, youcanBuy, dp);
         
         
-        /*
-        GREEDY METHOD
-        There is a classical saying in the stock market - "Buy the Dip".
-        Same concept you need to apply here
-        Compare two consecutive days and if you are making a profit, 
-        just buy on 1st day and sell on 2nd day
-        */
-        int ans = 0;
-        for(int i = 1 ; i < n ;i++){
-            if(prices[i] >= prices[i-1]){
-                ans += prices[i] - prices[i-1];
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));        
+        for(int i = n-1 ; i >=0 ; i--){
+            for(int j = 0 ; j <= 1 ; j++){
+                
+                if(j){
+                    //two options -> buy or not buy
+                    int notBuy = dp[i+1][true];
+                    int iBuy = -prices[i] + dp[i+1][false];
+                    dp[i][j] = max(notBuy, iBuy);
+                }
+                else{
+                    //sell or not sell
+                    int notsell = dp[i+1][false];
+                    int sell = prices[i] + dp[i+1][true];
+
+                    dp[i][j] = max(sell, notsell);
+                }  
             }
         }
         
-        return ans;
+        return dp[0][1];
+        
+        
+//         /*
+//         GREEDY METHOD
+//         There is a classical saying in the stock market - "Buy the Dip".
+//         Same concept you need to apply here
+//         Compare two consecutive days and if you are making a profit, 
+//         just buy on 1st day and sell on 2nd day
+//         */
+//         int ans = 0;
+//         for(int i = 1 ; i < n ;i++){
+//             if(prices[i] >= prices[i-1]){
+//                 ans += prices[i] - prices[i-1];
+//             }
+//         }
+        
+//         return ans;
         
     }
 };
