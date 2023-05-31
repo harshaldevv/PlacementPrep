84b84730bc4e7ba3b58fbfe631b1f964124c8bc5
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-01 01:16:05 +0530

    Time: 1495 ms (12.66%), Space: 292.4 MB (7.90%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,32 @@
 class Solution {
-    
-        
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        //hci researhc
-        int n = nums.size();
-        vector<int> dp(n, 0);
-        int len = 0;
-        for (int num : nums) {
-            int pos = lower_bound(dp.begin(), dp.begin() + len, num) - dp.begin();
-            dp[pos] = num;
-            if (pos == len) {
-                len++;
-            }
+    int f(int i, int prevIndex, vector<int> &nums, vector<vector<int>> &dp){
+        if( i>= nums.size()){
+            return 0;
         }
-        return len;
+        if(dp[i][prevIndex+1] != -1){
+            return dp[i][prevIndex+1];
+        }
+        
+        int nottake = f(i+1, prevIndex, nums, dp);
+        
+        int take = 0;
+        
+        if(prevIndex == -1 || nums[i] > nums[prevIndex]){
+            take = 1 + f(i+1, i, nums, dp);
+        }
+        //bas store karne ke lie prevIndex me +1 kia, kyuki prevIndex goes from -1 to n-1 
+        // therefore prevIndex +1 -> goes from 0 to n.
+        return dp[i][prevIndex+1] = max(take, nottake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        // f(i, prevIndx) -> means length of LIS starting from i indx 
+        // and having prevIndx as previous Index
+        
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return f(0, -1, nums, dp);
         
     }
 };
