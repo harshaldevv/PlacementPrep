64784e53b81c4799a0281f851b677ca87a336a49
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 15:38:55 +0530

    Time: 94 ms (25.52%), Space: 24.6 MB (12.05%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,85 +1,29 @@
 class Solution {
 public:
+    
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i< 0 || j < 0 ){
+        if(i < 0 || j < 0 ){
             return 0;
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
+        
         if(s1[i] == s2[j]){
-            return dp[i][j] = 1 + f(i-1, j-1, s1, s2, dp);
+            return dp[i][j] =  1 + f(i-1, j-1, s1, s2, dp);
         }
         else{
-            int a = f(i-1, j, s1, s2, dp);
-            int b = f(i,j -1, s1, s2, dp);
-            
-            return dp[i][j] = max(a, b);
+            return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp));
         }
-        
     }
     int longestCommonSubsequence(string s1, string s2) {
         
-        int n1 = s1.size();
+        int n1  = s1.size();
         int n2 = s2.size();
         
-        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        // return f(n1-1, n2-1, s1, s2, dp);
-        
-        //tabulation
-        
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
-        
-        // base case -> if either of the string's length is 0, toh ans = 0
-        // ie dp[0][j] = 0 and dp[i][0] = 0 
-        // we've already handled this by initialising dp with 0
-        string ans = "";
-        for(int i = 1 ; i <= n1 ; i++){
-            for(int j = 1 ; j <= n2 ; j++){
-                
-                if(s1[i-1] == s2[j-1]){
-                    // ans += s1[i-1]; // for printing the LCS
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    int a = dp[i-1][j];
-                    int b = dp[i][j -1];
-
-                    dp[i][j] = max(a, b);
-                }   
-            }
-        }
-        // cout << ans << endl; // this works too but ig issi case me karega
-        
-        //for prinitng the answer string
-        
-        int i = n1;
-        int j = n2;
-        
-        while(i>0 && j > 0){
-            if(s1[i-1] == s2[j-1]){
-                //matched
-                ans += s1[i-1];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                i--;
-            }
-            else{
-                j--;
-            }
-        }
-        
-        reverse(ans.begin(), ans.end());
-        
-        cout << "ans = " << ans << endl ;
-            
-        return dp[n1][n2];
-        
-        
-        
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return f(n1-1, n2-1, s1, s2, dp);
         
     }
 };
