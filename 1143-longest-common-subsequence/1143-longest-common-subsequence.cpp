c90144027588e6a235eb367532bdd6c4d5ae77d5
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 15:44:03 +0530

    Time: 40 ms (82.42%), Space: 6.5 MB (96.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -25,22 +25,44 @@ public:
         // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
         // return f(n1-1, n2-1, s1, s2, dp);
         
-        //tabulation
+//         //tabulation
         
-        vector<vector<int>> dp(n1+2, vector<int>(n2+2, 0));
+//         vector<vector<int>> dp(n1+2, vector<int>(n2+2, 0));
+        
+//         for(int i =1 ; i<= n1 ; i++){
+//             for(int j = 1 ; j <= n2 ; j++){
+//                 if(s1[i-1] == s2[j-1]){
+//                     dp[i][j] =  1 + dp[i-1][j-1];
+//                 }
+//                 else{
+//                     dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+//                 }
+//             }
+//         }
+        
+//         return dp[n1][n2];
+        
+        
+        //tabulation with space
+        
+        vector<int> curr (n2+2, 0);
+        vector<int> prev (n2+2, 0);
+        
         
         for(int i =1 ; i<= n1 ; i++){
             for(int j = 1 ; j <= n2 ; j++){
                 if(s1[i-1] == s2[j-1]){
-                    dp[i][j] =  1 + dp[i-1][j-1];
+                    curr[j] =  1 + prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    curr[j] = max(prev[j], curr[j-1]);
                 }
             }
+            
+            prev = curr;
         }
         
-        return dp[n1][n2];
+        return prev[n2];
         
         
     }
