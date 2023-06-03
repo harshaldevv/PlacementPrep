1cdac8599894514935ca033fed0801656e10f325
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 17:14:59 +0530

    Time: 328 ms (5.06%), Space: 73 MB (47.70%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    
     int LCS(string &s1, string &s2){
-        
         int n1 = s1.size();
         int n2 = s2.size();
         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
-        
-        //basecase handled
-        // dp[0][j] = 0 and dp[i][0] = 0;
-        
-        for(int i= 1 ; i <= n1 ; i++){
-            for(int j = 1 ; j <= n2 ; j++){
+        for(int i = 1 ; i <= n1 ; i++){
+            for(int j = 1; j <= n2 ; j++){
                 if(s1[i-1] == s2[j-1]){
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
@@ -25,12 +19,12 @@ public:
     }
     int longestPalindromeSubseq(string s) {
         
-        string a = s;
+        string s1 = s;
+        string s2 = s;
         
-        reverse(s.begin(), s.end());
-        string b = s;
+        reverse(s2.begin(), s2.end());
         
-        return LCS(a, b);
+        return LCS(s1, s2);
         
     }
 };
