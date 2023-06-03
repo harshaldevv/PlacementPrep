639d8e33830aa7f9bd4a965401500b51af7b88b6
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-04 00:21:29 +0530

    Time: 122 ms (76.44%), Space: 54.9 MB (89.66%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,93 +1,36 @@
 class Solution {
 public:
-    int f(int i, bool youcanbuy, vector<int>&prices, int &fees, vector<vector<int>> &dp){
-        if(i >= prices.size()){
-            return 0;
-        }
-        
-        if(dp[i][youcanbuy] != -1){
-            return dp[i][youcanbuy];
-        }
-        
-        if(youcanbuy){
-            //buy, not buy
-            int buy = -prices[i] - fees + f(i+1, false, prices, fees, dp);
-            int notbuy = f(i+1, true, prices, fees, dp);
-            return dp[i][youcanbuy] = max(buy, notbuy);
-        }
-        else{
-            //sell, not sell
-            int sell = +prices[i] + f(i+1, true, prices, fees, dp);
-            int notsell = f(i+1, false, prices, fees, dp);
-            
-            return dp[i][youcanbuy] = max(sell, notsell);
-        }
-        
-        
-    }
-    int maxProfit(vector<int>& prices, int &fees) {
+    int maxProfit(vector<int>& prices, int fee) {
         
         int n = prices.size();
-        // bool youcanbuy = true;
-        // vector<vector<int>> dp(n+1, vector<int>(3, -1));
-        // return f(0, youcanbuy, prices, fees, dp);
-        
-//         //tabulation
-//         vector<vector<int>> dp(n+1, vector<int>(3, 0));
-//         // i -> 0 to n-1 --> here n-1 to 0;
-//         // 1 --> 0 to 1
-        
-//         for(int i = n-1 ; i>= 0; i--){
-//             for(int j = 0 ; j <= 1 ; j++){
-                
-//                 if(j){
-//                     //buy, not buy
-//                     int buy = -prices[i] - fees + dp[i+1][false];
-//                     int notbuy = dp[i+1][true];
-                    
-//                     dp[i][j] = max(buy, notbuy);
-//                 }
-//                 else{
-//                     //sell, not sell
-//                     int sell = +prices[i] + dp[i+1][true];
-//                     int notsell = dp[i+1][false];
-
-//                     dp[i][j] = max(sell, notsell);
-//                 }
-                
-//             }
-//         }
-        
-//         return dp[0][1];
         
         
-        //tabulation with space optimisation
-        vector<int> after(3, 0);
-        vector<int> curr(3, 0);
+        // tabulation with space optimization
+        vector<int> after(2, 0);        
+        vector<int> curr (2, 0);
         
-        for(int i = n-1 ; i>= 0; i--){
+        for(int i = n-1 ; i >=0 ; i--){
             for(int j = 0 ; j <= 1 ; j++){
                 
                 if(j){
-                    //buy, not buy
-                    int buy = -prices[i] - fees + after[false];
-                    int notbuy = after[true];
-                    
-                    curr[j] = max(buy, notbuy);
+                    //two options -> buy or not buy
+                    int notBuy = after[true];
+                    int iBuy = -prices[i] -fee + after[false];
+                    curr[j] = max(notBuy, iBuy);
                 }
                 else{
-                    //sell, not sell
-                    int sell = +prices[i] + after[true];
+                    //sell or not sell
                     int notsell = after[false];
-
+                    int sell = prices[i] + after[true];
                     curr[j] = max(sell, notsell);
-                } 
+                }  
             }
             
             after = curr;
         }
         
-        return after[1];
+        return curr[1];
+        
         
         
         
