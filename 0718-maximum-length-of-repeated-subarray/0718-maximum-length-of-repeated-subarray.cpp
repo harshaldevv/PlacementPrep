db86705bf0277bbf5010ae233769b412bf1aa601
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 16:09:45 +0530

    Time: 322 ms (38.28%), Space: 85.6 MB (43.12%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,55 +1,27 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
-        //this is longest common substring
         
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
-//         //base case handled via our dp array ki intialisation
-//         // dp[0][j]= 0 and dp[i][0] = 0 for all i and j
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
         
-//         int ans = 0;
-//         for(int i =1 ; i<= n1 ; i++){
-//             for(int j =1 ; j <= n2 ; j++){
-//                 if(nums1[i-1] == nums2[j-1]){
-//                     dp[i][j] = 1 + dp[i-1][j-1];
-//                     ans = max(ans, dp[i][j]);
-//                 }
-//                 else{
-//                     dp[i][j] = 0;
-//                 }
-//             }
-//         }
+        int maxLen = 0;
         
-//         return ans;
-        
-        //space optimisation dp -> using 2 1D array
-        
-        
-        //base case handled via our dp array ki intialisation
-        // dp[0][j]= 0 and dp[i][0] = 0 for all i and j
-        int bada = max(n1, n2);
-        vector<int> curr(bada+1, 0);
-        vector<int> prev(bada+1, 0);
-        
-        int ANS = 0;
-        for(int i =1 ; i<= n1 ; i++){
-            for(int j =1 ; j <= n2 ; j++){
+        for(int i = 1 ; i <= n1 ; i++){
+            for(int j = 1 ; j <= n2 ; j++){
                 if(nums1[i-1] == nums2[j-1]){
-                    curr[j] = 1 + prev[j-1];
-                    ANS = max(ANS, curr[j]);
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    maxLen = max(maxLen, dp[i][j]);
                 }
                 else{
-                    curr[j] = 0;
+                    dp[i][j] = 0;
                 }
             }
-            
-            prev = curr;
         }
         
-        return ANS;
+        return maxLen;
         
     }
 };
