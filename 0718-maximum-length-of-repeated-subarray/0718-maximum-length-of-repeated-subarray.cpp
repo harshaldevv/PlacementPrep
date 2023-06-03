7d6c8a6212b2c5ce32e51a823a3037efb16bb9b3
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 16:11:00 +0530

    Time: 183 ms (83.22%), Space: 11 MB (93.89%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -5,20 +5,41 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        
+//         int maxLen = 0;
+        
+//         for(int i = 1 ; i <= n1 ; i++){
+//             for(int j = 1 ; j <= n2 ; j++){
+//                 if(nums1[i-1] == nums2[j-1]){
+//                     dp[i][j] = 1 + dp[i-1][j-1];
+//                     maxLen = max(maxLen, dp[i][j]);
+//                 }
+//                 else{
+//                     dp[i][j] = 0;
+//                 }
+//             }
+//         }
+        
+//         return maxLen;
+        
+        
+        vector<int> curr (n2+1, 0);
+        vector<int> prev (n2+1, 0);
         
         int maxLen = 0;
         
         for(int i = 1 ; i <= n1 ; i++){
             for(int j = 1 ; j <= n2 ; j++){
                 if(nums1[i-1] == nums2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    maxLen = max(maxLen, dp[i][j]);
+                    curr[j] = 1 + prev[j-1];
+                    maxLen = max(maxLen, curr[j]);
                 }
                 else{
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }
             }
+            prev = curr;
         }
         
         return maxLen;
