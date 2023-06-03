9b0d34fd5ac75257536673f37666c1db276f4b2a
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 23:46:19 +0530

    Time: 366 ms (58.63%), Space: 135.2 MB (55.14%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -66,9 +66,40 @@ public:
 //         vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(4, -1)));
 //         return f(0, youcanbuy, counter, prices, dp);
         
-        int transactionCounter = 0;
-        vector<vector<int>> dp(n+1, vector<int>(5, -1));
-        return ff(0, transactionCounter, prices, dp);
+        // int transactionCounter = 0;
+        // vector<vector<int>> dp(n+1, vector<int>(5, -1));
+        // return ff(0, transactionCounter, prices, dp);
+        
+
+        //do tabulation wala code.
+        
+        vector<vector<int>> dp(n+1, vector<int>(5, 0));
+        for(int i = n -1 ; i >= 0 ; i--){
+            for(int j = 3 ; j>=0 ; j--){
+                
+                
+                if(j%2 == 0){
+                    //buy
+                    // buy, not buy
+                    int buy = -prices[i] + dp[i+1][j+1];
+                    int notbuy = dp[i+1][j];
+                    dp[i][j] =  max(buy, notbuy);
+                }
+                else{
+                    //sell
+                    // sell, not sell
+                    int sell = +prices[i] + dp[i+1][j+1];
+                    int notsell = dp[i+1][j];
+
+                    dp[i][j] =  max(sell, notsell);
+                }
+                
+            }
+        }
+        
+        return dp[0][0];
+        
+        
     }
     
 };
