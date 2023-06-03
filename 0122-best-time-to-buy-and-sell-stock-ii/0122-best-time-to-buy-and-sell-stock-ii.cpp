676ae21ab1535c19dd142765ac77ca76489d3769
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 19:42:11 +0530

    Time: 9 ms (55.45%), Space: 12.9 MB (82.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,66 +1,121 @@
 class Solution {
 public:
-    
-    int f(int i, bool canyoubuy, vector<int> &prices, vector<vector<int>> &dp){
+    int f(int i, vector<int> &prices, bool youcanBuy, vector<vector<int>> &dp){
         
-        if(i >= prices.size()){
+        if(i>=prices.size()){
             return 0;
+            //end of the days
         }
         
-        if(dp[i][canyoubuy] != -1){
-            return dp[i][canyoubuy];
+        if(dp[i][youcanBuy] != -1){
+            return dp[i][youcanBuy];
         }
         
-        if(canyoubuy){
-            // buy, not buy
-            int buy = -prices[i] + f(i+1, false, prices, dp);
-            int notbuy = f(i+1, true, prices, dp);
-            
-            return dp[i][canyoubuy] = max(buy, notbuy);
+        if(youcanBuy){
+            //two options -> buy or not buy
+            int notBuy = f(i+1, prices, true, dp);
+            int iBuy = -prices[i] + f(i+1, prices, false, dp);
+            return dp[i][youcanBuy] = max(notBuy, iBuy);
         }
         else{
-            // sell, not sell
-            int sell = +prices[i] + f(i+1, true, prices, dp);
-            int notsell = f(i+1, false, prices, dp);
-            return dp[i][canyoubuy] = max(sell, notsell);
+            //sell or not sell
+            int notsell = f(i+1, prices, false, dp);
+            int sell = prices[i] + f(i+1, prices, true, dp);
+            
+            return dp[i][youcanBuy] = max(sell, notsell);
         }
+        
+        
+        
+        
     }
     int maxProfit(vector<int>& prices) {
-        
+        //maybe knapsack pattern
         int n = prices.size();
+        // bool youcanBuy = true;
+        // vector<vector<int>> dp(n+1, vector<int>(2, -1));        
+        // return f(0, prices, youcanBuy, dp);
         
-        bool youcanbuy = true;
-        
-        
-        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        // return f(0, youcanbuy, prices, dp);
-        
-        
-        //tabulation
-        vector<int> curr (2, 0);
-        vector<int> next (2, 0);
-        
-        for(int i = n-1 ; i >=0 ; i--){
-            for(int j = 0 ; j <= 1 ; j++){
-                if(j){
-                    // buy, not buy
-                    int buy = -prices[i] + next[false];
-                    int notbuy = next[true];
+//         // tabulation
+//         vector<vector<int>> dp(n+1, vector<int>(2, 0));        
+//         for(int i = n-1 ; i >=0 ; i--){
+//             for(int j = 0 ; j <= 1 ; j++){
+                
+//                 if(j){
+//                     //two options -> buy or not buy
+//                     int notBuy = dp[i+1][true];
+//                     int iBuy = -prices[i] + dp[i+1][false];
+//                     dp[i][j] = max(notBuy, iBuy);
+//                 }
+//                 else{
+//                     //sell or not sell
+//                     int notsell = dp[i+1][false];
+//                     int sell = prices[i] + dp[i+1][true];
 
-                    curr[j] = max(buy, notbuy);
-                }
-                else{
-                    // sell, not sell
-                    int sell = +prices[i] + next[true];
-                    int notsell = next[false];
-                    
-                    curr[j] = max(sell, notsell);
-                }
+//                     dp[i][j] = max(sell, notsell);
+//                 }  
+//             }
+//         }
+        
+//         return dp[0][1];
+        
+        
+//         /*
+//         GREEDY METHOD
+//         There is a classical saying in the stock market - "Buy the Dip".
+//         Same concept you need to apply here
+//         Compare two consecutive days and if you are making a profit, 
+//         just buy on 1st day and sell on 2nd day
+//         */
+        int ans = 0;
+        for(int i = 1 ; i < n ;i++){
+            if(prices[i] >= prices[i-1]){
+                ans += prices[i] - prices[i-1];
             }
-            next = curr;
         }
         
-        return next[true];
+        return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         // tabulation with space optimization
+//         vector<int> after(2, 0);        
+//         vector<int> curr (2, 0);
+        
+//         for(int i = n-1 ; i >=0 ; i--){
+//             for(int j = 0 ; j <= 1 ; j++){
+                
+//                 if(j){
+//                     //two options -> buy or not buy
+//                     int notBuy = after[true];
+//                     int iBuy = -prices[i] + after[false];
+//                     curr[j] = max(notBuy, iBuy);
+//                 }
+//                 else{
+//                     //sell or not sell
+//                     int notsell = after[false];
+//                     int sell = prices[i] + after[true];
+//                     curr[j] = max(sell, notsell);
+//                 }  
+//             }
+            
+//             after = curr;
+//         }
+        
+//         return curr[1];
+        
+        
+        
+        
+        
+        
         
         
     }
