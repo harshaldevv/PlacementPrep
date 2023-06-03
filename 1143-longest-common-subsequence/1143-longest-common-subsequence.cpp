4a0379a6f70121a9019d1abe02d7558dd39f2416
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2023-06-03 15:42:30 +0530

    Time: 60 ms (66.44%), Space: 24.5 MB (46.49%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i < 0 || j < 0 ){
+        if(i == 0 || j == 0 ){
             return 0;
         }
         
@@ -10,7 +10,7 @@ public:
             return dp[i][j];
         }
         
-        if(s1[i] == s2[j]){
+        if(s1[i-1] == s2[j-1]){
             return dp[i][j] =  1 + f(i-1, j-1, s1, s2, dp);
         }
         else{
@@ -22,8 +22,26 @@ public:
         int n1  = s1.size();
         int n2 = s2.size();
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return f(n1-1, n2-1, s1, s2, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return f(n1-1, n2-1, s1, s2, dp);
+        
+        //tabulation
+        
+        vector<vector<int>> dp(n1+2, vector<int>(n2+2, 0));
+        
+        for(int i =1 ; i<= n1 ; i++){
+            for(int j = 1 ; j <= n2 ; j++){
+                if(s1[i-1] == s2[j-1]){
+                    dp[i][j] =  1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[n1][n2];
+        
         
     }
 };
