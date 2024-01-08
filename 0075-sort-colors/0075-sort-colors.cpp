95f54015a88128dbf495f4c31f0df8a47de50efe
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-01-09 00:37:09 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (37.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
         
         int l = 0;
-        int r = n-1;
-        
+        int r = nums.size() -1;
         int i = 0;
         
         while(i<=r){
-            if(nums[i] == 0){
-                swap(nums[i++], nums[l++]);
-            }
-            else if(nums[i] == 2){
+            if(nums[i] == 2){
                 swap(nums[i], nums[r]);
                 r--;
+            }
+            else if(nums[i] == 0){
+                swap(nums[i], nums[l]);
+                l++;
+                i++;
             }
             else{
                 i++;
             }
         }
         
-        return ;
+        return;
         
     }
 };
