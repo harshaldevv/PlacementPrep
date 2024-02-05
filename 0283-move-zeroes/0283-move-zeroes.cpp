b30e8a91f4e4783ba9e703b33d26fa134672cbfc
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-02-05 23:29:57 +0530

    Time: 23 ms (26.25%), Space: 21.4 MB (28.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-
-        vector<int> ans(nums.size(), 0);
-        
         int j = 0;
-        for(int i = 0 ; i <nums.size() ; i++){
+        
+        for(int i = 0 ; i < nums.size() ; i++){
             if(nums[i] != 0){
-                ans[j++] = nums[i];
+                swap(nums[j], nums[i]);
+                j++;
+                
             }
         }
         
         
-        nums = ans;
+        return;
         
-        return ;
         
         
         
