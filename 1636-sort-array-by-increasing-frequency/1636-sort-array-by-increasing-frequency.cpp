efd880669ea58ff32301d8df31af6c3710e5144e
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-02-13 13:15:51 +0530

    Time: 10 ms (29.96%), Space: 15 MB (7.33%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,57 +1,53 @@
 class Solution {
 public:
-    class compareFunc{
-        public:
-        bool operator()(pair<int,int> &a, pair<int,int> &b){
-            if(a.first== b.first){
-                return a.second < b.second;
-            }
-            else{
-                return a.first > b.first;
-            }
-            
-        }
-    };
+    
+class Compare {
+public:
+    bool operator()(pair<int, int> a, pair<int, int>b){
+       if(a.first == b.first){
+           return a.second < b.second;
+       }
+        else{
+            return a.first > b.first;
+        }   
+    }
+};
+    
     vector<int> frequencySort(vector<int>& nums) {
         
-        unordered_map<int,int> mp;
+        // minheap with custom comp
         
-        for(auto &x :nums){
+        unordered_map<int, int> mp;
+        
+        
+        for(auto &x : nums){
             mp[x]++;
         }
         
-        // // hitesh way
-        // sort(nums.begin(), nums.end(), [&](auto &a, auto &b)->bool{
-        //     if(mp[a] == mp[b]){
-        //         return a > b;
-        //     }
-        //     else{
-        //         return mp[a] < mp[b];
-        //     }
-        // });
-        // return nums;
         
-        //Myway
-        priority_queue< pair<int,int> , vector<pair<int,int>>, compareFunc> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        
+        
         for(auto &x : mp){
-            pq.push({x.second, x.first});
-        }
-        vector<int> ans;
-        while(!pq.empty()){
-            int f = pq.top().first;
-            int e = pq.top().second;
-            while(f--){
-                ans.push_back(e);    
-            }
+            int f = x.second;
+            int numb = x.first;
             
+            pq.push({f, numb});
+        }
+        
+        vector<int> ans;
+        
+        while(!pq.empty()){
+            auto t = pq.top();
             pq.pop();
+            int f = t.first;
+            int numb = t.second;
+            while(f--){
+                ans.push_back(numb);    
+            }
         }
         
         return ans;
-        
-        
-        
-        
         
     }
 };
