ab5e2b985c210e1a08d6034a84d12ede7f4562c6
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-02-18 22:54:58 +0530

    Time: 84 ms (38.29%), Space: 57.5 MB (21.99%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& h, int bricks, int ladders) {
-        int i = 0 ;
         
         priority_queue<int> pq;
-        for(; i < h.size() -1; i++ ){
-            if(h[i] >= h[i+1] ){
+        
+        //bricks are our limiting factor, as ladders can help us jump any height
+        for(int i = 0 ; i < h.size()  -1 ; i++){
+            
+            if(h[i] > h[i+1]){
                 continue;
             }
             else{
-                // now we need to jump using ladder or bricks
+                // now we need to use bricks or ladder
                 
+                // use bricks
                 
                 int diff = abs(h[i] - h[i+1]);
-                bricks -=diff;
+                bricks-=diff;
                 pq.push(diff);
                 
                 // if bricks become negetive then there were not enough bricks. 
                 // So add a ladder in place of the step where most bricks were used
                 if(bricks < 0){
-                    // here use ladder
                     bricks += pq.top();
                     pq.pop();
                     
-                    if(ladders > 0){
-                        ladders--;
-                    }
-                    else{
-                        return i;
-                    }
+                    ladders--;
                 }
                 
+                if(ladders < 0){
+                    return i;
+                }
             }
         }
         
-        return i;
+        return h.size() - 1; // we crossed the whole array
         
     }
 };
