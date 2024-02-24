66c70d6a0513b243286927525813b969b2a22b73
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-02-24 14:49:44 +0530

    Time: 5 ms (82.10%), Space: 12.3 MB (55.57%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,43 +2,57 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         
+        vector<int> ans(2,-1);
+        ans[0] = nums[0];
+        ans[1] = nums[1];
         
-        int ans = 0;
+        
+        // xorr of all elements --> now any arbit set bit of xorr 
+        // --> now this same bit, it will set in A and unset in B
+        // --> If bit_i in (a xor b) is 1, bit_i at a and b are different.
+        
+        int xorr = 0;
         
         for(auto &x : nums){
-            ans ^= x;
+            xorr ^= x;
         }
         
+        
+        // find the first set bit from right
         int index = -1;
-        for(int i = 0 ;i < 32 ; i++){
-            if( ans >> i & 1){
-                // ith bit set
+        for(int i = 0 ; i < 32 ; i++){
+            if( (xorr & 1<<i) != 0 ){
+                // ith bit set is SET --> therefore we do != 0 
+                // we dont do == "1" --> bcz i-th bit  =1 hai toh jab & lenge toh
+                // toh non zero number aata hai
+                
                 index = i;
                 break;
             }
         }
+        cout <<"xorr = " << xorr << "index = " << index << endl;
         
+        //now divide elements in 2 groups
+        // 1st group --> have the i-th bit = 1
+        // 2nd group --> have the i-th bit = 0
         
-        vector<int> answer(2, 0);
-        
+        int first = 0;
+        int second = 0;
         for(auto &x : nums){
-            if( x >> index & 1){
-                // bit is set;
-                answer[0] ^=x;
-                // xor isliye kyuki -> baaki numbers duplicate hai
-                // so woh apne hat jayenge xor karte karte and 
-                // ek hi aisa number bachega jiski index-th bit set hai
+            if( (x & 1<<index) ==0){
+                // ith bit is not set 
+                second ^= x;
             }
             else{
-                answer[1] ^=x;
-                // xor isliye kyuki -> baaki numbers duplicate hai
-                // so woh apne hat jayenge xor karte karte and 
-                // ek hi aisa number bachega jiski index-th bit set NAHI hai
+                first ^= x;
             }
         }
         
-        return answer;
+        ans[0] = first;
+        ans[1] = second;
         
+
         
+        return ans;
     }
 };
