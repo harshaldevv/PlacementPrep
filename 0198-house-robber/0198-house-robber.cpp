f04a31a944c5c5865fd7953032cd29bba0f54afa
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-27 23:56:06 +0530

    Time: 0 ms (100.00%), Space: 9 MB (80.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    
-    int f(int i, vector<int> &nums, vector<int> &dp){
-        if(i == 0){
-            return nums[i];
+    int helper(int i, vector<int> &nums, vector<int> &dp){
+        if(i < 0){
+            return 0;
         }
-        if(i==1){
-            return max(nums[0], nums[1]);
+        
+        if( i == 0){
+            return dp[i] = nums[i];
         }
         
         if(dp[i] != -1){
             return dp[i];
         }
         
-        int nottake = f(i-1, nums, dp);
-        int take = f(i-2, nums, dp) + nums[i];
+        int pick = nums[i] + helper(i-2, nums, dp);
+        int notpick = helper(i-1, nums, dp);
         
-        return dp[i] =  max(take, nottake);
+        return dp[i] =  max(pick, notpick);
     }
     int rob(vector<int>& nums) {
         
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return f(n-1, nums, dp);
+        return helper(n-1, nums, dp);
         
     }
 };
