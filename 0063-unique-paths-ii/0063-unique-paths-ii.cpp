df3d94b70df191f55daab64072906d0fed1128d0
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-28 10:08:40 +0530

    Time: 0 ms (100.00%), Space: 10 MB (90.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,117 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp){
-        if(i < 0 || j < 0){
+    int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp){
+        if(i ==0 && j == 0){
+            if(grid[i][j] == 1){
+                //stone
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+        
+         if(i < 0 || j < 0 ){
             return 0;
         }
+        
         if(grid[i][j] == 1){
-            return dp[i][j] = 0;
+                //stone
+            return 0;
         }
         
-        if(i == 0 && j == 0 && grid[i][j] == 0){
-            return dp[i][j] = 1;
-        }
-        
+       
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-        return dp[i][j] = helper(i-1, j, grid, dp) + helper(i, j-1, grid, dp);
         
+        int up = f(i-1, j, grid, dp);
+        int left = f(i, j-1, grid, dp);
         
+        return dp[i][j] = up + left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         
         int m = grid.size();
         int n = grid[0].size();
+//         // vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+//         // return f(m-1, n-1, grid, dp);
         
-        if(grid[m-1][n-1] == 1){
-            return 0;
+        
+//         //base case
+        
+//         if(grid[0][0] == 1){
+//             return 0;
+//         }
+//         // tabulation
+//         for(int i = 0 ; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+                
+//                 if(grid[i][j] == 1){
+//                     //stone
+//                     dp[i][j] =  0;
+//                 }
+//                 else if(i == 0 && j == 0){
+//                     dp[i][j] =1 ;
+//                 }
+//                 else{
+//                     int up = 0;
+//                     int left = 0;
+                    
+//                     if(i >=1 ){
+//                         up = dp[i-1][j];
+//                     }
+                    
+//                     if(j >=1 ){
+//                         left = dp[i][j-1];
+//                     }
+                    
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+        
+        
+//         return dp[m-1][n-1];
+        
+        
+        // tabulation with space optimisation
+        
+        vector<int> curr(n, 0);
+        vector<int> prev(n, 0);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                
+                if(grid[i][j] == 1){
+                    //stone
+                    curr[j] =  0;
+                }
+                else if(i == 0 && j == 0){
+                    curr[j] = 1 ;
+                }
+                else{
+                    int up = 0;
+                    int left = 0;
+                    
+                    if(i >=1 ){
+                        up = prev[j];
+                    }
+                    
+                    if(j >=1 ){
+                        left = curr[j-1];
+                    }
+                    
+                    curr[j] = up + left;
+                }
+            }
+            
+            prev = curr;
         }
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return helper(m-1, n-1, grid, dp);
+        return prev[n-1];
+        
+        
         
     }
 };
