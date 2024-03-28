671e7bb84ae5026dea05b3fde4e84578522d6425
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-28 23:03:34 +0530

    Time: 46 ms (92.97%), Space: 16.3 MB (60.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,47 +1,70 @@
 class Solution {
 public:
-    
-    int helper(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
-        if(i < 0 || j < 0){
-            return false;
-        }
-        
-        if(j == 0){
-            return dp[i][j] = true;
+    int f(int i, int tar, vector<int> &nums, vector<vector<int>> &dp){
+        if(tar == 0){
+            return true;
         }
         
         if(i == 0){
-            return dp[i][j] =  nums[i] == j;
+            return nums[i] == tar;
         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        if(dp[i][tar] != -1){
+            return dp[i][tar];
         }
+        
+        bool nottake = f(i-1, tar, nums, dp);
         
         bool take = false;
-        bool nottake = false;
+        if(tar >= nums[i]){
+            take = f(i-1, tar - nums[i], nums, dp);
+        }
         
-        nottake= helper(i-1, j, nums, dp);
-        
-        take = helper(i-1, j - nums[i], nums, dp);
-        
-        return dp[i][j] =  take ||nottake;
+        return dp[i][tar] =  take | nottake;
     }
     bool canPartition(vector<int>& nums) {
         
-        int sum = 0;
+        int n = nums.size();
         
+        int s = 0;
         for(auto &x : nums){
-            sum +=x;
+            s+= x;
         }
         
-        if(sum%2 != 0){
+        if(s%2 != 0){
             return false;
         }
         
-        int n = nums.size() ;
-        vector<vector<int>> dp(n+1, vector<int>(sum/2 +1, -1 ));
-        return helper(n-1, sum/2, nums, dp);
+        
+        // vector<vector<int>> dp(n+1, vector<int> (s/2 +1, -1));
+        // return f(n-1, s/2, nums, dp);
+        
+        bool dp[n+1][s +1];
+        
+        memset(dp, false, sizeof(dp));
+        //basecase
+        for(int i = 0 ;i < n ; i++){
+            dp[i][0] = true;
+        }
+        
+        dp[0][nums[0]] = true;
+        
+        int tar = s/2;
+        for(int i = 1 ; i < n; i++){
+            for(int j= 1 ; j <= tar; j++){
+                
+                bool nottake = dp[i-1][j];
+                bool take = false;
+                if(j >= nums[i]){
+                    take = dp[i-1][j - nums[i]];
+                }
+
+                dp[i][j] =  take | nottake;
+                
+            }
+        }
+        
+        return dp[n-1][tar];
         
     }
 };
