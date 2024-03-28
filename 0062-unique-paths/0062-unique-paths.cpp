d5765d66a2d01deed96ede58e5d9518232d6b982
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-28 10:03:06 +0530

    Time: 3 ms (33.64%), Space: 7.1 MB (94.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,33 +1,76 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<vector<int>> &dp){
-        if(i < 0 || j < 0 ){
+    
+    int f(int i, int j, vector<vector<int>> &dp){
+        if(i == 0 && j == 0){
+            return 1;
+        }
+        
+        if(i == 0 || j == 0){
+            return 1;
+        }
+        
+        if(i < 0 || j < 0){
             return 0;
-        }
-        
-        if( i == 0 && j == 0){
-            return dp[i][j] = 1;
-        }
-        
-        if(i ==0){
-            return dp[i][j] = 1;
-        }
-        
-        
-        if(j ==0){
-            return dp[i][j] = 1;
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
+        int up = f(i-1, j, dp);
+        int left = f(i, j-1, dp);
         
-        return dp[i][j] = helper(i-1, j, dp)+ helper(i, j-1, dp);
-        
+        return dp[i][j] = up + left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return helper(m-1, n-1, dp);
+        
+//         vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+//         // return f(m-1, n-1, dp);
+        
+        
+//         dp[0][0] = 1;
+        
+//         for(int i = 0 ; i < m ; i++){
+//             dp[i][0] = 1;
+//         }
+        
+//         for(int i = 0 ; i < n ; i++){
+//             dp[0][i] = 1;   
+//         }
+        
+//         for(int i = 1 ; i< m; i++){
+//             for(int j = 1 ; j < n ; j++){
+                
+//                 int up = dp[i-1][j];
+//                 int left = dp[i][j-1];
+
+//                 dp[i][j] = up + left;
+                
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        //space optimisation
+        vector<int> prev(n, 1);  // 1D array for the previous row
+        vector<int> curr(n, 1);  // 1D array for the current row
+        
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                curr[j] = curr[j-1] + prev[j];
+            }
+            
+            prev = curr;
+        }
+        
+        return prev[n-1];
+        
+        
+        
+        
+        
+        
+        
         
     }
 };
