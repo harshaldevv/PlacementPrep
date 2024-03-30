6838a95261208dab64a6bf7d4d52e9ec57689721
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-31 00:39:27 +0530

    Time: 2194 ms (5.04%), Space: 589.7 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,43 +1,38 @@
 class Solution {
 public:
-    
-    int f(int i, int j, vector<int> &nums, vector<vector<int>> &dp){
-        if(j == 0){
-            return 0;
+    int f(int i, int j, vector<int> coins, vector<vector<int>> &dp){
+        if(i < 0 || j < 0 ){
+            return 1e5;
         }
         
         if(i == 0){
-            if(j%nums[i] == 0){
-                return j/nums[i];
+            if(j%coins[i] == 0){
+                return dp[i][j] = j/coins[i];
             }
             else{
-                return 1e5;
-                // agar nahi ban paa raha , toh no of coins req = INT MAX
-                // kyuki jitna bhi try karlo, nahi banega
+                return dp[i][j] =  1e5;
             }
+        }
+        
+        if(j == 0){
+            return dp[i][j] = 0;
         }
         
         if(dp[i][j] != -1){
-            return dp[i][j];
+            return dp[i][j] ;
         }
         
-        int nottake = f(i-1, j, nums, dp );
-        
+        int nottake = f(i-1, j, coins, dp);
         int take = INT_MAX;
-        
-        if(j >= nums[i]){
-            take = 1 +  f(i, j- nums[i], nums, dp) ;
+        if(j >= coins[i]){
+            take = 1 + f(i, j - coins[i], coins, dp);
         }
         
-        return dp[i][j] = min(take, nottake);
+        return dp[i][j] =  min(take, nottake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        
-        // knapsack
         int n = coins.size();
-        
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        //striver method
+        vector<vector<int>> dp(n+1, vector<int>(amount +1 , -1));
         int ans =  f(n-1, amount, coins, dp);
         
         if(ans >= 1e5){
