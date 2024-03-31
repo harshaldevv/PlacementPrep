03d07eab2fe29820178c55cef1efb3890027c8a7
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-03-31 16:14:56 +0530

    Time: 49 ms (8.06%), Space: 25.8 MB (30.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i == 0 || j == 0 ){
+        if(i < 0 || j < 0){
             return 0;
         }
         
@@ -10,82 +10,25 @@ public:
             return dp[i][j];
         }
         
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] =  1 + f(i-1, j-1, s1, s2, dp);
+        int op1 = 0;
+        if(s1[i] == s2[j]){
+            op1 =  1 + f(i-1, j-1, s1, s2, dp);
+            return dp[i][j] = op1;
         }
         else{
-            return dp[i][j] = max(f(i-1, j, s1, s2, dp), f(i, j-1, s1, s2, dp));
+            int op2 = f(i, j-1, s1, s2, dp);
+            int op3 = f(i-1, j, s1, s2, dp);
+            return dp[i][j] =  max(op2, op3);
         }
+        
     }
-    int longestCommonSubsequence(string s1, string s2) {
+    int longestCommonSubsequence(string text1, string text2) {
         
-        int n1  = s1.size();
-        int n2 = s2.size();
+        int n1 = text1.size();
+        int n2 = text2.size();
         
-        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        // return f(n1-1, n2-1, s1, s2, dp);
-        
-        //tabulation
-        
-        vector<vector<int>> dp(n1+2, vector<int>(n2+2, 0));
-        
-        for(int i =1 ; i<= n1 ; i++){
-            for(int j = 1 ; j <= n2 ; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] =  1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        
-        // now trying to print LCS
-        int i = n1;
-        int j = n2;
-        string ans = "";
-        while(i>0 && j > 0){
-            if(s1[i] == s2[j]){
-                ans += s1[i];
-                i--;
-                j--;
-            }
-            else if(dp[i-1][j] > dp[i][j-1]){
-                i--;
-            }
-            else{
-                j--;
-            }
-        }
-        
-        reverse(ans.begin(), ans.end());
-        cout << "Longest Common Subsequence = " << ans << endl;
-        
-        
-        return dp[n1][n2];
-        
-        
-//         //tabulation with space
-        
-//         vector<int> curr (n2+2, 0);
-//         vector<int> prev (n2+2, 0);
-        
-        
-//         for(int i =1 ; i<= n1 ; i++){
-//             for(int j = 1 ; j <= n2 ; j++){
-//                 if(s1[i-1] == s2[j-1]){
-//                     curr[j] =  1 + prev[j-1];
-//                 }
-//                 else{
-//                     curr[j] = max(prev[j], curr[j-1]);
-//                 }
-//             }
-            
-//             prev = curr;
-//         }
-        
-//         return prev[n2];
-        
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return f(n1-1, n2-1, text1, text2, dp);
         
     }
 };
