18e205448f39720ac10271c030f1d2f7f1ad8f10
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-02 12:27:30 +0530

    Time: 13 ms (28.41%), Space: 11.9 MB (54.54%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -34,8 +34,37 @@ public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size();
         int n2 = word2.size();
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        return  f(n1-1, n2-1,  word1, word2, dp);
+        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        // return  f(n1-1, n2-1,  word1, word2, dp);
         
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        
+        for(int i = 0 ; i <= n1 ; i++){
+            for(int j = 0 ; j <= n2 ; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+                else if(j == 0){
+                    dp[i][j] = i;
+                }
+            }
+        }
+        
+        for(int i = 1 ; i <= n1 ; i++){
+            for(int j = 1 ; j <= n2 ; j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insertStep = 1 + dp[i][j-1];
+                    int deleteStep = 1 + dp[i-1][j];
+                    int replaceStep = 1 + dp[i-1][j-1];
+                    dp[i][j] =  min(insertStep, min(deleteStep, replaceStep));
+                }
+            }
+        }
+        
+        
+        return dp[n1][n2];
     }
 };
