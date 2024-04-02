6e7d43d7de53ad88df4695bce9f404a2490ab6a7
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-02 11:20:35 +0530

    Time: 26 ms (34.40%), Space: 25.9 MB (13.38%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
+    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+        if(i <= 0 || j <= 0){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        if(s1[i-1] == s2[j-1]){
+            return dp[i][j] =  1 + f(i-1, j-1, s1, s2, dp);
+        }
+        else{
+            return dp[i][j] = max(f(i-1,j,s1, s2, dp), f(i, j-1, s1, s2, dp));
+        }
+        
+        
+    }
     string shortestCommonSupersequence(string s1, string s2) {
         
         int n1 = s1.size();
         int n2 = s2.size();
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        int LCS =  f(n1, n2, s1,s2, dp);
         
-        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        cout << "LCS = " << LCS << endl;
         
-        // dp[0][j] = 0, dp[i][0] = 0
+        int scs_length = n1+n2 - LCS;
+        cout << "SCS length = " <<scs_length << endl;
         
-        for(int i = 1 ; i <= n1 ; i++){
-            for(int j = 1 ; j <= n2 ; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        
-        // lcs aa gaya
-        int lcs = dp[n1][n2];
-        // cout << "lcs = " <<  lcs << endl;
-        int scs_len = n1+n2 - lcs;
-        // cout << "scs len = " <<  scs_len << endl;
+        string ans = "";
         
         int i = n1;
         int j = n2;
-        string ans = "";
         
-        while(i>0 && j >0){
+        while(i > 0 && j > 0){
             if(s1[i-1] == s2[j-1]){
                 ans += s1[i-1];
                 i--;
                 j--;
             }
             else if(dp[i-1][j] > dp[i][j-1]){
-                ans += s1[i-1] ;
+                ans += s1[i-1];
                 i--;
             }
             else{
@@ -46,20 +51,20 @@ public:
             }
         }
         
-        cout << ans <<endl;
-        while(i>0){
-            ans += s1[i-1];
-            i--;
-        }
+         while(i > 0){
+             ans += s1[i-1];
+             i--;
+         }
         
-        while(j>0){
-            ans += s2[j-1];
-            j--;
-        }
+        while(j > 0){
+             ans += s2[j-1];
+             j--;
+         }
         
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(),ans.end());
         
         return ans;
+        
         
     }
 };
