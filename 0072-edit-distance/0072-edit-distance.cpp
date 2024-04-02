120efa48e13ce0b6929a4df54bffc4b5397e4be5
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-02 12:20:23 +0530

    Time: 7 ms (64.13%), Space: 12.1 MB (25.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,75 +1,41 @@
 class Solution {
 public:
     int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(i==0){
-            return j;
+        
+        
+        if(i < 0){
+            return j+1;
         }
         
-        if(j==0){
-            return i;
+        if(j < 0){
+            return i+1;
         }
+        
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        if(s1[i-1] == s2[j-1]){
-            return dp[i][j] = f(i-1, j-1, s1, s2, dp);
+        if(s1[i] == s2[j]){
+            // character has matched
+            // do nothing
+            return dp[i][j] = f(i-1, j-1, s1,s2, dp);
         }
         else{
-            int insert = f(i, j-1, s1, s2, dp);
-            int delet = f(i-1, j, s1, s2, dp);
-            int replace = f(i-1, j-1, s1, s2, dp);
+            int insertStep = 1 + f(i, j-1, s1, s2, dp);
+            int deleteStep = 1 + f(i-1, j, s1, s2, dp);
+            int replaceStep = 1 + f(i-1,j-1, s1, s2, dp);
             
-            return dp[i][j] = min({insert, delet, replace}) +1;
+            
+            return dp[i][j] =  min(insertStep, min(deleteStep, replaceStep));
+            
         }
-        
     }
     int minDistance(string word1, string word2) {
-        
         int n1 = word1.size();
         int n2 = word2.size();
-        // vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        // return f(n1, n2, word1, word2, dp);
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
+        return  f(n1-1, n2-1,  word1, word2, dp);
         
-        
-        //tabulation
-//         vector<vector<int>> dp(n1+1, vector<int>(n2+1, -1));
-        
-//         for(int j = 0 ; j <= n2 ; j++){
-//             dp[0][j] = j;
-//         }
-        
-//         for(int i = 0 ; i <= n1 ; i++){
-//             dp[i][0] = i;
-//         }
-        
-        vector<int> prev(n2+1, 0);
-        vector<int> curr(n2+1, 0);
-        
-        for(int j = 0 ; j <= n2 ; j++){
-            prev[j] = j;
-        }
-        
-        for(int i= 1 ; i <= n1 ; i++){
-            curr[0] = i; //base case
-            for(int j = 1 ; j <= n2 ; j++){
-                 if(word1[i-1] == word2[j-1]){
-                    curr[j] = prev[j-1];
-                }
-                else{
-                    int insert = curr[j-1];
-                    int delet = prev[j];
-                    int replace = prev[j-1];
-
-                    curr[j] = min({insert, delet, replace}) +1;
-                }
-            }
-            
-            prev = curr;
-        }
-        
-        return prev[n2];
-
     }
 };
