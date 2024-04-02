779af1b093a54befcd134fa039309ff2b4565404
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-02 10:39:48 +0530

    Time: 5 ms (32.33%), Space: 14.4 MB (26.95%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,61 +1,30 @@
 class Solution {
 public:
-    int f(int i, bool canbuy, vector<int> &prices, vector<vector<int>> &dp){
-        if(i>= prices.size()){
+    int f(int i, vector<int> &prices, bool toBuy, vector<vector<int>> &dp){
+        if( i >= prices.size()){
             return 0;
         }
         
-        if(dp[i][canbuy] != -1){
-            return dp[i][canbuy];
+        if(dp[i][toBuy] != -1){
+            return dp[i][toBuy];
         }
         
-        if(canbuy){
-            //buy, not buy
-            int buy = -prices[i] + f(i+1, false, prices, dp);
-            int notbuy = f(i+1, true, prices, dp);
-            return  dp[i][canbuy] = max(buy, notbuy);
+        if(toBuy){
+            int buy = -prices[i] + f(i+1, prices, false, dp);
+            int notbuy = f(i+1, prices, true, dp);
+            return dp[i][toBuy] = max(buy, notbuy);
         }
         else{
-            //sell
-            // sell, not sell
-            int sell = +prices[i] + f(i+2, true, prices, dp);
-            int notsell = f(i+1, false, prices, dp);
-            return dp[i][canbuy] =  max(sell, notsell);
-            
+            int sell = +prices[i] + f(i+2, prices, true, dp);
+            int notsell =  f(i+1, prices, false, dp);
+            return dp[i][toBuy] =  max(sell, notsell);
         }
     }
     int maxProfit(vector<int>& prices) {
+        
         int n = prices.size();
-        
-        
-        // vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        // return f(0, true, prices, dp);
-        
-        
-        vector<vector<int>> dp(n+2, vector<int>(3, 0));
-        
-        for(int i = n-1 ; i >= 0 ; i--){
-            for(int j = 0 ; j <= 1 ; j++){
-                if(j){
-                    //buy, not buy
-                    int buy = -prices[i] +  dp[i+1][false];
-                    int notbuy = dp[i+1][true];
-                    dp[i][j] = max(buy, notbuy);
-                }
-                else{
-                    //sell
-                    // sell, not sell
-                    
-                    int notsell = dp[i+1][false];
-                    int sell = +prices[i] + dp[i+2][true];   
-    
-                
-                    dp[i][j] =  max(sell, notsell);
-                }
-            }
-        }
-        
-        return dp[0][true];
+        vector<vector<int>> dp(n+1, vector<int>(3, -1));
+        return f(0, prices, true, dp);
         
     }
 };
