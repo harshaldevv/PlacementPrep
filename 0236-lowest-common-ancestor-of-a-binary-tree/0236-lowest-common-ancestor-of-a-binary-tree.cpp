bd75293bbbac7397815b6115bc18b6daaa608675
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-04 09:38:34 +0530

    Time: 13 ms (54.57%), Space: 16 MB (51.17%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,27 +9,17 @@
  */
 class Solution {
 public:
-    
-    TreeNode *LCA(TreeNode* root, TreeNode* p, TreeNode* q){
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root){
-            return NULL;
+            return nullptr;
         }
         
-        if(!p && !q){
-            return NULL;
-        }
-        
-        if(!p || !q){
-            return NULL;
-        }
-        
-        if(root == p || root == q){
+        if(root == p || root== q){
             return root;
         }
         
-        
-        TreeNode *left = LCA(root->left, p, q);
-        TreeNode *right = LCA(root->right, p, q);
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
         
         if(left && right){
             return root;
@@ -41,10 +31,6 @@ public:
             return right;
         }
         
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        return LCA(root, p, q);
         
     }
 };
