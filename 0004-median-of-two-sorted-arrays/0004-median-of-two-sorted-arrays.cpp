d4b54cb3daf95d5eea0cbba3ba7bfd885fd41fc3
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-07 22:17:09 +0530

    Time: 19 ms (86.60%), Space: 94.3 MB (91.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -11,31 +11,30 @@ public:
         
         int l = 0;
         int r = n1;
-        int len = n1 + n2 ;
+        int len = n1 + n2;
         
         while(l<=r){
             int mid = l + (r-l)/2;
+            int cut1 = mid; // left side kitne chahiye elements
+            int cut2 = (len+1)/2 - cut1; // right side kitne elements honge
             
-            int cut1 = mid;
-            int cut2 = (len+1)/2 - cut1;
-            
-            int l1 = cut1 - 1 >= 0 ?  nums1[cut1-1]  : INT_MIN;
-            int l2 = cut2 - 1 >= 0 ? nums2[cut2-1] : INT_MIN;
+            int l1 = cut1 -1 >= 0 ? nums1[cut1 -1] : INT_MIN;
+            int l2 = cut2 -1 >= 0 ? nums2[cut2 -1] : INT_MIN;
             int r1 = cut1 < n1 ? nums1[cut1] : INT_MAX;
             int r2 = cut2 < n2 ? nums2[cut2] : INT_MAX;
             
             if(l1 <= r2 && l2 <= r1){
-                if( (len+1)%2 == 0 ){
-                    // odd;
+                if( (len+1)%2 == 0){
+                    // therefore len is odd
                     return max(l1, l2);
                 }
                 else{
-                    return ( max(l1, l2) + min(r1, r2) )/ 2.0 ;
+                    return (max(l1, l2) + min(r1, r2) )/2.0;
                 }
             }
-            else if(l1  > r2){
+            else if(l1 > r2){
                 // go left
-                r = mid-1;
+                r = mid -1;
             }
             else{
                 l = mid +1;
