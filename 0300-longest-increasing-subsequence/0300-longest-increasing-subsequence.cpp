5d204bac594e57305c8bb9ab81f7a405f157b85e
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2024-04-14 23:46:35 +0530

    Time: 530 ms (10.20%), Space: 294.8 MB (9.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,77 +1,32 @@
 class Solution {
 public:
-    int f(int i, int prevIndex, vector<int> &nums, vector<vector<int>> &dp){
-        if( i>= nums.size()){
+    int f(int i, int previndex, vector<int> &nums, vector<vector<int>> &dp){
+        if(i >= nums.size()){
             return 0;
         }
-        if(dp[i][prevIndex+1] != -1){
-            return dp[i][prevIndex+1];
-        }
         
-        int nottake = f(i+1, prevIndex, nums, dp);
+        int j = previndex;
+        if(dp[i][j +1] != -1){
+            return dp[i][j+1];
+        }
+        // not take 
+        int nottake = f(i+1, previndex, nums, dp);
         
         int take = 0;
-        
-        if(prevIndex == -1 || nums[i] > nums[prevIndex]){
+        if(previndex == -1 || nums[i] > nums[previndex]){
             take = 1 + f(i+1, i, nums, dp);
         }
-        //bas store karne ke lie prevIndex me +1 kia, kyuki prevIndex goes from -1 to n-1 
-        // therefore prevIndex +1 -> goes from 0 to n.
-        return dp[i][prevIndex+1] = max(take, nottake);
+        
+        return dp[i][j+1] = max(take, nottake);
     }
+    
     int lengthOfLIS(vector<int>& nums) {
+        
         int n = nums.size();
+        int previndex = -1;
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
         
-        // f(i, prevIndx) -> means length of LIS starting from i indx 
-        // and having prevIndx as previous Index
-        
-        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        // return f(0, -1, nums, dp);
-        
-        
-//         // tabulation
-//         vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-//         for(int i = n-1 ; i >=0 ; i--){
-//             for(int j = n-1 ; j>= -1 ; j--){
-                
-//                 int nottake = dp[i+1][j+1];
-        
-//                 int take = 0;
-
-//                 if(j == -1 || nums[i] > nums[j]){
-//                     take = 1 + dp[i+1][i+1];
-//                 }
-                
-//                 dp[i][j+1] = max(take, nottake);   
-//             }
-//         }
-        
-//         return dp[0][0];
-        
-        
-        // tabulation with space optimisation
-        
-        vector<int> ahead(n+1, 0);
-        vector<int> curr(n+1, 0);
-        
-        for(int i = n-1 ; i >=0 ; i--){
-            for(int j = n-1 ; j>= -1 ; j--){
-                
-                int nottake = ahead[j+1];
-        
-                int take = 0;
-
-                if(j == -1 || nums[i] > nums[j]){
-                    take = 1 + ahead[i+1];
-                }
-                
-                curr[j+1] = max(take, nottake);   
-            }
-            ahead = curr;
-        }
-        
-        return ahead[0];
-        
+        return f(0, previndex, nums, dp);
         
     }
 };
