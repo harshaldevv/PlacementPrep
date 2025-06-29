739d6cd8daf4ee3d8a5089f1d53d9d174708cc7c
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2025-06-29 22:13:59 +0530

    Time: 30 ms (5.32%), Space: 18.5 MB (23.98%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,37 +1,35 @@
-
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        // hci research
-        
-        
-        // Create a hash table to store the frequency of each element in nums
-        unordered_map<int, int> freqMap;
-        for (int num : nums) {
-            freqMap[num]++;
+
+        // min heap
+        unordered_map<int, int> mp;
+
+        for(auto &x : nums){
+            mp[x]++;
         }
-        
-        // Create a min heap of size k to store the top k frequent elements
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        
-        // Loop through the hash table and add the top k frequent elements to the min heap
-        for (auto entry : freqMap) {
-            minHeap.push({entry.second, entry.first});
-            if (minHeap.size() > k) {
-                minHeap.pop();
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+        for(auto &[val, freq] : mp){
+            cout << "val = " << val << ", freq = " << freq << endl;
+            pq.push({freq, val});
+
+            if(pq.size() > k){
+                pq.pop();
             }
         }
-        
-        // Create a vector to store the top k frequent elements
-        vector<int> result(k);
-        
-        // Pop the top k frequent elements from the min heap and add them to the result vector
-        for (int i = k - 1; i >= 0; i--) {
-            result[i] = minHeap.top().second;
-            minHeap.pop();
+
+        vector<int> ans;
+        while(pq.size() > 0){
+            ans.push_back(pq.top()[1]);
+            pq.pop();
         }
+
+        return ans;
+
+
         
-        return result;
+        
     }
 };
