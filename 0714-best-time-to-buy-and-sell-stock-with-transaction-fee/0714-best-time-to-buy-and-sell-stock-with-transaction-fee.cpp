06c9cf9f4d82f93c0ebd77e8ab95c2fad5b1ba62
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2025-07-09 07:55:48 +0530

    Time: 89 ms (62.27%), Space: 58.9 MB (78.81%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -34,11 +34,39 @@ public:
         // return f(0, true, fee, prices, dp);
 
 
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        // f(0, true, fee, prices, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+        // // n-1 to 0
+        // // false to true
+
+        // for(int i = n-1 ; i>= 0; i--){
+        //     for(int j = 0 ; j <= 1; j++){
+        //         bool canbuy = j;
+
+        //         if(canbuy){
+        //             // i can buy -> buy or skip
+        //             int buy = -prices[i] + dp[i+1][false];
+        //             int skip = dp[i+1][true];
+        //             dp[i][j] = max(buy, skip);
+        //         }
+        //         else{
+        //             // i cannot buy -> hold or sell
+        //             int hold = dp[i+1][false];
+        //             int sell = +prices[i] - fee + dp[i+1][true];
+        //             dp[i][j] = max(hold, sell);
+        //         } 
+        //     }
+        // }
+
+        // return dp[0][true];
+
+        
+        // -------
 
         // n-1 to 0
         // false to true
+        vector<int> curr(2, 0);
+        vector<int> ahead(2, 0);
 
         for(int i = n-1 ; i>= 0; i--){
             for(int j = 0 ; j <= 1; j++){
@@ -46,20 +74,22 @@ public:
 
                 if(canbuy){
                     // i can buy -> buy or skip
-                    int buy = -prices[i] + dp[i+1][false];
-                    int skip = dp[i+1][true];
-                    dp[i][j] = max(buy, skip);
+                    int buy = -prices[i] + ahead[false];
+                    int skip = ahead[true];
+                    curr[j] = max(buy, skip);
                 }
                 else{
                     // i cannot buy -> hold or sell
-                    int hold = dp[i+1][false];
-                    int sell = +prices[i] - fee + dp[i+1][true];
-                    dp[i][j] = max(hold, sell);
+                    int hold = ahead[false];
+                    int sell = +prices[i] - fee + ahead[true];
+                    curr[j] = max(hold, sell);
                 } 
             }
+
+            ahead = curr;
         }
 
-        return dp[0][true];
+        return ahead[true];
         
     }
 
