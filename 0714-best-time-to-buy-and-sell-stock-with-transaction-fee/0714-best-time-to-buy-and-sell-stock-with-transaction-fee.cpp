628d066eadef36ad6de7f79c00e01503db1f9807
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2025-07-09 07:50:13 +0530

    Time: 112 ms (30.11%), Space: 100.6 MB (7.57%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,95 +1,38 @@
 class Solution {
 public:
-    int f(int i, bool youcanbuy, vector<int>&prices, int &fees, vector<vector<int>> &dp){
-        if(i >= prices.size()){
+    int f(int i, bool canbuy, int &fees, vector<int> &prices, vector<vector<int>> &dp){
+        
+        if( i>= prices.size()){
             return 0;
         }
-        
-        if(dp[i][youcanbuy] != -1){
-            return dp[i][youcanbuy];
+
+        int j = canbuy;
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
-        
-        if(youcanbuy){
-            //buy, not buy
-            int buy = -prices[i] - fees + f(i+1, false, prices, fees, dp);
-            int notbuy = f(i+1, true, prices, fees, dp);
-            return dp[i][youcanbuy] = max(buy, notbuy);
+
+        if(canbuy){
+            // i can buy -> buy or skip
+
+            int buy = -prices[i] + f(i+1, false, fees, prices, dp);
+            int skip = f(i+1, true, fees, prices, dp);
+            return dp[i][j] = max(buy, skip);
         }
         else{
-            //sell, not sell
-            int sell = +prices[i] + f(i+1, true, prices, fees, dp);
-            int notsell = f(i+1, false, prices, fees, dp);
-            
-            return dp[i][youcanbuy] = max(sell, notsell);
+            // i cannot buy -> hold or sell
+            int hold = f(i+1, false,  fees, prices, dp);
+            int sell = +prices[i] - fees + f(i+1, true, fees, prices, dp);
+            return dp[i][j] = max(hold, sell);
         }
-        
-        
+
+
     }
-    int maxProfit(vector<int>& prices, int &fees) {
-        
+    int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        // bool youcanbuy = true;
-        // vector<vector<int>> dp(n+1, vector<int>(3, -1));
-        // return f(0, youcanbuy, prices, fees, dp);
-        
-//         //tabulation
-//         vector<vector<int>> dp(n+1, vector<int>(3, 0));
-//         // i -> 0 to n-1 --> here n-1 to 0;
-//         // 1 --> 0 to 1
-        
-//         for(int i = n-1 ; i>= 0; i--){
-//             for(int j = 0 ; j <= 1 ; j++){
-                
-//                 if(j){
-//                     //buy, not buy
-//                     int buy = -prices[i] - fees + dp[i+1][false];
-//                     int notbuy = dp[i+1][true];
-                    
-//                     dp[i][j] = max(buy, notbuy);
-//                 }
-//                 else{
-//                     //sell, not sell
-//                     int sell = +prices[i] + dp[i+1][true];
-//                     int notsell = dp[i+1][false];
-
-//                     dp[i][j] = max(sell, notsell);
-//                 }
-                
-//             }
-//         }
-        
-//         return dp[0][1];
-        
-        
-        //tabulation with space optimisation
-        vector<int> after(3, 0);
-        vector<int> curr(3, 0);
-        
-        for(int i = n-1 ; i>= 0; i--){
-            for(int j = 0 ; j <= 1 ; j++){
-                
-                if(j){
-                    //buy, not buy
-                    int buy = -prices[i] - fees + after[false];
-                    int notbuy = after[true];
-                    
-                    curr[j] = max(buy, notbuy);
-                }
-                else{
-                    //sell, not sell
-                    int sell = +prices[i] + after[true];
-                    int notsell = after[false];
-
-                    curr[j] = max(sell, notsell);
-                } 
-            }
-            
-            after = curr;
-        }
-        
-        return after[1];
-        
-        
+        vector<vector<int>> dp(n+1, vector<int>(2, -1));
+        return f(0, true, fee, prices, dp);
         
     }
+
 };
