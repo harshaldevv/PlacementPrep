0745d04078c10ac2d111cd4f08bf0b4c40a1ab13
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2025-07-13 10:23:46 +0530

    Time: 35 ms (75.83%), Space: 53.5 MB (85.16%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
-    bool isPal(string &s, int l, int r){
-        while(l<r){
-            if(s[l] != s[r]){
+    bool isPal(string &s){
+        int i = 0;
+        int j = s.size() -1;
+
+        while(i < j){
+            if(s[i++] != s[j--]){
                 return false;
-            }
-            else{
-                l++;
-                r--;
             }
         }
         return true;
     }
     
-    void helper(int i, vector<string> &holder, vector<vector<string>> &ans, string &s){
-        if(i >= s.size()){
+    void f(int start, string &str, vector<string> &holder, vector<vector<string>> &ans){
+        if(start >= str.size()){
             ans.push_back(holder);
             return ;
         }
         
-        for(int j = i ; j < s.size() ; j++){
-            if(isPal(s, i, j)){
-                string temp = s.substr(i, j-i+1);
-                holder.push_back(temp);
-                helper(j+1, holder, ans, s);
+        
+        for(int i = start; i < str.size(); i++){
+            string myStr = str.substr(start, i - start +1);
+
+            bool firstHalfIsPalindrome = isPal(myStr);
+            if(firstHalfIsPalindrome){
+                holder.push_back(myStr);
+                
+                // only now look to find more
+                f(i+1, str, holder, ans);
+
                 holder.pop_back();
             }
         }
+
+        return;
     }
-    vector<vector<string>> partition(string s) {
-        
+
+    vector<vector<string>> partition(string &s) {
+
         vector<vector<string>> ans;
         vector<string> holder;
-        
-        int i = 0;
-        helper(i, holder, ans, s);
+        if(s.size() == 0){
+            return ans;
+        }
+
+        string temp = "";
+        f(0, s, holder, ans);
         
         return ans;
-        
         
     }
 };
