de1d7c7732ddfa01b9d8cd4347ad09f45919baf7
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2026-01-15 20:12:29 +0530

    Time: 7 ms (41.54%), Space: 24.7 MB (19.5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
+
         sort(intervals.begin(), intervals.end());
+
         vector<vector<int>> ans;
-        int n = intervals.size();
         ans.push_back(intervals[0]);
         
-        for(int i = 1 ; i < n ; i++){
-            auto curr = intervals[i];
-            auto prev = ans.back();
-            
-            if(prev[1] >= curr[0]){
-                ans.back()[1] = max(curr[1], prev[1]);
+
+        for(int i = 1 ; i < intervals.size(); i++){
+            auto lastMeeting = ans.back();
+            int lastMeetingStart = lastMeeting[0];
+            int lastMeetingEnd = lastMeeting[1];
+            int currMeetingStart = intervals[i][0];
+            int currMeetingEnd = intervals[i][1];
+
+            if(currMeetingStart <= lastMeetingEnd){
+                // overlap found
+                ans.back()[1] = max(ans.back()[1], currMeetingEnd);
             }
             else{
-                ans.push_back(curr);
+                ans.push_back(intervals[i]);
             }
         }
-        
+
         return ans;
+
         
     }
 };
