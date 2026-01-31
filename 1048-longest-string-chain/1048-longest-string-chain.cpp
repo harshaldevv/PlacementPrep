072347b43a8f799e6101d6c175b50b9fb70a7b46
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2026-01-31 19:50:58 +0530

    Time: 343 ms (25.47%), Space: 19.9 MB (56.26%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,60 +1,45 @@
 class Solution {
 public:
-    bool checkWord(string &currWord, string &lastWord){
 
-        // now we will start deleting char from currWord, to see if they match up to lastWord
-        for(int i = 0 ; i < currWord.size(); i++){
+    bool checkWord(string &currWord, string &prevWord){
+        // currWord -> bigger word
+        // prevWord -> smaller word
+        
+        // first check the size
+        if(currWord.size() - prevWord.size() != 1){
+            return false;
+        }
+
+        for(int i = 0 ; i < currWord.size() ; i++ ){
             string p1 = currWord.substr(0, i);
-            string p2 = currWord.substr(i+1, currWord.size() - i - 1);
-
-            string wordToCheck = p1+ p2;
-
-            if(wordToCheck == lastWord){
+            string p2 = currWord.substr(i+1, currWord.size() - (i+1));
+            string newWord = p1+p2;
+            if(newWord == prevWord){
                 return true;
             }
         }
-
         return false;
     }
 
-    int f(int i, int j, vector<string> &words, vector<vector<int>> &dp){
-        // i = curr word index (bigger)
-        // j = last word index (smaller)
-
-        if( i >= words.size() ){
-            return 0;
-        }
-
-        if(dp[i][j+1] != -1){
-            return dp[i][j+1];
-        }
-
-        int notake = f(i+1, j, words, dp);
-
-        // take
-        int take = 0;
-        if(j == -1 || words[i].size() - words[j].size() == 1 && checkWord(words[i], words[j])){
-            take = 1 + f(i+1, i, words, dp);
-        }
-
-        return dp[i][j+1] = max(take, notake);
-
-    }
-
     int longestStrChain(vector<string>& words) {
-        // sort words length wise
-        
+
+        // sort by length
         sort(words.begin(), words.end(), [](string &s1, string &s2){
             return s1.size() < s2.size();
         });
 
-        // LIS -> 0th index se start hoga
-
         int n = words.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return f(0, -1, words, dp);
-
-
-        
+        vector<int> dp(n+1, 1);
+        int ans = 1;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(checkWord(words[i], words[j])){
+                    dp[i] = max(1+ dp[j], dp[i]);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
+
 };
