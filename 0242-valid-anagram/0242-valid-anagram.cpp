88b548ce6ff9c4ef6e880f6099154fab0ed546bb
Harshal Dev (Dholchike) <55681046+harshaldevv@users.noreply.github.com>
2026-03-01 15:23:26 +0530

    Time: 0 ms (100%), Space: 9.8 MB (23.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,71 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        // hci research
-        if (s.length() != t.length()) {
+
+        if(s.size() != t.size()){
             return false;
         }
+
         
-        unordered_map<char, int> charCount;
-        
-        for (int i = 0; i < s.length(); i++) {
-            charCount[s[i]]++;
-            charCount[t[i]]--;
+
+        // vector<int> v(26, 0);
+
+        // for(int i = 0 ; i < s.size(); i++){
+        //     v[ s[i] - 'a']++;
+        //     v[ t[i] - 'a']--;
+        // }
+
+        // // eventually all buckets should be zero
+        // for(auto &x: v){
+        //     if(x != 0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+
+
+        //  if(s.size() != t.size()){
+        //     return false;
+        // }
+
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+
+        // int l1 = 0;
+        // int l2 = 0;
+        // for(int i = 0 ; i < s.size(); i++){
+        //     if(s[i] != t[i]){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+
+        unordered_map<char, int> mp;
+        for(auto &x: s){
+            mp[x]++;
         }
-        
-        for (auto& p : charCount) {
-            if (p.second != 0) {
+
+        for(auto &x: t){
+            if(mp.find(x) == mp.end()){
                 return false;
             }
+            else{
+                mp[x]--;
+                if(mp[x] == 0){
+                    mp.erase(x);
+                }
+            }
         }
-        
+
         return true;
+
+
+
+
+        
     }
 };
